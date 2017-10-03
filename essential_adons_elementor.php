025002cc922d8ad5abad0534b4daf69c411719d8
Nazmul Hasan Rupok <re.enter.rupok@gmail.com>
2017-10-03 12:39:10 +0600

    settings action menu added

--- essential_adons_elementor.php
+++ essential_adons_elementor.php
@@ -121,14 +121,14 @@ add_action( 'elementor/editor/before_enqueue_scripts', function() {
 
 // Action menus
 
-function plugin_add_settings_link( $links ) {
+function eael_add_settings_link( $links ) {
     $settings_link = sprintf( '<a href="admin.php?page=eael-settings">' . __( 'Settings' ) . '</a>' );
     $go_pro_link = sprintf( '<a href="https://essential-addons.com/elementor/buy.php" target="_blank" style="color: #39b54a; font-weight: bold;">' . __( 'Go Pro' ) . '</a>' );
     array_push( $links, $settings_link, $go_pro_link );
    return $links;
 }
 $plugin = plugin_basename( __FILE__ );
-add_filter( "plugin_action_links_$plugin", 'plugin_add_settings_link' );
+add_filter( "plugin_action_links_$plugin", 'eael_add_settings_link' );
 
 
 
