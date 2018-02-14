f77219852124acbc3e633cba25c0b1a797d930c7
Md Rabiul Islam Robi <robicse11127@gmail.com>
2018-02-14 13:59:51 +0600

    added a new function for init modules scripts by default

--- essential_adons_elementor.php
+++ essential_adons_elementor.php
@@ -21,12 +21,40 @@ require_once ESSENTIAL_ADDONS_EL_PATH.'includes/queries.php';
 require_once ESSENTIAL_ADDONS_EL_PATH.'includes/class-plugin-usage-tracker.php';
 require_once ESSENTIAL_ADDONS_EL_PATH.'admin/settings.php';
 
+/**
+ * This function will return true for all activated modules
+ *
+ * @since   v2.4.1
+ */
+function eael_activated_modules() {
+
+   $eael_default_keys = [ 'contact-form-7', 'count-down', 'creative-btn', 'fancy-text', 'img-comparison', 'instagram-gallery', 'interactive-promo',  'lightbox', 'post-block', 'post-grid', 'post-timeline', 'product-grid', 'team-members', 'testimonial-slider', 'testimonials', 'testimonials', 'weforms', 'static-product', 'call-to-action', 'flip-box', 'info-box', 'dual-header', 'price-table', 'flip-carousel', 'interactive-cards', 'ninja-form', 'gravity-form', 'caldera-form', 'wisdom_registered_setting', 'twitter-feed', 'facebook-feed', 'data-table', 'filter-gallery' ];
+
+   $eael_default_settings  = array_fill_keys( $eael_default_keys, true );
+   $eael_get_settings      = get_option( 'eael_save_settings', $eael_default_settings );
+   $eael_new_settings      = array_diff_key( $eael_default_settings, $eael_get_settings );
+
+   if( ! empty( $eael_new_settings ) ) {
+      $eael_updated_settings = array_merge( $eael_get_settings, $eael_new_settings );
+      update_option( 'eael_save_settings', $eael_updated_settings );
+   }
+
+   return $eael_get_settings = get_option( 'eael_save_settings', $eael_default_settings );
+
+}
+
+/**
+ * Acivate or Deactivate Modules
+ *
+ * @since v1.0.0
+ */
 function add_eael_elements(){
 
    $eael_default_keys = [ 'contact-form-7', 'count-down', 'creative-btn', 'fancy-text', 'img-comparison', 'instagram-gallery', 'interactive-promo',  'lightbox', 'post-block', 'post-grid', 'post-timeline', 'product-grid', 'team-members', 'testimonial-slider', 'testimonials', 'testimonials', 'weforms', 'static-product', 'call-to-action', 'flip-box', 'info-box', 'dual-header', 'price-table', 'flip-carousel', 'interactive-cards', 'ninja-form', 'gravity-form', 'caldera-form', 'wisdom_registered_setting', 'twitter-feed', 'facebook-feed', 'data-table', 'filter-gallery' ];
    $eael_default_settings = array_fill_keys( $eael_default_keys, true );
 
-   $is_component_active = get_option( 'eael_save_settings', $eael_default_settings );
+   $is_component_active = eael_activated_modules();
+
    // load elements
    if( $is_component_active['post-grid'] ) {
       require_once ESSENTIAL_ADDONS_EL_PATH.'elements/post-grid/post-grid.php';
@@ -104,9 +132,13 @@ function add_eael_elements(){
 }
 add_action('elementor/widgets/widgets_registered','add_eael_elements');
 
-
+/**
+ * Load module's scripts and styles if any module is active.
+ *
+ * @since v1.0.0
+ */
 function essential_addons_el_enqueue(){
-   $is_component_active = get_option( 'eael_save_settings' );
+   $is_component_active = eael_activated_modules();
    wp_enqueue_style('essential_addons_elementor-css',ESSENTIAL_ADDONS_EL_URL.'assets/css/essential-addons-elementor.css');
 
    if( $is_component_active['fancy-text'] ) {
@@ -139,8 +171,9 @@ function essential_addons_el_enqueue(){
 add_action( 'wp_enqueue_scripts', 'essential_addons_el_enqueue' );
 
 
-// Editor CSS
-
+/**
+ * Editor Css
+ */
 add_action( 'elementor/editor/before_enqueue_scripts', function() {
 
    wp_register_style( 'essential_addons_elementor_editor-css', ESSENTIAL_ADDONS_EL_URL.'assets/css/essential-addons-editor.css');
@@ -148,8 +181,9 @@ add_action( 'elementor/editor/before_enqueue_scripts', function() {
 
 } );
 
-// Action menus
-
+/**
+ * Creates an Action Menu
+ */
 function eael_add_settings_link( $links ) {
     $settings_link = sprintf( '<a href="admin.php?page=eael-settings">' . __( 'Settings' ) . '</a>' );
     $go_pro_link = sprintf( '<a href="https://wpdeveloper.net/in/upgrade-essential-addons-elementor" target="_blank" style="color: #39b54a; font-weight: bold;">' . __( 'Go Pro' ) . '</a>' );
@@ -159,17 +193,21 @@ function eael_add_settings_link( $links ) {
 $plugin = plugin_basename( __FILE__ );
 add_filter( "plugin_action_links_$plugin", 'eael_add_settings_link' );
 
-
-
-// Redirect to options page
-
-register_activation_hook(__FILE__, 'eael_activate');
-add_action('admin_init', 'eael_redirect');
-
+/**
+ * Activation redirects
+ *
+ * @since v1.0.0
+ */
 function eael_activate() {
     add_option('eael_do_activation_redirect', true);
 }
+register_activation_hook(__FILE__, 'eael_activate');
 
+/**
+ * Redirect to options page
+ *
+ * @since v1.0.0
+ */
 function eael_redirect() {
     if (get_option('eael_do_activation_redirect', false)) {
         delete_option('eael_do_activation_redirect');
@@ -179,9 +217,13 @@ function eael_redirect() {
         }
     }
 }
+add_action('admin_init', 'eael_redirect');
 
-// Optional usage tracker
-
+/**
+ * Optional usage tracker
+ *
+ * @since v1.0.0
+ */
 if( ! class_exists( 'Eael_Plugin_Usage_Tracker') ) {
     require_once dirname( __FILE__ ) . '/includes/class-plugin-usage-tracker.php';
 }
@@ -199,29 +241,31 @@ if( ! function_exists( 'essential_addons_elementor_lite_start_plugin_tracking' )
     essential_addons_elementor_lite_start_plugin_tracking();
 }
 
-
-// admin notice
-
+/**
+ * Admin Notice
+ *
+ * @since v1.0.0
+ */
+function eael_admin_notice() {
+  if ( current_user_can( 'install_plugins' ) ) {
+    global $current_user ;
+    $user_id = $current_user->ID;
+    /* Check that the user hasn't already clicked to ignore the message */
+    if ( ! get_user_meta($user_id, 'eael_ignore_notice241') ) {
+      echo '<div class="eael-admin-notice updated"><p>';
+      echo '<img style="width: 15px; padding-right: 6px; float: left" src="' . plugins_url( '/', __FILE__ ).'admin/assets/images/gift-icon.png'. '" > ';
+      printf(__('Last chance to grab the <strong>New Year Deal!!</strong> Enjoy <strong>25&#37;</strong> discount on <strong>Essential Addons for Elementor Pro.</strong> Use the coupon <strong>"NewYear"</strong><a href="https://wpdeveloper.net/in/upgrade-essential-addons-elementor" target="_blank" style="text-decoration: none;"><span class="dashicons dashicons-tickets-alt" style="margin-top: -1px;margin-left: 10px;"></span> Grab the Deal</a>
+        <a href="%1$s" style="text-decoration: none; margin-left: 10px;"><span class="dashicons dashicons-dismiss"></span> No Thanks</a>'),  admin_url( 'admin.php?page=eael-settings&eael_nag_ignore=0' ));
+      echo "</p></div>";
+    }
+  }
+}
 add_action('admin_notices', 'eael_admin_notice');
 
-function eael_admin_notice() {
-if ( current_user_can( 'install_plugins' ) )
-   {
-  global $current_user ;
-        $user_id = $current_user->ID;
-        /* Check that the user hasn't already clicked to ignore the message */
-  if ( ! get_user_meta($user_id, 'eael_ignore_notice241') ) {
-        echo '<div class="eael-admin-notice updated"><p>';
-        echo '<img style="width: 15px; padding-right: 6px; float: left" src="' . plugins_url( '/', __FILE__ ).'admin/assets/images/gift-icon.png'. '" > ';
-        printf(__('Last chance to grab the <strong>New Year Deal!!</strong> Enjoy <strong>25&#37;</strong> discount on <strong>Essential Addons for Elementor Pro.</strong> Use the coupon <strong>"NewYear"</strong><a href="https://wpdeveloper.net/in/upgrade-essential-addons-elementor" target="_blank" style="text-decoration: none;"><span class="dashicons dashicons-tickets-alt" style="margin-top: -1px;margin-left: 10px;"></span> Grab the Deal</a>
-           <a href="%1$s" style="text-decoration: none; margin-left: 10px;"><span class="dashicons dashicons-dismiss"></span> No Thanks</a>'),  admin_url( 'admin.php?page=eael-settings&eael_nag_ignore=0' ));
-        echo "</p></div>";
-  }
-    }
-}
 
-add_action('admin_init', 'eael_nag_ignore');
-
+/**
+ * Nag Ignore
+ */
 function eael_nag_ignore() {
   global $current_user;
         $user_id = $current_user->ID;
@@ -230,3 +274,4 @@ function eael_nag_ignore() {
              add_user_meta($user_id, 'eael_ignore_notice241', 'true', true);
   }
 }
+add_action('admin_init', 'eael_nag_ignore');
