fe3d8848e43df71691201806f11f61ed4d0668b0
abdulhalimmanik <abdulhalimmanik@users.noreply.github.com>
2018-07-15 14:25:21 +0600

    Integrate swiper to content ticker

--- assets/js/eael-scripts.js
+++ assets/js/eael-scripts.js
@@ -303,6 +303,60 @@
         loadMore( options, settings );
     }
 
+    var ContentTicker = function ($scope, $) {
+        var $contentTicker              = $scope.find('.eael-content-ticker').eq(0),
+            $items                      = ($contentTicker.data("items") !== undefined) ? $contentTicker.data("items") : 3,
+            $items_tablet               = ($contentTicker.data("items-tablet") !== undefined) ? $contentTicker.data("items-tablet") : 3,
+            $items_mobile               = ($contentTicker.data("items-mobile") !== undefined) ? $contentTicker.data("items-mobile") : 3,
+            $margin                     = ($contentTicker.data("margin") !== undefined) ? $contentTicker.data("margin") : 10,
+            $margin_tablet              = ($contentTicker.data("margin-tablet") !== undefined) ? $contentTicker.data("margin-tablet") : 10,
+            $margin_mobile              = ($contentTicker.data("margin-mobile") !== undefined) ? $contentTicker.data("margin-mobile") : 10,
+            $effect                     = ($contentTicker.data("effect") !== undefined) ? $contentTicker.data("effect") : 'slide',
+            $speed                      = ($contentTicker.data("speed") !== undefined) ? $contentTicker.data("speed") : 400,
+            $autoplay                   = ($contentTicker.data("autoplay") !== undefined) ? $contentTicker.data("autoplay") : 999999,
+            $loop                       = ($contentTicker.data("loop") !== undefined) ? $contentTicker.data("loop") : 0,
+            $grab_cursor                = ($contentTicker.data("grab-cursor") !== undefined) ? $contentTicker.data("grab-cursor") : 0,
+            $pagination                 = ($contentTicker.data("pagination") !== undefined) ? $contentTicker.data("pagination") : '.swiper-pagination',
+            $arrows                     = ($contentTicker.data("arrows") !== undefined) ? $contentTicker.data("arrows") : false,
+            $arrow_next                 = ($contentTicker.data("arrow-next") !== undefined) ? $contentTicker.data("arrow-next") : '.swiper-button-next',
+            $arrow_prev                 = ($contentTicker.data("arrow-prev") !== undefined) ? $contentTicker.data("arrow-prev") : '.swiper-button-prev',
+            
+            mySwiper = new Swiper($contentTicker, {
+                direction:              'horizontal',
+                speed:                  $speed,
+                effect:                 $effect,
+                slidesPerView:          $items,
+                spaceBetween:           $margin,
+                grabCursor:             $grab_cursor,
+                paginationClickable:    true,
+                autoHeight:             true,
+                loop:                   $loop,
+                autoplay: {
+                    delay: $autoplay,
+                },
+                pagination: {
+                    el: $pagination,
+                    clickable: true,
+                },
+                navigation: {
+                    nextEl: $arrow_next,
+                    prevEl: $arrow_prev,
+                },
+                breakpoints: {
+                    // when window width is <= 480px
+                    480: {
+                        slidesPerView:  $items_mobile,
+                        spaceBetween:   $margin_mobile
+                    },
+                    // when window width is <= 640px
+                    768: {
+                        slidesPerView:  $items_tablet,
+                        spaceBetween:   $margin_tablet
+                    }
+                }
+            });
+    };
+
     $(window).on('elementor/frontend/init', function () {
         elementorFrontend.hooks.addAction('frontend/element_ready/eael-filterable-gallery.default', FilterGallery);
         elementorFrontend.hooks.addAction('frontend/element_ready/eael-adv-tabs.default', AdvanceTabHandler);
@@ -310,5 +364,6 @@
         elementorFrontend.hooks.addAction('frontend/element_ready/eael-filterable-gallery.default', FilterGallery);
         elementorFrontend.hooks.addAction('frontend/element_ready/eael-facebook-feed.default', FacebookFeedHandler);
         elementorFrontend.hooks.addAction('frontend/element_ready/eael-twitter-feed.default', TwitterFeedHandler);
+        elementorFrontend.hooks.addAction('frontend/element_ready/eael-content-ticker.default', ContentTicker);
     });
 }(jQuery));
