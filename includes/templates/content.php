e86b0594aec4f2a0d59b3a12ed7a294642aa7c37
Nazmul Hasan Rupok <re.enter.rupok@gmail.com>
2018-07-18 11:37:08 +0600

    Content ticker updated

--- includes/templates/content.php
+++ includes/templates/content.php
@@ -7,7 +7,9 @@ if( isset( $post_args['post_style'] ) ) :
     if( $post_args['post_style'] == 'ticker' ) : 
 ?>
     <div class="swiper-slide">
-        <a href="<?php the_permalink(); ?>" class="ticker-content"><?php the_title(); ?></a>
+        <div class="ticker-content">
+            <a href="<?php the_permalink(); ?>" class="ticker-content-link"><?php the_title(); ?></a>
+        </div>
     </div>
 <?php 
     endif;
