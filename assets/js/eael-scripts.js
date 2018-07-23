8dcd3623f8640e3b791a21a9bf95711842b5a506
abdulhalimmanik <abdulhalimmanik@users.noreply.github.com>
2018-07-23 11:43:55 +0600

    Image accordion script updated

--- assets/js/eael-scripts.js
+++ assets/js/eael-scripts.js
@@ -420,6 +420,29 @@
         }
     }
 
+    var ImageAccordion = function ($scope, $) {
+        var $imageAccordion         = $scope.find('.eael-img-accordion').eq(0),
+            $id                     = ($imageAccordion.data("img-accordion-id") !== undefined) ? $imageAccordion.data("img-accordion-id") : '',
+            $type             = ($imageAccordion.data("img-accordion-type")  !== undefined) ? $imageAccordion.data("img-accordion-type") : '';
+           
+
+        if( 'on-click' === $type ) {
+            $('#eael-img-accordion-'+ $id +' a').on('click', function(e) {
+                e.preventDefault();
+                $('#eael-img-accordion-'+ $id +' a').css('flex', '1');
+                $(this).find('.overlay').parent('a').addClass('overlay-active');
+                $('#eael-img-accordion-'+ $id +' a').find('.overlay-inner').removeClass('overlay-inner-show');
+                $(this).find('.overlay-inner').addClass('overlay-inner-show');
+                $(this).css('flex', '3');
+            });
+            $('#eael-img-accordion-'+ $id +' a').on('blur', function(e) {
+                $('#eael-img-accordion-'+ $id +' a').css('flex', '1');
+                $('#eael-img-accordion-'+ $id +' a').find('.overlay-inner').removeClass('overlay-inner-show');
+                $(this).find('.overlay').parent('a').removeClass('overlay-active');
+            });
+        }
+    }
+
     $(window).on('elementor/frontend/init', function () {
         elementorFrontend.hooks.addAction('frontend/element_ready/eael-filterable-gallery.default', FilterGallery);
         elementorFrontend.hooks.addAction('frontend/element_ready/eael-adv-tabs.default', AdvanceTabHandler);
@@ -429,6 +452,7 @@
         elementorFrontend.hooks.addAction('frontend/element_ready/eael-content-ticker.default', ContentTicker);
         elementorFrontend.hooks.addAction('frontend/element_ready/eael-data-table.default', dataTable);
         elementorFrontend.hooks.addAction('frontend/element_ready/eael-fancy-text.default', FancyText);
+        elementorFrontend.hooks.addAction('frontend/element_ready/eael-image-accordion.default', ImageAccordion);
     });
 
 }(jQuery));
