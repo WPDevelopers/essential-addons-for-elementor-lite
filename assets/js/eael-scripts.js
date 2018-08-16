49f46eade9b0602188567f7c8995338ae547e679
Mahfuz <m.mahfuz.me@gmail.com>
2018-08-16 14:18:12 +0600

    pricing table tooltip added

--- assets/js/eael-scripts.js
+++ assets/js/eael-scripts.js
@@ -495,10 +495,37 @@
         });
 
     }
+
+    var PricingTooltip = function($scope, $) {
+		if( $.fn.tooltipster ) {
+			var $tooltip = $scope.find('.tooltip'), i;
+
+			for( i = 0; i < $tooltip.length; i++) {
+				var $currentTooltip = $( '#' + $($tooltip[i]).attr('id') ),
+					$tooltipSide	= ( $currentTooltip.data('side') !== undefined ) ? $currentTooltip.data('side') : false,
+					$tooltipTrigger	= ( $currentTooltip.data('trigger') !== undefined ) ? $currentTooltip.data('trigger') : 'hover',
+					$animation		= ( $currentTooltip.data('animation') !== undefined ) ? $currentTooltip.data('animation') : 'fade',
+					$anim_duration	= ( $currentTooltip.data('animation_duration') !== undefined ) ? $currentTooltip.data('animation_duration') : 300,
+					$theme 			= ( $currentTooltip.data('theme') !== undefined ) ? $currentTooltip.data('theme') : 'default',
+					$arrow			= ( 'yes' == $currentTooltip.data('arrow') ) ? true : false;
+
+				$currentTooltip.tooltipster({
+					animation: $animation,
+					trigger: $tooltipTrigger,
+					side: $tooltipSide,
+					delay: $anim_duration,
+					arrow: $arrow,
+					theme: 'tooltipster-' + $theme
+				});
+
+			}
+		}
+	}
     
     $(window).on('elementor/frontend/init', function () {
         elementorFrontend.hooks.addAction('frontend/element_ready/eael-filterable-gallery.default', FilterGallery);
         elementorFrontend.hooks.addAction('frontend/element_ready/eael-adv-tabs.default', AdvanceTabHandler);
+        elementorFrontend.hooks.addAction('frontend/element_ready/eael-pricing-table.default', PricingTooltip);
         elementorFrontend.hooks.addAction('frontend/element_ready/eael-post-timeline.default', postTimelineHandler);
         elementorFrontend.hooks.addAction('frontend/element_ready/eael-facebook-feed.default', FacebookFeedHandler);
         elementorFrontend.hooks.addAction('frontend/element_ready/eael-twitter-feed.default', TwitterFeedHandler);
