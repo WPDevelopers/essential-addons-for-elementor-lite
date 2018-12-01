3f20cde0586cac6bfa2ae2e151eae8d75a85af4d
Mahfuz <m.mahfuz.me@gmail.com>
2018-12-01 16:04:26 +0600

    version rollback is ready for test

--- admin/assets/js/rollback-admin.js
+++ admin/assets/js/rollback-admin.js
@@ -1,26 +1,24 @@
 (function($) {
 
-
-    // have to change all selectors, objects, properties.
-    $( '.eael-version-rollback' ).on( 'click', function( event ) {
-        event.preventDefault();
-    
-        var $this = $( this ),
-            dialogsManager = new DialogsManager.Instance();
-    
-        dialogsManager.createWidget( 'confirm', {
-            headerMessage: EAELRollBackConfirm.i18n.rollback_to_previous_version,
-            message: EAELRollBackConfirm.i18n.rollback_confirm,
-            strings: {
-                cancel: EAELRollBackConfirm.i18n.cancel,
-                confirm: EAELRollBackConfirm.i18n.yes,
-            },
-            onConfirm: function() {
-                $this.addClass( 'loading' );
-    
-                location.href = $this.attr( 'href' );
-            }
-        } ).show();
-    } );
+    $(document).ready(function() {
+        $('.eael-version-rollback').on( 'click', function(e) {
+            e.preventDefault();
+            var $this          = $( this ),
+                dialogsManager = new DialogsManager.Instance();
+        
+            dialogsManager.createWidget( 'confirm', {
+                headerMessage: EAELRollBackConfirm.i18n.rollback_to_previous_version,
+                message      : EAELRollBackConfirm.i18n.rollback_confirm,
+                strings      : {
+                    cancel : EAELRollBackConfirm.i18n.cancel,
+                    confirm: EAELRollBackConfirm.i18n.yes,
+                },
+                onConfirm: function() {
+                    $this.addClass( 'loading' );
+                    location.href = $this.attr( 'href' );
+                }
+            } ).show();
+        } );
+    });
 
 })(jQuery);
