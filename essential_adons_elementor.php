b182ef472fa1a737079aa3e0763028dd70beab8e
Rupok <re.enter.rupok@gmail.com>
2019-01-25 13:34:20 +0600

    update to v2.9.2

--- essential_adons_elementor.php
+++ essential_adons_elementor.php
@@ -4,7 +4,7 @@
  * Description: The ultimate elements library for Elementor page builder plugin for WordPress.
  * Plugin URI: https://essential-addons.com/elementor/
  * Author: WPDeveloper
- * Version: 2.9.1
+ * Version: 2.9.2
  * Author URI: https://wpdeveloper.net/
  *
  * Text Domain: essential-addons-elementor
