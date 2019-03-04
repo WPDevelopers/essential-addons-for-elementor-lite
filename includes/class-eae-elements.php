e0397967547535fb666ea0331784a6a88ad15e6a
Mahfuz <m.mahfuz.me@gmail.com>
2019-03-04 15:14:03 +0600

    requiring elements in new way

--- includes/class-eae-elements.php
+++ includes/class-eae-elements.php
@@ -29,6 +29,15 @@ if( ! class_exists('Essential_Addons_Elements') ) {
             add_action('elementor/widgets/widgets_registered', array($this, 'add_eael_elements'));
         }
 
+        public function elements_path( $file ) {
+            $file = ltrim( $file, '/' );
+            $file = ESSENTIAL_ADDONS_EL_PATH.'elements/'.$file.'/'.$file.'.php';
+            if(file_exists($file)) {
+                return $file;
+            }
+            return false;
+        }
+
         /**
          * Acivate or Deactivate Modules
          *
@@ -36,103 +45,93 @@ if( ! class_exists('Essential_Addons_Elements') ) {
         */
         function add_eael_elements() {
 
+            $elements = [
+                [ 'name'  => 'post-grid' ],
+                [ 'name'  => 'post-timeline' ],
+                [ 'name'  => 'fancy-text' ],
+                [ 'name'  => 'creative-btn' ],
+                [ 'name'  => 'count-down' ],
+                [ 'name'  => 'team-members' ],
+                [ 'name'  => 'testimonials' ],
+                [ 'name'  => 'info-box' ],
+                [ 'name'  => 'flip-box' ],
+                [ 'name'  => 'call-to-action' ],
+                [ 'name'  => 'dual-header' ],
+                [ 'name'  => 'price-table' ],
+                [ 'name'  => 'twitter-feed' ],
+                [ 'name'  => 'data-table' ],
+                [ 'name'  => 'filter-gallery' ],
+                [ 'name'  => 'image-accordion' ],
+                [ 'name'  => 'content-ticker' ],
+                [ 'name'  => 'tooltip' ],
+                [ 'name'  => 'adv-accordion' ],
+                [ 'name'  => 'adv-tabs' ],
+                [ 'name'  => 'progress-bar' ],
+                [ 'name'  => 'feature-list' ],
+                [
+                    'name'      => 'product-grid',
+                    'condition' => [
+                        'function_exists',
+                        'WC'
+                    ]
+                ],
+                [
+                    'name'      => 'contact-form-7',
+                    'condition' => [
+                        'function_exists',
+                        'wpcf7'
+                    ]
+                ],
+                [
+                    'name'      => 'weforms',
+                    'condition' => [
+                        'function_exists',
+                        'WeForms'
+                    ]
+                ],
+                [
+                    'name'      => 'ninja-form',
+                    'condition' => [
+                        'function_exists',
+                        'Ninja_Forms'
+                    ]
+                ],
+                [
+                    'name'      => 'gravity-form',
+                    'condition' => [
+                        'class_exists',
+                        'GFForms'
+                    ]
+                ],
+                [
+                    'name'      => 'caldera-form',
+                    'condition' => [
+                        'class_exists',
+                        'Caldera_Forms'
+                    ]
+                ],
+                [
+                    'name'      => 'wpforms',
+                    'condition' => [
+                        'class_exists',
+                        '\WPForms\WPForms'
+                    ]
+                ]
+            ];
+
             $is_component_active = Essential_Addons_EL::eael_activated_modules();
-
-            // load elements
-            if( $is_component_active['post-grid'] ) {
-            require_once ESSENTIAL_ADDONS_EL_PATH.'elements/post-grid/post-grid.php';
-            }
-            if( $is_component_active['post-timeline'] ) {
-            require_once ESSENTIAL_ADDONS_EL_PATH.'elements/post-timeline/post-timeline.php';
-            }
-            if( $is_component_active['fancy-text'] ) {
-            require_once ESSENTIAL_ADDONS_EL_PATH.'elements/fancy-text/fancy-text.php';
-            }
-            if( $is_component_active['creative-btn'] ) {
-            require_once ESSENTIAL_ADDONS_EL_PATH.'elements/creative-button/creative-button.php';
-            }
-            if( $is_component_active['count-down'] ) {
-            require_once ESSENTIAL_ADDONS_EL_PATH.'elements/countdown/countdown.php';
-            }
-            if( $is_component_active['team-members'] ) {
-            require_once ESSENTIAL_ADDONS_EL_PATH.'elements/team-members/team-members.php';
-            }
-            if( $is_component_active['testimonials'] ) {
-            require_once ESSENTIAL_ADDONS_EL_PATH.'elements/testimonials/testimonials.php';
-            }
-
-            if ( function_exists( 'WC' ) && $is_component_active['product-grid'] ) {
-            require_once ESSENTIAL_ADDONS_EL_PATH.'elements/product-grid/product-grid.php';
-            }
-
-            if ( function_exists( 'wpcf7' ) && $is_component_active['contact-form-7'] ) {
-            require_once ESSENTIAL_ADDONS_EL_PATH.'elements/contact-form-7/contact-form-7.php';
-            }
-
-            if ( function_exists( 'WeForms' ) && $is_component_active['weforms'] ) {
-            require_once ESSENTIAL_ADDONS_EL_PATH.'elements/weforms/weforms.php';
-            }
-
-            if( $is_component_active['info-box'] ) {
-            require_once ESSENTIAL_ADDONS_EL_PATH.'elements/infobox/infobox.php';
-            }
-
-            if( $is_component_active['flip-box'] ) {
-            require_once ESSENTIAL_ADDONS_EL_PATH.'elements/flipbox/flipbox.php';
-            }
-
-            if( $is_component_active['call-to-action'] ) {
-            require_once ESSENTIAL_ADDONS_EL_PATH.'elements/call-to-action/call-to-action.php';
-            }
-
-            if( $is_component_active['dual-header'] ) {
-            require_once ESSENTIAL_ADDONS_EL_PATH.'elements/dual-color-header/dual-color-header.php';
-            }
-            if( $is_component_active['price-table'] ) {
-            require_once ESSENTIAL_ADDONS_EL_PATH.'elements/pricing-table/pricing-table.php';
-            }
-            if( function_exists( 'Ninja_Forms' ) && $is_component_active['ninja-form'] ) {
-            require_once ESSENTIAL_ADDONS_EL_PATH.'elements/ninja-form/ninja-form.php';
-            }
-            if( class_exists( 'GFForms' ) && $is_component_active['gravity-form'] ) {
-            require_once ESSENTIAL_ADDONS_EL_PATH.'elements/gravity-form/gravity-form.php';
-            }
-            if( class_exists( 'Caldera_Forms' ) && $is_component_active['caldera-form'] ) {
-            require_once ESSENTIAL_ADDONS_EL_PATH.'elements/caldera-forms/caldera-forms.php';
-            }
-            if( class_exists( '\WPForms\WPForms' ) && $is_component_active['wpforms'] ) {
-            require_once ESSENTIAL_ADDONS_EL_PATH.'elements/wpforms/wpforms.php';
-            }
-            if( $is_component_active['twitter-feed'] ) {
-            require_once ESSENTIAL_ADDONS_EL_PATH.'elements/twitter-feed/twitter-feed.php';
-            }
-
-            if( $is_component_active['data-table'] ) {
-            require_once ESSENTIAL_ADDONS_EL_PATH.'elements/data-table/data-table.php';
-            }
-            if( $is_component_active['filter-gallery'] ) {
-            require_once ESSENTIAL_ADDONS_EL_PATH.'elements/filterable-gallery/filterable-gallery.php';
-            }
-            if( $is_component_active['image-accordion'] ) {
-            require_once ESSENTIAL_ADDONS_EL_PATH.'elements/image-accordion/image-accordion.php';
-            }
-            if( $is_component_active['content-ticker'] ) {
-            require_once ESSENTIAL_ADDONS_EL_PATH.'elements/content-ticker/content-ticker.php';
-            }
-            if( $is_component_active['tooltip'] ) {
-            require_once ESSENTIAL_ADDONS_EL_PATH.'elements/tooltip/tooltip.php';
-            }
-            if( $is_component_active['adv-accordion'] ) {
-            require_once ESSENTIAL_ADDONS_EL_PATH.'elements/advance-accordion/advance-accordion.php';
-            }
-            if( $is_component_active['adv-tabs'] ) {
-            require_once ESSENTIAL_ADDONS_EL_PATH.'elements/advance-tabs/advance-tabs.php';
-            }
-            if( $is_component_active['progress-bar'] ) {
-            require_once ESSENTIAL_ADDONS_EL_PATH.'elements/progress-bar/progress-bar.php';
-            }
-            if( $is_component_active['feature-list'] ) {
-            require_once ESSENTIAL_ADDONS_EL_PATH.'elements/feature-list/feature-list.php';
+            $ea_elements = apply_filters( 'add_eae_element', $elements );
+            
+            foreach($ea_elements as $element) {
+                if( isset($element['condition']) ) {
+                    if( ($element['condition'][0]($element['condition'][1])) && $is_component_active[$element['name']] ) {
+                        require_once $this->elements_path($element['name']);
+                    }
+                }else {
+                    if($is_component_active[$element['name']]) {
+                        require_once $this->elements_path($element['name']);
+                    }
+                }
             }
         }
 
