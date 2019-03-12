3119951beb945404c5d9f64d18674eefbfe9d49f
Sabuz <naz.sabuz@gmail.com>
2019-03-12 14:12:29 +0600

    Updated: Image Accordion visibility rules

--- assets/js/eael-scripts.js
+++ assets/js/eael-scripts.js
@@ -488,23 +488,25 @@
     }
 
     var ImageAccordion = function ($scope, $) {
-        var $imageAccordion         = $scope.find('.eael-img-accordion').eq(0),
-            $id                     = ($imageAccordion.data("img-accordion-id") !== undefined) ? $imageAccordion.data("img-accordion-id") : '',
-            $type             = ($imageAccordion.data("img-accordion-type")  !== undefined) ? $imageAccordion.data("img-accordion-type") : '';
-           
+        var $imageAccordion = $scope.find('.eael-img-accordion').eq(0),
+            $id = ($imageAccordion.data("img-accordion-id") !== undefined) ? $imageAccordion.data("img-accordion-id") : '',
+            $type = ($imageAccordion.data("img-accordion-type") !== undefined) ? $imageAccordion.data("img-accordion-type") : '';
 
-        if( 'on-click' === $type ) {
-            $('#eael-img-accordion-'+ $id +' a').on('click', function(e) {
-                e.preventDefault();
-                $('#eael-img-accordion-'+ $id +' a').css('flex', '1');
+        if ('on-click' === $type) {
+            $('#eael-img-accordion-' + $id + ' a').on('click', function (e) {
+                if ($(this).hasClass('overlay-active') == false) {
+                    e.preventDefault();
+                }
+
+                $('#eael-img-accordion-' + $id + ' a').css('flex', '1');
                 $(this).find('.overlay').parent('a').addClass('overlay-active');
-                $('#eael-img-accordion-'+ $id +' a').find('.overlay-inner').removeClass('overlay-inner-show');
+                $('#eael-img-accordion-' + $id + ' a').find('.overlay-inner').removeClass('overlay-inner-show');
                 $(this).find('.overlay-inner').addClass('overlay-inner-show');
                 $(this).css('flex', '3');
             });
-            $('#eael-img-accordion-'+ $id +' a').on('blur', function(e) {
-                $('#eael-img-accordion-'+ $id +' a').css('flex', '1');
-                $('#eael-img-accordion-'+ $id +' a').find('.overlay-inner').removeClass('overlay-inner-show');
+            $('#eael-img-accordion-' + $id + ' a').on('blur', function (e) {
+                $('#eael-img-accordion-' + $id + ' a').css('flex', '1');
+                $('#eael-img-accordion-' + $id + ' a').find('.overlay-inner').removeClass('overlay-inner-show');
                 $(this).find('.overlay').parent('a').removeClass('overlay-active');
             });
         }
