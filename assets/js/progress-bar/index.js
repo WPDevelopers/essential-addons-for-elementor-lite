e7e0f3769751a577497df61706e2edc172c307a7
Sabuz <naz.sabuz@gmail.com>
2019-03-21 12:56:19 +0600

    Updated: js scripts

--- assets/js/progress-bar/index.js
+++ assets/js/progress-bar/index.js
@@ -1,4 +1,9 @@
-var ProgressBar = function ($scope, $) {
-    $('.eael-progressbar', $scope).eaelProgressBar()
+var ProgressBar = function($scope, $) {
+    $(".eael-progressbar", $scope).eaelProgressBar();
 };
-elementorFrontend.hooks.addAction('frontend/element_ready/eael-progress-bar.default', ProgressBar);
+jQuery(window).on("elementor/frontend/init", function() {
+    elementorFrontend.hooks.addAction(
+        "frontend/element_ready/eael-progress-bar.default",
+        ProgressBar
+    );
+});
