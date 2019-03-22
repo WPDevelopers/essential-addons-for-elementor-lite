701a9763e8fc007e375cb254c65bed909b7fd7a7
Sabuz <naz.sabuz@gmail.com>
2019-03-22 22:47:12 +0600

    structure updated

--- elements/post-grid/post-grid.php
+++ elements/post-grid/post-grid.php
@@ -417,7 +417,7 @@ class Widget_Eael_Post_Grid extends Widget_Base {
 		/**
 		 * Get posts from database.
 		 */
-		$posts = eael_load_more_ajax( $query_args );
+		$posts = $this->eael_load_more_ajax( $query_args );
 		/**
 		 * Set total posts.
 		 */
