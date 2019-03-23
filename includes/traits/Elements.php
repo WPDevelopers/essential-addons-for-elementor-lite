6b0c3868ddf07562162348c79f01502262104d2e
Sabuz <naz.sabuz@gmail.com>
2019-03-23 16:39:42 +0600

    Update Elements.php

--- includes/traits/Elements.php
+++ includes/traits/Elements.php
@@ -45,96 +45,96 @@ trait Elements
 
         $elements = [
             [
-                'name'  => 'post-grid',
-                'class' => 'Eael_Adv_Accordion'
+                'name' => 'post-grid',
+                'class' => 'Eael_Adv_Accordion',
             ],
             [
                 'name' => 'post-timeline',
-                'class' => 'Eael_Post_Timeline'
+                'class' => 'Eael_Post_Timeline',
             ],
             [
                 'name' => 'fancy-text',
-                'class' => 'Eael_Fancy_Text'
+                'class' => 'Eael_Fancy_Text',
             ],
             [
                 'name' => 'creative-btn',
-                'class' => 'Eael_Creative_Button'
+                'class' => 'Eael_Creative_Button',
             ],
             [
                 'name' => 'count-down',
-                'class' => 'Eael_Countdown'
+                'class' => 'Eael_Countdown',
             ],
             [
                 'name' => 'team-members',
-                'class' => 'Eael_Team_Member'
+                'class' => 'Eael_Team_Member',
             ],
             [
                 'name' => 'testimonials',
-                'class' => 'Eael_Testimonial'
+                'class' => 'Eael_Testimonial',
             ],
             [
                 'name' => 'info-box',
-                'class' => 'Eael_Info_Box'
+                'class' => 'Eael_Info_Box',
             ],
             [
                 'name' => 'flip-box',
-                'class' => 'Eael_Flip_Box'
+                'class' => 'Eael_Flip_Box',
             ],
             [
                 'name' => 'call-to-action',
-                'class' => 'Eael_Cta_Box'
+                'class' => 'Eael_Cta_Box',
             ],
             [
                 'name' => 'dual-header',
-                'class' => 'Eael_Dual_Color_Header'
+                'class' => 'Eael_Dual_Color_Header',
             ],
             [
                 'name' => 'price-table',
-                'class' => 'Eael_Pricing_Table'
+                'class' => 'Eael_Pricing_Table',
             ],
             [
                 'name' => 'twitter-feed',
-                'class' => 'Eael_Twitter_Feed'
+                'class' => 'Eael_Twitter_Feed',
             ],
             [
                 'name' => 'data-table',
-                'class' => 'Eael_Data_Table'
+                'class' => 'Eael_Data_Table',
             ],
             [
                 'name' => 'filter-gallery',
-                'class' => 'Eael_Filterable_Gallery'
+                'class' => 'Eael_Filterable_Gallery',
             ],
             [
                 'name' => 'image-accordion',
-                'class' => 'Eael_Image_Accordion'
+                'class' => 'Eael_Image_Accordion',
             ],
             [
                 'name' => 'content-ticker',
-                'class' => 'Eael_Content_Tickersss'
+                'class' => 'Eael_Content_Tickersss',
             ],
             [
                 'name' => 'tooltip',
-                'class' => 'Eael_Tooltip'
+                'class' => 'Eael_Tooltip',
             ],
             [
                 'name' => 'adv-accordion',
-                'class' => 'Eael_Adv_Accordion'
+                'class' => 'Eael_Adv_Accordion',
             ],
             [
                 'name' => 'adv-tabs',
-                'class' => 'Eael_Adv_Tabs'
+                'class' => 'Eael_Adv_Tabs',
             ],
             [
                 'name' => 'progress-bar',
-                'class' => 'Eael_Progress_Bar'
+                'class' => 'Eael_Progress_Bar',
             ],
             [
                 'name' => 'feature-list',
-                'class' => 'Eael_Feature_List'
+                'class' => 'Eael_Feature_List',
             ],
             [
-                'name'      => 'product-grid',
-                'class'     => 'Eael_Product_Grid',
+                'name' => 'product-grid',
+                'class' => 'Eael_Product_Grid',
                 'condition' => [
                     'function_exists',
                     'WC',
@@ -142,47 +142,47 @@ trait Elements
             ],
             [
                 'name' => 'contact-form-7',
-                'class'     => 'Eael_Contact_Form_7',
+                'class' => 'Eael_Contact_Form_7',
                 'condition' => [
                     'function_exists',
                     'wpcf7',
                 ],
             ],
             [
-                'name'      => 'weforms',
-                'class'     => 'Eael_WeForms',
+                'name' => 'weforms',
+                'class' => 'Eael_WeForms',
                 'condition' => [
                     'function_exists',
                     'WeForms',
                 ],
             ],
             [
-                'name'      => 'ninja-form',
-                'class'     => 'Eael_NinjaForms',
+                'name' => 'ninja-form',
+                'class' => 'Eael_NinjaForms',
                 'condition' => [
                     'function_exists',
                     'Ninja_Forms',
                 ],
             ],
             [
-                'name'      => 'gravity-form',
-                'class'     => 'Eael_GravityForms',
+                'name' => 'gravity-form',
+                'class' => 'Eael_GravityForms',
                 'condition' => [
                     'class_exists',
                     'GFForms',
                 ],
             ],
             [
-                'name'      => 'caldera-form',
-                'class'     => 'Eael_Caldera_Forms',
+                'name' => 'caldera-form',
+                'class' => 'Eael_Caldera_Forms',
                 'condition' => [
                     'class_exists',
                     'Caldera_Forms',
                 ],
             ],
             [
-                'name'      => 'wpforms',
-                'class'     => 'Eael_WpForms',
+                'name' => 'wpforms',
+                'class' => 'Eael_WpForms',
                 'condition' => [
                     'class_exists',
                     '\WPForms\WPForms',
@@ -196,11 +196,13 @@ trait Elements
         foreach ($ea_elements as $element) {
             if (isset($element['condition'])) {
                 if (($element['condition'][0]($element['condition'][1])) && $is_component_active[$element['name']]) {
-                    require_once $this->elements_path($element['name']);
+                    $element_class = '\EssentialAddonsElementor\Elements\\' . $element['class'];
+                    $widgets_manager->register_widget_type(new $element_class);
                 }
             } else {
                 if ($is_component_active[$element['name']]) {
-                    require_once $this->elements_path($element['name']);
+                    $element_class = '\EssentialAddonsElementor\Elements\\' . $element['class'];
+                    $widgets_manager->register_widget_type(new $element_class);
                 }
             }
         }
