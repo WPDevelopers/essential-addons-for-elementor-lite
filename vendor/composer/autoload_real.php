4c55b563175354ccb3486182bffaf283af9ceafc
Mahfuz <m.mahfuz.me@gmail.com>
2019-03-24 12:37:51 +0600

    js contact issue fixed

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -36,7 +36,7 @@ class ComposerAutoloaderInite6e2cbae70c835106a2f6367baecac5f
         }
 
         $loader->setClassMapAuthoritative(true);
-        $loader->setApcuPrefix('WeerzRfPqZT1IvY9l/UxJ');
+        $loader->setApcuPrefix('yN6/ONnxgDc8PO5PRSHFh');
         $loader->register(true);
 
         return $loader;
