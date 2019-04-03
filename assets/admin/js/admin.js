431e47ead4bb39ffb4b99f2d4320f0398c41cdc2
Mahfuz <m.mahfuz.me@gmail.com>
2019-04-03 15:46:08 +0600

    generator spinner added

--- assets/admin/js/admin.js
+++ assets/admin/js/admin.js
@@ -81,6 +81,8 @@
 
     $('#eael-regenerate-files').on('click', function(e) {
         e.preventDefault();
+        var _this = $(this);
+
         $.ajax({
             url: localize.ajaxurl,
             type: "post",
@@ -88,11 +90,23 @@
                 action: "regenerate_files_with_ajax",
                 security: localize.nonce,
             },
-            success: function(response) {
-                console.log(response);
+            beforeSend: function () {
+                _this.html(
+                    '<svg id="eael-spinner" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 48 48"><circle cx="24" cy="4" r="4" fill="#fff"/><circle cx="12.19" cy="7.86" r="3.7" fill="#fffbf2"/><circle cx="5.02" cy="17.68" r="3.4" fill="#fef7e4"/><circle cx="5.02" cy="30.32" r="3.1" fill="#fef3d7"/><circle cx="12.19" cy="40.14" r="2.8" fill="#feefc9"/><circle cx="24" cy="44" r="2.5" fill="#feebbc"/><circle cx="35.81" cy="40.14" r="2.2" fill="#fde7af"/><circle cx="42.98" cy="30.32" r="1.9" fill="#fde3a1"/><circle cx="42.98" cy="17.68" r="1.6" fill="#fddf94"/><circle cx="35.81" cy="7.86" r="1.3" fill="#fcdb86"/></svg><span>Generating..</span>'
+                );
             },
-            error: function(response) {
-                console.log(response);
+            success: function(response) {
+                setTimeout(function () {
+                    _this.html("Regenerate Files");
+                    swal(
+                        "Files Generated!",
+                        "Click OK to continue",
+                        "success"
+                    );
+                }, 2000);
+            },
+            error: function () {
+                swal("Oops...", "Something went wrong!", "error");
             }
         });
 
