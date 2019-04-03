2ef7d8bb07e7de70e799b651534fb736dabb2f7b
Sabuz <naz.sabuz@gmail.com>
2019-04-03 18:00:18 +0600

    Update admin.js

--- assets/admin/js/admin.js
+++ assets/admin/js/admin.js
@@ -47,8 +47,8 @@
                 url: localize.ajaxurl,
                 type: "post",
                 data: {
-					action: "save_settings_with_ajax",
-					security: localize.nonce,
+                    action: "save_settings_with_ajax",
+                    security: localize.nonce,
                     fields: $("form#eael-settings").serialize()
                 },
                 beforeSend: function() {
@@ -79,7 +79,8 @@
         }
     });
 
-    $('#eael-regenerate-files').on('click', function(e) {
+    // Clear cache files
+    $("#eael-regenerate-files").on("click", function(e) {
         e.preventDefault();
         var _this = $(this);
 
@@ -87,29 +88,23 @@
             url: localize.ajaxurl,
             type: "post",
             data: {
-                action: "regenerate_files_with_ajax",
-                security: localize.nonce,
+                action: "clear_cache_files_with_ajax",
+                security: localize.nonce
             },
-            beforeSend: function () {
+            beforeSend: function() {
                 _this.html(
-                    '<svg id="eael-spinner" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 48 48"><circle cx="24" cy="4" r="4" fill="#fff"/><circle cx="12.19" cy="7.86" r="3.7" fill="#fffbf2"/><circle cx="5.02" cy="17.68" r="3.4" fill="#fef7e4"/><circle cx="5.02" cy="30.32" r="3.1" fill="#fef3d7"/><circle cx="12.19" cy="40.14" r="2.8" fill="#feefc9"/><circle cx="24" cy="44" r="2.5" fill="#feebbc"/><circle cx="35.81" cy="40.14" r="2.2" fill="#fde7af"/><circle cx="42.98" cy="30.32" r="1.9" fill="#fde3a1"/><circle cx="42.98" cy="17.68" r="1.6" fill="#fddf94"/><circle cx="35.81" cy="7.86" r="1.3" fill="#fcdb86"/></svg><span>Clearing..</span>'
+                    '<svg id="eael-spinner" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 48 48"><circle cx="24" cy="4" r="4" fill="#fff"/><circle cx="12.19" cy="7.86" r="3.7" fill="#fffbf2"/><circle cx="5.02" cy="17.68" r="3.4" fill="#fef7e4"/><circle cx="5.02" cy="30.32" r="3.1" fill="#fef3d7"/><circle cx="12.19" cy="40.14" r="2.8" fill="#feefc9"/><circle cx="24" cy="44" r="2.5" fill="#feebbc"/><circle cx="35.81" cy="40.14" r="2.2" fill="#fde7af"/><circle cx="42.98" cy="30.32" r="1.9" fill="#fde3a1"/><circle cx="42.98" cy="17.68" r="1.6" fill="#fddf94"/><circle cx="35.81" cy="7.86" r="1.3" fill="#fcdb86"/></svg><span>Clearing...</span>'
                 );
             },
             success: function(response) {
-                setTimeout(function () {
+                setTimeout(function() {
                     _this.html("Clear Cache");
-                    swal(
-                        "Cache Cleared!",
-                        "Click OK to continue",
-                        "success"
-                    );
-                }, 500);
+                    swal("Cache Cleared!", "Click OK to continue", "success");
+                }, 1000);
             },
-            error: function () {
-                swal("Oops...", "Something went wrong!", "error");
+            error: function() {
+                swal("Ops!", "Something went wrong!", "error");
             }
         });
-
     });
-
 })(jQuery);
