80fea0330fd8b07432d17acab9082b428a0935ee
Sabuz <naz.sabuz@gmail.com>
2019-05-19 13:07:03 +0600

    Fixed: Fatal error, Remove bootstrap access from world

--- includes/Elements/Dual_Color_Header.php
+++ includes/Elements/Dual_Color_Header.php
@@ -145,7 +145,7 @@ class Dual_Color_Header extends Widget_Base {
 
 		$this->end_controls_section();
 
-		if(!Bootstrap::pro_enabled()) {
+		if(!apply_filters('eael/pro_enabled', false)) {
 			$this->start_controls_section(
 				'eael_section_pro',
 				[
--- includes/Elements/Filterable_Gallery.php
+++ includes/Elements/Filterable_Gallery.php
@@ -549,7 +549,7 @@ class Filterable_Gallery extends Widget_Base
 
         $this->end_controls_section();
 
-        if(!Bootstrap::pro_enabled()) {
+        if(!apply_filters('eael/pro_enabled', false)) {
             $this->start_controls_section(
                 'eael_section_pro',
                 [
