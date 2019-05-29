ecbd0beececf1fca29900aabdd17f2dab6e72079
Sabuz <naz.sabuz@gmail.com>
2019-05-29 14:45:16 +0600

    updated: assets

--- essential_adons_elementor.php
+++ essential_adons_elementor.php
@@ -4,7 +4,7 @@
  * Description: The ultimate elements library for Elementor page builder plugin for WordPress.
  * Plugin URI: https://essential-addons.com/elementor/
  * Author: WPDeveloper
- * Version: 3.0.0
+ * Version: 3.0.1
  * Author URI: https://wpdeveloper.net/
  *
  * Text Domain: essential-addons-elementor
