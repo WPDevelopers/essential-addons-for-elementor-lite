01dc19b6bc9ba490c77b81e08b36dc282e38e2ea
Rupok <re.enter.rupok@gmail.com>
2019-06-12 16:08:06 +0600

    update to v3.0.5

--- essential_adons_elementor.php
+++ essential_adons_elementor.php
@@ -4,7 +4,7 @@
  * Description: The ultimate elements library for Elementor page builder plugin for WordPress.
  * Plugin URI: https://essential-addons.com/elementor/
  * Author: WPDeveloper
- * Version: 3.0.4
+ * Version: 3.0.5
  * Author URI: https://wpdeveloper.net/
  * Text Domain: essential-addons-elementor
  * Domain Path: /languages
@@ -23,7 +23,7 @@ define('EAEL_PLUGIN_FILE', __FILE__);
 define('EAEL_PLUGIN_BASENAME', plugin_basename(__FILE__));
 define('EAEL_PLUGIN_PATH', plugin_dir_path(__FILE__));
 define('EAEL_PLUGIN_URL', plugins_url('/', __FILE__));
-define('EAEL_PLUGIN_VERSION', '3.0.4');
+define('EAEL_PLUGIN_VERSION', '3.0.5');
 define('EAEL_ASSET_PATH', wp_upload_dir()['basedir'] . DIRECTORY_SEPARATOR . 'essential-addons-elementor');
 define('EAEL_ASSET_URL', wp_upload_dir()['baseurl'] . '/essential-addons-elementor');
 
