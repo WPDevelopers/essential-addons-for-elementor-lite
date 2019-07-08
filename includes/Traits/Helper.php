ca68107cc74d8c7050746da5284410291ef0fba8
Sabuz <naz.sabuz@gmail.com>
2019-07-08 15:05:35 +0600

    Fixed: Twitter Data

--- includes/Traits/Helper.php
+++ includes/Traits/Helper.php
@@ -1622,10 +1622,10 @@ foreach ($settings['eael_pricing_table_items'] as $item):
                         }
                     $html .= '</div>
                     <div class="eael-twitter-feed-item-content">
-                        <p>' . substr(substr($item['full_text'], 0, (strpos($item['full_text'], ' ' . $item['entities']['urls'][0]['url']) > 2 ? strpos($item['full_text'], ' ' . $item['entities']['urls'][0]['url']) : 400)), 0, $settings['eael_twitter_feed_content_length']) . '...</p>';
+                        <p>' . substr(str_replace(@$item['entities']['urls'][0]['url'], '', $item['full_text']), 0, $settings['eael_twitter_feed_content_length']) . '...</p>';
                         
                         if ($settings['eael_twitter_feed_show_read_more'] == 'true') {
-                            $html .= '<a href="' . $item['entities']['urls'][0]['url'] . '" target="_blank" class="read-more-link">Read More <i class="fa fa-angle-double-right"></i></a>';
+                            $html .= '<a href="//twitter.com/' . @$item['user']['screen_name'] . '\/status/' . $item['id'] . '" target="_blank" class="read-more-link">Read More <i class="fa fa-angle-double-right"></i></a>';
                         }
                     $html .= '</div>
                     ' . (isset($item['extended_entities']['media'][0]) && $settings['eael_twitter_feed_media'] == 'true' ? ($item['extended_entities']['media'][0]['type'] == 'photo' ? '<img src="' . $item['extended_entities']['media'][0]['media_url_https'] . '">' : '') : '') . '
