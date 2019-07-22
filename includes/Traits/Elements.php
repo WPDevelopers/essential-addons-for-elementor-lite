08fa99f37d7f3112ac0385ec1d3722c9cd152d71
Sabuz <naz.sabuz@gmail.com>
2019-07-22 13:16:07 +0600

    working on global scroll progress

--- includes/Traits/Elements.php
+++ includes/Traits/Elements.php
@@ -6,7 +6,7 @@ if (!defined('ABSPATH')) {
     exit();
 } // Exit if accessed directly
 
-use Essential_Addons_Elementor\Classes\Group_Control_EA_Posts as Group_Control_EA_Posts;
+use Essential_Addons_Elementor\Classes\Group_Control_EA_Posts;
 
 trait Elements
 {
@@ -84,6 +84,51 @@ trait Elements
             }
 
             new $extension['class'];
+        }
+    }
+
+    /**
+     * Register extensions
+     *
+     * @since v3.1.4
+     */
+    public function render_global_html()
+    {
+        if (is_singular() && !in_array('eael-scroll-progress', (array) $this->transient_extensions)) {
+            $settings = get_option('eael_global_settings');
+
+            if ($settings['scroll_progress']['enabled']) {
+                if ($settings['scroll_progress']['display_condition'] == 'pages' && !is_page()) {
+                    return;
+                }
+                if ($settings['scroll_progress']['display_condition'] == 'posts' && !is_post()) {
+                    return;
+                }
+                if ($settings['scroll_progress']['display_condition'] == 'all' && !is_singular()) {
+                    return;
+                }
+
+                add_filter('eael/section/after_render', function ($extensions) {
+                    $extensions[] = 'eael-scroll-progress';
+                    return $extensions;
+                });
+    
+                echo '<div class="eael-scroll-progress eael-scroll-progress-' . $settings['scroll_progress']['position'] . '">
+                    <div class="eael-scroll-progress-fill"></div>
+                    <style scoped>
+                        .eael-scroll-progress, .eael-scroll-progress .eael-scroll-progress-fill {
+                            height: ' . $settings['scroll_progress']['height']['size'] . 'px;
+                        }
+                        .eael-scroll-progress {
+                            background-color: ' . $settings['scroll_progress']['bg_color'] . ';
+                        }
+                        .eael-scroll-progress .eael-scroll-progress-fill {
+                            background-color: ' . $settings['scroll_progress']['fill_color'] . ';
+                            transition: width ' . $settings['scroll_progress']['animation_speed']['size'] . 'ms ease;
+                        }
+                    </style>
+                </div>';
+            }
         }
     }
 
