8008549bf7c9a128c603a89dbb0d0c550188d0bb
Sabuz <naz.sabuz@gmail.com>
2019-08-29 12:40:14 +0600

    removed: unnecessary js

--- assets/front-end/js/eael.js
+++ assets/front-end/js/eael.js
@@ -11778,65 +11778,61 @@ return $;
 (function($) {
     "use strict";
 
-    window.eaelLoadMore = function() {
-        $(".eael-load-more-button").on("click", function(e) {
-            e.stopPropagation();
-            e.stopImmediatePropagation();
-            e.preventDefault();
+    $(".eael-load-more-button").on("click", function(e) {
+        e.stopPropagation();
+        e.stopImmediatePropagation();
+        e.preventDefault();
 
-            var $this = $(this),
-                $text = $("span", $this).html(),
-                $widget_id = $this.data("widget"),
-                $class = $this.data("class"),
-                $args = $this.data("args"),
-                $settings = $this.data("settings"),
-                $layout = $this.data("layout"),
-                $page = parseInt($this.data("page")) + 1;
+        var $this = $(this),
+            $text = $("span", $this).html(),
+            $widget_id = $this.data("widget"),
+            $class = $this.data("class"),
+            $args = $this.data("args"),
+            $settings = $this.data("settings"),
+            $layout = $this.data("layout"),
+            $page = parseInt($this.data("page")) + 1;
 
-            $this.addClass("button--loading");
-            $("span", $this).html("Loading...");
+        $this.addClass("button--loading");
+        $("span", $this).html("Loading...");
 
-            $.ajax({
-                url: localize.ajaxurl,
-                type: "post",
-                data: {
-                    action: "load_more",
-                    class: $class,
-                    args: $args,
-                    settings: $settings,
-                    page: $page
-                },
-                success: function(response) {
-                    var $content = $(response);
+        $.ajax({
+            url: localize.ajaxurl,
+            type: "post",
+            data: {
+                action: "load_more",
+                class: $class,
+                args: $args,
+                settings: $settings,
+                page: $page
+            },
+            success: function(response) {
+                var $content = $(response);
 
-                    if (
-                        $content.hasClass("no-posts-found") ||
-                        $content.length == 0
-                    ) {
-                        $this.remove();
+                if (
+                    $content.hasClass("no-posts-found") ||
+                    $content.length == 0
+                ) {
+                    $this.remove();
+                } else {
+                    if ($layout == "masonry") {
+                        $(".eael-post-appender-" + $widget_id)
+                            .append($content)
+                            .isotope("appended", $content);
                     } else {
-                        if ($layout == "masonry") {
-                            $(".eael-post-appender-" + $widget_id)
-                                .append($content)
-                                .isotope("appended", $content);
-                        } else {
-                            $(".eael-post-appender-" + $widget_id).append(
-                                $content
-                            );
-                        }
-
-                        $this.removeClass("button--loading");
-                        $("span", $this).html($text);
-
-                        $this.data("page", $page);
+                        $(".eael-post-appender-" + $widget_id).append($content);
                     }
-                },
-                error: function(response) {
-                    console.log(response);
+
+                    $this.removeClass("button--loading");
+                    $("span", $this).html($text);
+
+                    $this.data("page", $page);
                 }
-            });
+            },
+            error: function(response) {
+                console.log(response);
+            }
         });
-    };
+    });
 })(jQuery);
 
 var AdvAccordionHandler = function($scope, $) {
@@ -12464,8 +12460,6 @@ var PostGrid = function ($scope, $) {
     $gallery.imagesLoaded().progress(function() {
         $gallery.isotope("layout");
     });
-
-    eaelLoadMore();
 }
 
 jQuery(window).on("elementor/frontend/init", function() {
@@ -12474,60 +12468,6 @@ jQuery(window).on("elementor/frontend/init", function() {
         PostGrid
     );
 });
-var postTimelineHandler = function($scope, $) {
-    var $_this = $scope.find(".eael-post-timeline"),
-        $currentTimelineId = "#" + $_this.attr("id"),
-        $total_posts = parseInt($_this.data("total_posts"), 10),
-        $timeline_id = $_this.data("timeline_id"),
-        $post_type = $_this.data("post_type"),
-        $posts_per_page = parseInt($_this.data("posts_per_page"), 10),
-        $post_order = $_this.data("post_order"),
-        $post_orderby = $_this.data("post_orderby"),
-        $post_offset = parseInt($_this.data("post_offset"), 10),
-        $show_images = $_this.data("show_images"),
-        $image_size = $_this.data("image_size"),
-        $show_title = $_this.data("show_title"),
-        $show_excerpt = $_this.data("show_excerpt"),
-        $excerpt_length = parseInt($_this.data("excerpt_length"), 10),
-        $btn_text = $_this.data("btn_text"),
-        $tax_query = $_this.data("tax_query"),
-        $post__in = $_this.data("post__in"),
-        $exclude_posts = $_this.data("exclude_posts");
-
-    var options = {
-        totalPosts: $total_posts,
-        loadMoreBtn: $("#eael-load-more-btn-" + $timeline_id),
-        postContainer: $(".eael-post-appender-" + $timeline_id),
-        postStyle: "timeline"
-    };
-
-    var settings = {
-        postType: $post_type,
-        perPage: $posts_per_page,
-        postOrder: $post_order,
-        orderBy: $post_orderby,
-        offset: $post_offset,
-
-        showImage: $show_images,
-        imageSize: $image_size,
-        showTitle: $show_title,
-        showExcerpt: $show_excerpt,
-        excerptLength: parseInt($excerpt_length, 10),
-        btnText: $btn_text,
-        tax_query: $tax_query,
-        post__in: $post__in,
-        exclude_posts: $exclude_posts
-    };
-
-    eaelLoadMore(options, settings);
-};
-jQuery(window).on("elementor/frontend/init", function() {
-    elementorFrontend.hooks.addAction(
-        "frontend/element_ready/eael-post-timeline.default",
-        postTimelineHandler
-    );
-});
-
 var PricingTooltip = function($scope, $) {
     if ($.fn.tooltipster) {
         var $tooltip = $scope.find(".tooltip"),
--- assets/front-end/js/vendor/load-more/load-more.js
+++ assets/front-end/js/vendor/load-more/load-more.js
@@ -1,63 +1,59 @@
 (function($) {
     "use strict";
 
-    window.eaelLoadMore = function() {
-        $(".eael-load-more-button").on("click", function(e) {
-            e.stopPropagation();
-            e.stopImmediatePropagation();
-            e.preventDefault();
+    $(".eael-load-more-button").on("click", function(e) {
+        e.stopPropagation();
+        e.stopImmediatePropagation();
+        e.preventDefault();
 
-            var $this = $(this),
-                $text = $("span", $this).html(),
-                $widget_id = $this.data("widget"),
-                $class = $this.data("class"),
-                $args = $this.data("args"),
-                $settings = $this.data("settings"),
-                $layout = $this.data("layout"),
-                $page = parseInt($this.data("page")) + 1;
+        var $this = $(this),
+            $text = $("span", $this).html(),
+            $widget_id = $this.data("widget"),
+            $class = $this.data("class"),
+            $args = $this.data("args"),
+            $settings = $this.data("settings"),
+            $layout = $this.data("layout"),
+            $page = parseInt($this.data("page")) + 1;
 
-            $this.addClass("button--loading");
-            $("span", $this).html("Loading...");
+        $this.addClass("button--loading");
+        $("span", $this).html("Loading...");
 
-            $.ajax({
-                url: localize.ajaxurl,
-                type: "post",
-                data: {
-                    action: "load_more",
-                    class: $class,
-                    args: $args,
-                    settings: $settings,
-                    page: $page
-                },
-                success: function(response) {
-                    var $content = $(response);
+        $.ajax({
+            url: localize.ajaxurl,
+            type: "post",
+            data: {
+                action: "load_more",
+                class: $class,
+                args: $args,
+                settings: $settings,
+                page: $page
+            },
+            success: function(response) {
+                var $content = $(response);
 
-                    if (
-                        $content.hasClass("no-posts-found") ||
-                        $content.length == 0
-                    ) {
-                        $this.remove();
+                if (
+                    $content.hasClass("no-posts-found") ||
+                    $content.length == 0
+                ) {
+                    $this.remove();
+                } else {
+                    if ($layout == "masonry") {
+                        $(".eael-post-appender-" + $widget_id)
+                            .append($content)
+                            .isotope("appended", $content);
                     } else {
-                        if ($layout == "masonry") {
-                            $(".eael-post-appender-" + $widget_id)
-                                .append($content)
-                                .isotope("appended", $content);
-                        } else {
-                            $(".eael-post-appender-" + $widget_id).append(
-                                $content
-                            );
-                        }
-
-                        $this.removeClass("button--loading");
-                        $("span", $this).html($text);
-
-                        $this.data("page", $page);
+                        $(".eael-post-appender-" + $widget_id).append($content);
                     }
-                },
-                error: function(response) {
-                    console.log(response);
+
+                    $this.removeClass("button--loading");
+                    $("span", $this).html($text);
+
+                    $this.data("page", $page);
                 }
-            });
+            },
+            error: function(response) {
+                console.log(response);
+            }
         });
-    };
+    });
 })(jQuery);
