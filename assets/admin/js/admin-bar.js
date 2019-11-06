90fb8fe4f2ba3e2b70335a69d1ef0eab013736ca
Sabuz <naz.sabuz@gmail.com>
2019-11-06 12:50:18 +0600

    updated: texts replacing the term cache

--- assets/admin/js/admin-bar.js
+++ assets/admin/js/admin-bar.js
@@ -1,26 +1,27 @@
 (function($) {
-    "use strict";
+    'use strict';
 
-    $(document).on('click', '.ea-clear-cache', function (e) {
+    $(document).on('click', '.ea-clear-cache', function(e) {
         e.preventDefault();
 
-        if(typeof localize != 'undefined' && localize) {
-            var pageID = $(this).parent().find('.ea-clear-cache-id').data('pageid'),
-            text = $(this).find('.ab-item');
+        if (typeof localize != 'undefined' && localize) {
+            var pageID = $(this)
+                    .parent()
+                    .find('.ea-clear-cache-id')
+                    .data('pageid'),
+                text = $(this).find('.ab-item');
 
             $.ajax({
                 url: localize.ajaxurl,
-                type: "post",
+                type: 'post',
                 data: {
-                    action: "clear_cache_files_with_ajax",
+                    action: 'clear_cache_files_with_ajax',
                     security: localize.nonce,
                     pageID: pageID,
                     actionType: 'post'
                 },
                 beforeSend: function() {
-                    text.text(
-                        'Generating...'
-                    );
+                    text.text('Generating...');
                 },
                 success: function(response) {
                     setTimeout(function() {
@@ -37,23 +38,21 @@
         }
     });
 
-    $(document).on('click', '.ea-all-cache-clear', function (e) {
+    $(document).on('click', '.ea-all-cache-clear', function(e) {
         e.preventDefault();
 
-        if(typeof localize != 'undefined' && localize) {
+        if (typeof localize != 'undefined' && localize) {
             var text = $(this).find('.ab-item');
-            
+
             $.ajax({
                 url: localize.ajaxurl,
-                type: "post",
+                type: 'post',
                 data: {
-                    action: "clear_cache_files_with_ajax",
+                    action: 'clear_cache_files_with_ajax',
                     security: localize.nonce
                 },
                 beforeSend: function() {
-                    text.text(
-                        'Generating...'
-                    );
+                    text.text('Generating...');
                 },
                 success: function(response) {
                     setTimeout(function() {
@@ -65,9 +64,8 @@
                     console.log('Something went wrong!');
                 }
             });
-        }else {
-            console.log('This page has no widget from EA, Clear cache from Dashboard');
+        } else {
+            console.log('This page has no widget from EA, Regenerate Assets from Dashboard');
         }
     });
-
 })(jQuery);
