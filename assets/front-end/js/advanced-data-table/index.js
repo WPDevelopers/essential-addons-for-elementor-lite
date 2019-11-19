79d6fb494b10117a54e57b53700bc6aca2e29622
Sabuz <naz.sabuz@gmail.com>
2019-11-19 18:03:22 +0600

    updated: data table

--- assets/front-end/js/advanced-data-table/index.js
+++ assets/front-end/js/advanced-data-table/index.js
@@ -7,27 +7,24 @@ var Advanced_Data_Table = function($scope, $) {
 
         $(this).focus();
     });
+};
 
-    // update static table
-    $('.ea-advanced-data-table', $scope).on('DOMSubtreeModified', function() {
-        clearTimeout(interval);
+// Inline edit
+var Advanced_Data_Table_Inline_Edit = function(panel, model, view) {
+    
+    if (view.el.querySelector('.ea-advanced-data-table')) {
+        view.el.querySelector('.ea-advanced-data-table').addEventListener('DOMSubtreeModified', function(e) {
+            panel.el.querySelector('#elementor-panel-saver-button-publish').classList.remove('elementor-disabled');
+            panel.el.querySelector('#elementor-panel-saver-button-save-options').classList.remove('elementor-disabled');
+        });
+    }
 
-        interval = setTimeout(function() {
-            // let canvasDocument = iframe.contentDocument || iframe.contentWindow;
-
-            // console.log(parent.querySelector('[data-setting="ea_adv_data_table_static_html"]'));
-            console.log(jQuery(window));
-        }, 250);
+    panel.el.querySelector('#elementor-panel-saver-button-publish').addEventListener('click', function() {
+        model.setSetting('ea_adv_data_table_static_html', view.el.querySelector('.ea-advanced-data-table').innerHTML);
     });
 };
 
 jQuery(window).on('elementor/frontend/init', function() {
+    elementor.hooks.addAction('panel/open_editor/widget/eael-advanced-data-table', Advanced_Data_Table_Inline_Edit);
     elementorFrontend.hooks.addAction('frontend/element_ready/eael-advanced-data-table.default', Advanced_Data_Table);
-
-    //     elementor.hooks.addAction('panel/open_editor/widget/eael-advanced-data-table', function(panel, model, view) {
-    //         // model.addChangeCallback('ea_adv_data_table_static_num_row', function(val) {
-    //         //     console.log(val);
-    //         // });
-    //         model.setSetting('ea_adv_data_table_static_html', 'dsakfj;slkdfj;l');
-    //     });
 });
--- includes/Elements/Advanced_Data_Table.php
+++ includes/Elements/Advanced_Data_Table.php
@@ -57,68 +57,12 @@ class Advanced_Data_Table extends Widget_Base
         );
 
         $this->add_control(
-            'ea_adv_data_table_static_num_row',
-            [
-                'label' => __('Row', 'essential-addons-elementor'),
-                'type' => Controls_Manager::NUMBER,
-                'min' => 1,
-                'max' => 1000,
-                'step' => 1,
-                'default' => 4,
-                'condition' => [
-                    'ea_adv_data_table_source' => 'static',
-                ],
-            ]
-        );
-
-        $this->add_control(
-            'ea_adv_data_table_static_num_col',
-            [
-                'label' => __('Column', 'essential-addons-elementor'),
-                'type' => Controls_Manager::NUMBER,
-                'min' => 1,
-                'max' => 1000,
-                'step' => 1,
-                'default' => 4,
-                'condition' => [
-                    'ea_adv_data_table_source' => 'static',
-                ],
-            ]
-        );
-        
-        $this->add_control(
             'ea_adv_data_table_static_html',
             [
-                'label' => __('Column', 'essential-addons-elementor'),
                 'type' => Controls_Manager::HIDDEN,
                 'default' => $this->html_table,
             ]
         );
-
-
-
-        // $this->add_control(
-        //     'ea_adv_data_table_static_generate',
-        //     [
-        //         'type' => Controls_Manager::BUTTON,
-        //         'text' => __('Generate Table', 'essential-addons-elementor'),
-        //         'event' => 'ea:atab:gen',
-        //         'condition' => [
-        //             'ea_adv_data_table_source' => 'static',
-        //         ],
-        //     ]
-        // );
-
-        // $this->add_control(
-        //     'ea_adv_data_table_static_generate',
-        //     [
-        //         'type' => Controls_Manager::RAW_HTML,
-        //         'label' => '<div style="display:block;"><button class="elementor-button ea-adv-data-table-gen" data-id="' . $this->get_id() . '" style="display:block;background:#d30c5c;color:#fff;margin:0 auto;padding:5px 10px;">Generate</button></div>',
-        //         'condition' => [
-        //             'ea_adv_data_table_source' => 'static',
-        //         ],
-        //     ]
-        // );
 
         $this->end_controls_section();
     }
@@ -131,8 +75,6 @@ class Advanced_Data_Table extends Widget_Base
             $this->add_render_attribute('ea-adv-data-table', [
                 'class' => "ea-advanced-data-table ea-advanced-data-table-{$settings['ea_adv_data_table_source']} ea-advanced-data-table-{$this->get_id()}",
                 'data-id' => $this->get_id(),
-                'data-num-row' => $settings['ea_adv_data_table_static_num_row'],
-                'data-num-col' => $settings['ea_adv_data_table_static_num_col'],
             ]);
         }
 
@@ -143,25 +85,7 @@ class Advanced_Data_Table extends Widget_Base
 
     protected function html_static_table($settings)
     {
-        $row = $settings['ea_adv_data_table_static_num_row'];
-        $col = $settings['ea_adv_data_table_static_num_col'];
-        $table = $settings['ea_adv_data_table_static_html'];
-
-        echo $table;
-
-        $table = '<table>';
-
-        foreach (range(1, $row) as $r) {
-            $table .= '<tr>';
-            foreach (range(1, $col) as $c) {
-                $table .= '<td contenteditable></td>';
-            }
-            $table .= '</tr>';
-        }
-
-        $table .= '</table>';
-
-        return $table;
+        return $settings['ea_adv_data_table_static_html'];
     }
 
 }
