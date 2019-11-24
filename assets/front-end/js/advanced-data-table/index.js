dc5e477789ef8a123e5d82f15f156b7dee440295
Sabuz <naz.sabuz@gmail.com>
2019-11-24 15:42:14 +0600

    updated: data table static

--- assets/front-end/js/advanced-data-table/index.js
+++ assets/front-end/js/advanced-data-table/index.js
@@ -2,38 +2,28 @@ var Advanced_Data_Table = function($scope, $) {};
 
 // Inline edit
 var Advanced_Data_Table_Inline_Edit = function(panel, model, view) {
-    if (view.el.querySelector('.ea-advanced-data-table')) {
-        // enable inline edit
-        // view.el.querySelector('.ea-advanced-data-table').addEventListener('click', function(e) {
-        //     if (e.target.tagName.toLowerCase() == 'th' || e.target.tagName.toLowerCase() == 'td') {
-        //         e.target.focus();
-        //     }
-        // });
+    setTimeout(function() {
+        var table = view.el.querySelector('.ea-advanced-data-table').querySelector('table');
 
-        view.el
-            .querySelector('.ea-advanced-data-table')
-            .querySelectorAll('th, td')
-            .forEach(function(el) {
-                el.addEventListener('click', function(e) {
-                    e.target.focus();
-                });
-                el.addEventListener('keyup', function(e) {
-                    model.remoteRender = false;
-                    model.setSetting('ea_adv_data_table_static_html', this.innerHTML);
+        // inline edit
+        table.addEventListener('click', function(e) {
+            if (e.target.tagName.toLowerCase() == 'th' || e.target.tagName.toLowerCase() == 'td') {
+                e.target.focus();
+            }
+        });
+
+        // update table
+        table.querySelectorAll('th, td').forEach(function(el) {
+            el.addEventListener('focusout', function(e) {
+                model.remoteRender = false;
+                model.setSetting('ea_adv_data_table_static_html', table.innerHTML);
+
+                setTimeout(function() {
                     model.remoteRender = true;
-                });
+                }, 1001);
             });
-
-        // if (e.code === 'KeyZ' && e.ctrlKey === true) {
-        //     var row = e.target.parentNode.rowIndex;
-        //     var cell = e.target.cellIndex;
-
-        //     this.innerHTML = model.getSetting('ea_adv_data_table_static_html');
-        //     this.querySelector('table').rows[row].cells[cell].focus();
-        // } else {
-        //     model.setSetting('ea_adv_data_table_static_html', this.innerHTML);
-        // }
-    }
+        });
+    }, 500);
 };
 
 jQuery(window).on('elementor/frontend/init', function() {
