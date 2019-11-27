2ce1712bbc9c0c19e58c087b9e71947f411261eb
Nazmul Sabuz <naz.sabuz@gmail.com>
2019-11-27 16:45:48 +0600

    updated: data table

--- assets/front-end/js/advanced-data-table/index.js
+++ assets/front-end/js/advanced-data-table/index.js
@@ -1,4 +1,4 @@
-var Advanced_Data_Table = function($scope, $) {
+var Advanced_Data_Table = function ($scope, $) {
     if (isEditMode) {
         var table = $scope.context.querySelector('.ea-advanced-data-table');
 
@@ -6,7 +6,7 @@ var Advanced_Data_Table = function($scope, $) {
         table.classList.add('ea-advanced-data-table-editable')
 
         // insert editable area
-        table.querySelectorAll('th, td').forEach(function(el) {
+        table.querySelectorAll('th, td').forEach(function (el) {
             var value = el.innerHTML;
 
             if (value.indexOf('<textarea rows="1">') !== 0) {
@@ -17,22 +17,22 @@ var Advanced_Data_Table = function($scope, $) {
 };
 
 // Inline edit
-var Advanced_Data_Table_Inline_Edit = function(panel, model, view) {
-    setTimeout(function() {
+var Advanced_Data_Table_Inline_Edit = function (panel, model, view) {
+    setTimeout(function () {
         if (view.el.querySelector('.ea-advanced-data-table')) {
             var interval;
             var table = view.el.querySelector('.ea-advanced-data-table');
 
             // save input on edit
-            table.querySelectorAll('textarea').forEach(function(el) {
-                el.addEventListener('focusout', function(e) {
+            table.querySelectorAll('textarea').forEach(function (el) {
+                el.addEventListener('focusout', function (e) {
                     clearTimeout(interval);
 
                     // clone current table
                     var origTable = table.cloneNode(true);
 
                     // remove editable area
-                    origTable.querySelectorAll('th, td').forEach(function(el) {
+                    origTable.querySelectorAll('th, td').forEach(function (el) {
                         var value = el.querySelector('textarea').value;
                         el.innerHTML = value;
                     });
@@ -45,7 +45,7 @@ var Advanced_Data_Table_Inline_Edit = function(panel, model, view) {
 
                     // enable elementor remote server render just after elementor throttle
                     // ignore multiple assign
-                    interval = setTimeout(function() {
+                    interval = setTimeout(function () {
                         model.remoteRender = true;
                     }, 1001);
                 });
@@ -54,8 +54,47 @@ var Advanced_Data_Table_Inline_Edit = function(panel, model, view) {
     }, 300);
 };
 
-jQuery(window).on('elementor/frontend/init', function() {
+
+Advanced_Data_Table_Context_Menu = function (groups, element) {
+    if (element.options.model.attributes.widgetType == 'eael-advanced-data-table') {
+        groups.push({
+            name: 'ea_advanced_data_table',
+            actions: [
+                {
+                    name: 'add_row',
+                    title: 'Add Row',
+                    callback: function () {
+                        var table = document.querySelector('.ea-advanced-data-table-' + element.options.model.attributes.id);
+                        var row = table.insertRow(table.rows.length);
+
+                        for (var i = 0; i < table.rows[0].cells.length; i++) {
+                            var cell = row.insertCell(i);
+                            cell.innerHTML = '<textarea rows="1"></textarea>';
+                        }
+                    }
+                },
+                {
+                    name: 'add_column',
+                    title: 'Add Column',
+                    callback: function () {
+                        var table = document.querySelector('.ea-advanced-data-table-' + element.options.model.attributes.id);
+
+                        for (var i = 0; i < table.rows.length; i++) {
+                            var cell = table.rows[i].insertCell(table.rows[i].cells.length - 1);
+                            cell.innerHTML = '<textarea rows="1"></textarea>';
+                        }
+                    }
+                }
+            ]
+        });
+    }
+
+    return groups;
+}
+
+jQuery(window).on('elementor/frontend/init', function () {
     if (isEditMode) {
+        elementor.hooks.addFilter('elements/widget/contextMenuGroups', Advanced_Data_Table_Context_Menu);
         elementor.hooks.addAction('panel/open_editor/widget/eael-advanced-data-table', Advanced_Data_Table_Inline_Edit);
     }
 
