05b98e2d92ca547402a39a601cb1e388770ee1c8
Mahafuz <m.mahfuz.me@gmail.com>
2019-12-17 11:34:51 +0600

    EA breaks when used in Elementor templates for search page

--- includes/Traits/Generator.php
+++ includes/Traits/Generator.php
@@ -184,7 +184,7 @@ trait Generator
 
         $elements = array_filter(array_unique(array_merge($elements, $extensions)));
 
-        if (is_singular() || is_home() || is_archive()) {
+        if (is_singular() || is_home() || is_archive() || is_404() || is_search()) {
             $queried_object = get_queried_object_id();
             $post_type = (is_singular() || is_home() ? 'post' : 'term');
             $old_elements = (array) get_metadata($post_type, $queried_object, 'eael_transient_elements', true);
