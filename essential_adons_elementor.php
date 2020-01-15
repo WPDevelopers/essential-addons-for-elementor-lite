c2afc0059f33217cf5c822a6e19a3cf3b0353f97
Mahafuz <m.mahfuz.me@gmail.com>
2020-01-15 11:17:07 +0600

    Fluent form improvement initial update

--- essential_adons_elementor.php
+++ essential_adons_elementor.php
@@ -4,7 +4,7 @@
  * Description: The ultimate elements library for Elementor page builder plugin for WordPress.
  * Plugin URI: https://essential-addons.com/elementor/
  * Author: WPDeveloper
- * Version: 3.7.0
+ * Version: 3.7.1
  * Author URI: https://wpdeveloper.net/
  *
  * Text Domain: essential-addons-for-elementor-lite
