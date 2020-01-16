a78fd873b8362441d10589962d2b4ae74bd42e47
manzurahammed <manzur.ahammed236@gmail.com>
2020-01-16 13:07:32 +0600

    check page condition

--- includes/Traits/Core.php
+++ includes/Traits/Core.php
@@ -161,7 +161,6 @@ trait Core
 
         //save table of content global value
         if($page_settings_model->get_settings('eael_ext_toc_global') == 'yes' && $page_settings_model->get_settings('eael_ext_table_of_content') == 'yes') {
-
             $global_settings['table_of_content'] = [
                 'post_id'                                           => $post_id,
                 'enabled'                                           => ($page_settings_model->get_settings('eael_ext_toc_global') == 'yes' ? true : false),
