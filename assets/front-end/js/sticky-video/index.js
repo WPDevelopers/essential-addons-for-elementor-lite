e567ca541989409d14824df2d5f1bb42666082e6
Nazmul Sabuz <naz.sabuz@gmail.com>
2020-01-22 15:11:24 +0600

    Fix: sticky video issue

--- assets/front-end/js/sticky-video/index.js
+++ assets/front-end/js/sticky-video/index.js
@@ -107,6 +107,10 @@ jQuery(window).on('elementor/frontend/init', function () {
         });
 
         element.parent().css('height', element.height() + 'px');
+
+        $(window).resize(function() {
+            element.parent().css('height', element.height() + 'px');
+        });
     });
 
 });
