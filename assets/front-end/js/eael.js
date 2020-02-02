0f1775bb010fe3fd12768b420f16d2705be1603f
manzurahammed <manzur.ahammed236@gmail.com>
2020-02-02 17:36:09 +0600

    calendar border color

--- assets/front-end/js/eael.js
+++ assets/front-end/js/eael.js
@@ -28194,6 +28194,7 @@ var EventCalendar = function($scope, $) {
 			draggable: false,
 			firstDay: firstDay,
 			slotLabelFormat: "HH:mm",
+			slotDuration: '01:00',
 			timeFormat: "hh:mm a",
 			nextDayThreshold: "00:00:00",
 			header: {
