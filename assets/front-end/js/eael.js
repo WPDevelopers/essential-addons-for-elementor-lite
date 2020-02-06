f3ded4f41c0f95ff87b6f1a12efdc0f5f093b52e
manzurahammed <manzur.ahammed236@gmail.com>
2020-02-06 17:00:48 +0600

    add scroll bar

--- assets/front-end/js/eael.js
+++ assets/front-end/js/eael.js
@@ -38981,6 +38981,67 @@ return $;
     });
 })(jQuery);
 
+var AdvAccordionHandler = function($scope, $) {
+    var $advanceAccordion = $scope.find(".eael-adv-accordion"),
+        $accordionHeader = $scope.find(".eael-accordion-header"),
+        $accordionType = $advanceAccordion.data("accordion-type"),
+        $accordionSpeed = $advanceAccordion.data("toogle-speed");
+
+    // Open default actived tab
+    $accordionHeader.each(function() {
+        if ($(this).hasClass("active-default")) {
+            $(this).addClass("show active");
+            $(this)
+                .next()
+                .slideDown($accordionSpeed);
+        }
+    });
+
+    // Remove multiple click event for nested accordion
+    $accordionHeader.unbind("click");
+
+    $accordionHeader.click(function(e) {
+        e.preventDefault();
+
+        var $this = $(this);
+
+        if ($accordionType === "accordion") {
+            if ($this.hasClass("show")) {
+                $this.removeClass("show active");
+                $this.next().slideUp($accordionSpeed);
+            } else {
+                $this
+                    .parent()
+                    .parent()
+                    .find(".eael-accordion-header")
+                    .removeClass("show active");
+                $this
+                    .parent()
+                    .parent()
+                    .find(".eael-accordion-content")
+                    .slideUp($accordionSpeed);
+                $this.toggleClass("show active");
+                $this.next().slideToggle($accordionSpeed);
+            }
+        } else {
+            // For acccordion type 'toggle'
+            if ($this.hasClass("show")) {
+                $this.removeClass("show active");
+                $this.next().slideUp($accordionSpeed);
+            } else {
+                $this.addClass("show active");
+                $this.next().slideDown($accordionSpeed);
+            }
+        }
+    });
+};
+jQuery(window).on("elementor/frontend/init", function() {
+    elementorFrontend.hooks.addAction(
+        "frontend/element_ready/eael-adv-accordion.default",
+        AdvAccordionHandler
+    );
+});
+
 var AdvanceTabHandler = function($scope, $) {
     var $currentTab = $scope.find(".eael-advance-tabs"),
         $currentTabId = "#" + $currentTab.attr("id").toString();
@@ -39082,67 +39143,6 @@ jQuery(window).on("elementor/frontend/init", function() {
     elementorFrontend.hooks.addAction(
         "frontend/element_ready/eael-adv-tabs.default",
         AdvanceTabHandler
-    );
-});
-
-var AdvAccordionHandler = function($scope, $) {
-    var $advanceAccordion = $scope.find(".eael-adv-accordion"),
-        $accordionHeader = $scope.find(".eael-accordion-header"),
-        $accordionType = $advanceAccordion.data("accordion-type"),
-        $accordionSpeed = $advanceAccordion.data("toogle-speed");
-
-    // Open default actived tab
-    $accordionHeader.each(function() {
-        if ($(this).hasClass("active-default")) {
-            $(this).addClass("show active");
-            $(this)
-                .next()
-                .slideDown($accordionSpeed);
-        }
-    });
-
-    // Remove multiple click event for nested accordion
-    $accordionHeader.unbind("click");
-
-    $accordionHeader.click(function(e) {
-        e.preventDefault();
-
-        var $this = $(this);
-
-        if ($accordionType === "accordion") {
-            if ($this.hasClass("show")) {
-                $this.removeClass("show active");
-                $this.next().slideUp($accordionSpeed);
-            } else {
-                $this
-                    .parent()
-                    .parent()
-                    .find(".eael-accordion-header")
-                    .removeClass("show active");
-                $this
-                    .parent()
-                    .parent()
-                    .find(".eael-accordion-content")
-                    .slideUp($accordionSpeed);
-                $this.toggleClass("show active");
-                $this.next().slideToggle($accordionSpeed);
-            }
-        } else {
-            // For acccordion type 'toggle'
-            if ($this.hasClass("show")) {
-                $this.removeClass("show active");
-                $this.next().slideUp($accordionSpeed);
-            } else {
-                $this.addClass("show active");
-                $this.next().slideDown($accordionSpeed);
-            }
-        }
-    });
-};
-jQuery(window).on("elementor/frontend/init", function() {
-    elementorFrontend.hooks.addAction(
-        "frontend/element_ready/eael-adv-accordion.default",
-        AdvAccordionHandler
     );
 });
 
@@ -40271,24 +40271,26 @@ var EventCalendar = function($scope, $) {
 			 	event = info.event;
 			element.attr("href", "javascript:void(0);");
 			element.click(function() {
-				var endDate = event.end;
-				var timeFormate = "h:mm A";
+				var startDate 	= event.start;
+				var	timeFormate = "h:mm A";
+				var endDate 	= event.end;
 				if(event.allDay === "yes"){
 					var newEnd = moment(endDate).subtract(1, "days");
 					endDate = newEnd._d;
 					timeFormate = " ";
 				}
+				$("span.eaelec-event-date-start").html(" ");
+				$("span.eaelec-event-date-end").html(" ");
 				var timeFormatLen = timeFormate.trim().length;
-				console.log(timeFormatLen+' t');
 				ecModal.addClass('eael-ec-popup-ready').removeClass('eael-ec-modal-removing');
 				if (event.allDay === "yes" && event.end === null) {
 					$("span.eaelec-event-date-start").html('<i class="eicon-calendar"></i> ' + moment(event.start).format("MMM Do"));
 				} else {
-					if (moment(event.start).isSame(Date.now(), "day") == true) {
+					if (moment(event.start).isSame(Date.now(), "day") === true) {
 						$("span.eaelec-event-date-start").html('<i class="eicon-calendar"></i> Today, ' + moment(event.start).format(timeFormate));
 					}
 					if (
-						moment(event.start).format("MM-DD-YYYY") ==
+						moment(startDate).format("MM-DD-YYYY") ===
 						moment(new Date())
 							.add(1, "days")
 							.format("MM-DD-YYYY")
@@ -40296,8 +40298,8 @@ var EventCalendar = function($scope, $) {
 						$("span.eaelec-event-date-start").html('<i class="eicon-calendar"></i> Tomorrow, ' + moment(event.start).format(timeFormate));
 					}
 					if (
-						moment(event.start).format("MM-DD-YYYY") < moment(new Date()).format("MM-DD-YYYY") ||
-						moment(event.start).format("MM-DD-YYYY") >
+						moment(startDate).format("MM-DD-YYYY") < moment(new Date()).format("MM-DD-YYYY") ||
+						moment(startDate).format("MM-DD-YYYY") >
 						moment(new Date())
 							.add(1, "days")
 							.format("MM-DD-YYYY")
@@ -40305,15 +40307,15 @@ var EventCalendar = function($scope, $) {
 						$("span.eaelec-event-date-start").html('<i class="eicon-calendar"></i> ' + moment(event.start).format("MMM Do, "+timeFormate));
 					}
 
-					if (moment(endDate).isSame(Date.now(), "day") == true && timeFormatLen>0) {
+					if (moment(endDate).isSame(Date.now(), "day") === true && timeFormatLen>0) {
 						$("span.eaelec-event-date-end").html("- " + moment(endDate).format(timeFormate));
 					}
 					if (
-						moment(event.start).format("MM-DD-YYYY") !=
+						moment(startDate).format("MM-DD-YYYY") !==
 						moment(new Date())
 							.add(1, "days")
 							.format("MM-DD-YYYY") &&
-						moment(endDate).format("MM-DD-YYYY") ==
+						moment(endDate).format("MM-DD-YYYY") ===
 						moment(new Date())
 							.add(1, "days")
 							.format("MM-DD-YYYY")
@@ -40321,21 +40323,19 @@ var EventCalendar = function($scope, $) {
 						$("span.eaelec-event-date-end").html("- Tomorrow, " + moment(endDate).format(timeFormate));
 					}
 					if (
-						moment(event.start).format("MM-DD-YYYY") ==
+						moment(startDate).format("MM-DD-YYYY") ===
 						moment(new Date())
 							.add(1, "days")
 							.format("MM-DD-YYYY") &&
-						moment(endDate).format("MM-DD-YYYY") ==
+						moment(event.end).format("MM-DD-YYYY") ===
 						moment(new Date())
 							.add(1, "days")
 							.format("MM-DD-YYYY")
 					) {
-						if(timeFormatLen>0){
-							$("span.eaelec-event-date-end").html("- " + moment(endDate).format(timeFormate));
-						}
+						$("span.eaelec-event-date-end").html("- " + moment(endDate).format(timeFormate));
 					}
 					if (
-						moment(endDate).format("MM-DD-YYYY") >
+						moment(event.end).format("MM-DD-YYYY") >
 						moment(new Date())
 							.add(1, "days")
 							.format("MM-DD-YYYY")
@@ -40344,26 +40344,23 @@ var EventCalendar = function($scope, $) {
 					}
 
 					if (
-						moment(event.start).format("MM-DD-YYYY") >
+						moment(startDate).format("MM-DD-YYYY") >
 						moment(new Date())
 							.add(1, "days")
 							.format("MM-DD-YYYY") &&
-						moment(event.start).format("MM-DD-YYYY") == moment(endDate).format("MM-DD-YYYY")
+						moment(startDate).format("MM-DD-YYYY") === moment(event.end).format("MM-DD-YYYY")
 					) {
-						if(timeFormatLen>0){
-							$("span.eaelec-event-date-end").html("- " + moment(endDate).format(timeFormate));
-						}
-
+						$("span.eaelec-event-date-end").html("- " + moment(endDate).format(timeFormate));
 					}
 				}
 
 				$(".eaelec-modal-header h2").html(event.title);
 				$(".eaelec-modal-body p").html(event.extendedProps.description);
 				$(".eaelec-modal-footer a").attr("href", event.url);
-				if (event.external == "on") {
+				if (event.external === "on") {
 					$(".eaelec-modal-footer a").attr("target", "_blank");
 				}
-				if (event.nofollow == "on") {
+				if (event.nofollow === "on") {
 					$(".eaelec-modal-footer a").attr("rel", "nofollow");
 				}
 				if (event.url == "") {
