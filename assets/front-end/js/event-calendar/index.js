671af58a3d71d9d517b3f57e16159d3fb84ff9c4
manzurahammed <manzur.ahammed236@gmail.com>
2020-02-06 17:00:48 +0600

    add scroll bar

--- assets/front-end/js/event-calendar/index.js
+++ assets/front-end/js/event-calendar/index.js
@@ -33,23 +33,26 @@ var EventCalendar = function($scope, $) {
 			 	event = info.event;
 			element.attr("href", "javascript:void(0);");
 			element.click(function() {
-				var endDate = event.end;
-				var timeFormate = "h:mm A";
+				var startDate 	= event.start;
+				var	timeFormate = "h:mm A";
+				var endDate 	= event.end;
 				if(event.allDay === "yes"){
 					var newEnd = moment(endDate).subtract(1, "days");
 					endDate = newEnd._d;
 					timeFormate = " ";
 				}
+				$("span.eaelec-event-date-start").html(" ");
+				$("span.eaelec-event-date-end").html(" ");
 				var timeFormatLen = timeFormate.trim().length;
 				ecModal.addClass('eael-ec-popup-ready').removeClass('eael-ec-modal-removing');
 				if (event.allDay === "yes" && event.end === null) {
 					$("span.eaelec-event-date-start").html('<i class="eicon-calendar"></i> ' + moment(event.start).format("MMM Do"));
 				} else {
-					if (moment(event.start).isSame(Date.now(), "day") == true) {
+					if (moment(event.start).isSame(Date.now(), "day") === true) {
 						$("span.eaelec-event-date-start").html('<i class="eicon-calendar"></i> Today, ' + moment(event.start).format(timeFormate));
 					}
 					if (
-						moment(event.start).format("MM-DD-YYYY") ==
+						moment(startDate).format("MM-DD-YYYY") ===
 						moment(new Date())
 							.add(1, "days")
 							.format("MM-DD-YYYY")
@@ -57,8 +60,8 @@ var EventCalendar = function($scope, $) {
 						$("span.eaelec-event-date-start").html('<i class="eicon-calendar"></i> Tomorrow, ' + moment(event.start).format(timeFormate));
 					}
 					if (
-						moment(event.start).format("MM-DD-YYYY") < moment(new Date()).format("MM-DD-YYYY") ||
-						moment(event.start).format("MM-DD-YYYY") >
+						moment(startDate).format("MM-DD-YYYY") < moment(new Date()).format("MM-DD-YYYY") ||
+						moment(startDate).format("MM-DD-YYYY") >
 						moment(new Date())
 							.add(1, "days")
 							.format("MM-DD-YYYY")
@@ -66,15 +69,15 @@ var EventCalendar = function($scope, $) {
 						$("span.eaelec-event-date-start").html('<i class="eicon-calendar"></i> ' + moment(event.start).format("MMM Do, "+timeFormate));
 					}
 
-					if (moment(endDate).isSame(Date.now(), "day") == true && timeFormatLen>0) {
+					if (moment(endDate).isSame(Date.now(), "day") === true && timeFormatLen>0) {
 						$("span.eaelec-event-date-end").html("- " + moment(endDate).format(timeFormate));
 					}
 					if (
-						moment(event.start).format("MM-DD-YYYY") !=
+						moment(startDate).format("MM-DD-YYYY") !==
 						moment(new Date())
 							.add(1, "days")
 							.format("MM-DD-YYYY") &&
-						moment(endDate).format("MM-DD-YYYY") ==
+						moment(endDate).format("MM-DD-YYYY") ===
 						moment(new Date())
 							.add(1, "days")
 							.format("MM-DD-YYYY")
@@ -82,21 +85,19 @@ var EventCalendar = function($scope, $) {
 						$("span.eaelec-event-date-end").html("- Tomorrow, " + moment(endDate).format(timeFormate));
 					}
 					if (
-						moment(event.start).format("MM-DD-YYYY") ==
+						moment(startDate).format("MM-DD-YYYY") ===
 						moment(new Date())
 							.add(1, "days")
 							.format("MM-DD-YYYY") &&
-						moment(endDate).format("MM-DD-YYYY") ==
+						moment(event.end).format("MM-DD-YYYY") ===
 						moment(new Date())
 							.add(1, "days")
 							.format("MM-DD-YYYY")
 					) {
-						if(timeFormatLen>0){
-							$("span.eaelec-event-date-end").html("- " + moment(endDate).format(timeFormate));
-						}
+						$("span.eaelec-event-date-end").html("- " + moment(endDate).format(timeFormate));
 					}
 					if (
-						moment(endDate).format("MM-DD-YYYY") >
+						moment(event.end).format("MM-DD-YYYY") >
 						moment(new Date())
 							.add(1, "days")
 							.format("MM-DD-YYYY")
@@ -105,26 +106,23 @@ var EventCalendar = function($scope, $) {
 					}
 
 					if (
-						moment(event.start).format("MM-DD-YYYY") >
+						moment(startDate).format("MM-DD-YYYY") >
 						moment(new Date())
 							.add(1, "days")
 							.format("MM-DD-YYYY") &&
-						moment(event.start).format("MM-DD-YYYY") == moment(endDate).format("MM-DD-YYYY")
+						moment(startDate).format("MM-DD-YYYY") === moment(event.end).format("MM-DD-YYYY")
 					) {
-						if(timeFormatLen>0){
-							$("span.eaelec-event-date-end").html("- " + moment(endDate).format(timeFormate));
-						}
-
+						$("span.eaelec-event-date-end").html("- " + moment(endDate).format(timeFormate));
 					}
 				}
 
 				$(".eaelec-modal-header h2").html(event.title);
 				$(".eaelec-modal-body p").html(event.extendedProps.description);
 				$(".eaelec-modal-footer a").attr("href", event.url);
-				if (event.external == "on") {
+				if (event.external === "on") {
 					$(".eaelec-modal-footer a").attr("target", "_blank");
 				}
-				if (event.nofollow == "on") {
+				if (event.nofollow === "on") {
 					$(".eaelec-modal-footer a").attr("rel", "nofollow");
 				}
 				if (event.url == "") {
