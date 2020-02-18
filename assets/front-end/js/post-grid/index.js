fbd1ec0eebd325ce8e177d51334e6164a096bfe2
Mahafuz <m.mahfuz.me@gmail.com>
2020-02-18 12:56:35 +0600

    Fixed & Added: Responsive controls are not working for mobile device & new layout mode grid added

--- assets/front-end/js/post-grid/index.js
+++ assets/front-end/js/post-grid/index.js
@@ -1,16 +1,19 @@
 var PostGrid = function($scope, $) {
-    var $gallery = $(".eael-post-appender", $scope).isotope({
-        itemSelector: ".eael-grid-post",
-        masonry: {
-            columnWidth: ".eael-post-grid-column",
+    var $gallery = $(".eael-post-appender", $scope),
+        $layout_mode = $gallery.data('layout-mode');
+        
+    if($layout_mode === 'masonry') {
+        $gallery.isotope({
+            itemSelector: ".eael-grid-post",
+            layoutMode: $layout_mode,
             percentPosition: true
-        }
-    });
+        });
 
-    // layout gal, while images are loading
-    $gallery.imagesLoaded().progress(function() {
-        $gallery.isotope("layout");
-    });
+        // layout gal, while images are loading
+        $gallery.imagesLoaded().progress(function() {
+            $gallery.isotope("layout");
+        });
+    }
 };
 
 jQuery(window).on("elementor/frontend/init", function() {
