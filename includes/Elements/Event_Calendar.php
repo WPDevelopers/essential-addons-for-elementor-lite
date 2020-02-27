5a96331d0f9ceda34d37d3d5dfa9857139d4556e
Manzur Ahammed <manzur.ahammed236@gmail.com>
2020-02-27 11:58:57 +0600

    add control in event calendar

--- includes/Elements/Event_Calendar.php
+++ includes/Elements/Event_Calendar.php
@@ -291,6 +291,85 @@ class Event_Calendar extends Widget_Base
 
         $this->end_controls_section();
 
+        //the events calendar
+
+        $this->start_controls_section(
+            'eael_event_the_events_calendar',
+            [
+                'label' => __('The Event Calendar', 'essential-addons-for-elementor-lite'),
+                'tab' => Controls_Manager::TAB_CONTENT,
+                'condition' => [
+                    'eael_event_calendar_type' => 'the_events_calendar',
+                ],
+            ]
+        );
+
+        $this->add_control(
+            'eael_the_events_calendar_fetch',
+            [
+                'label' => __('Get Events', 'essential-addons-for-elementor-lite'),
+                'type' => Controls_Manager::SELECT,
+                'label_block' => true,
+                'default' => ['all'],
+                'options' => [
+                    'all' => __('All', 'essential-addons-for-elementor-lite'),
+                    'date_range' => __('Date Range', 'essential-addons-for-elementor-lite'),
+                ],
+                'render_type' => 'none',
+            ]
+        );
+
+
+
+        $this->add_control(
+            'eael_the_events_calendar_start_date',
+            [
+                'label' => __('Start Date', 'essential-addons-for-elementor-lite'),
+                'type' => Controls_Manager::DATE_TIME,
+                'default' => date('Y-m-d H:i', current_time('timestamp', 0)),
+                'condition' => [
+                    'eael_the_events_calendar_fetch' => 'date_range',
+                ],
+            ]
+        );
+
+        $this->add_control(
+            'eael_the_events_calendar_end_date',
+            [
+                'label' => __('End Date', 'essential-addons-for-elementor-lite'),
+                'type' => Controls_Manager::DATE_TIME,
+                'default' => date('Y-m-d H:i', strtotime("+6 months", current_time('timestamp', 0))),
+                'condition' => [
+                    'eael_the_events_calendar_fetch' => 'date_range',
+                ],
+            ]
+        );
+
+        $this->add_control(
+            'eael_the_events_calendar_category',
+            [
+                'label' => __('Event Category', 'essential-addons-for-elementor-lite'),
+                'type' => Controls_Manager::SELECT2,
+                'multiple' => true,
+                'label_block' => true,
+                'default' => [],
+                'options' => $this->eael_get_tags(['taxonomy' => 'tribe_events_cat', 'hide_empty' => false]),
+            ]
+        );
+
+        $this->add_control(
+            'eael_the_events_calendar_max_result',
+            [
+                'label' => __('Max Result', 'essential-addons-for-elementor-lite'),
+                'type' => Controls_Manager::NUMBER,
+                'min' => 1,
+                'max' => 200,
+                'default' => 20,
+            ]
+        );
+
+        $this->end_controls_section();
+
         do_action('eael/event-calendar/source/control', $this);
 
         $this->start_controls_section(
