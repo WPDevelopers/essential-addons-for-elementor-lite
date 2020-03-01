b797f3a743044628db984fc6ce732d9e1c991cbe
mahafuz <m.mahfuz.me@gmail.com>
2020-03-01 14:09:23 +0600

    Post grid warning fixed

--- includes/Template/Content/Post_Grid.php
+++ includes/Template/Content/Post_Grid.php
@@ -10,7 +10,6 @@ trait Post_Grid
 {
     public static function render_template_($args, $settings)
     {
-        error_log(print_r($settings, 1));
 
         $query = new \WP_Query($args);
          
