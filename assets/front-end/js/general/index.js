5b2a23b7ff60a8eb5eddbdb9a2db165414d5742a
Nazmul Sabuz <naz.sabuz@gmail.com>
2020-03-16 17:48:15 +0600

    working on: pro listing in lite

--- assets/front-end/js/general/index.js
+++ assets/front-end/js/general/index.js
@@ -1,7 +1,34 @@
 (function($) {
-    window.isEditMode = false;
+	window.isEditMode = false;
 
-    $(window).on("elementor/frontend/init", function() {
-        window.isEditMode = elementorFrontend.isEditMode();
-    });
+	$(window).on("elementor/frontend/init", function() {
+		window.isEditMode = elementorFrontend.isEditMode();
+
+		if (isEditMode) {
+			var interval = setInterval(function() {
+				if (parent.document.querySelectorAll(".elementor-element--promotion").length > 0) {
+					parent.document.querySelectorAll(".elementor-element--promotion").forEach(function(widget) {
+						widget.addEventListener("click", function(e) {
+							var dialog = parent.document.querySelector("#elementor-element--promotion__dialog");
+
+							if (dialog.querySelector("#elementor-element--promotion__dialog__title").innerHTML.match("EA")) {
+								dialog.querySelector(".dialog-buttons-action").innerHTML = "Go Pro";
+
+								dialog.querySelector(".dialog-action").addEventListener("click", function(e) {
+                                    return false
+                                    e.preventDefault();
+                                    
+                                    console.log(e)
+								});
+							} else {
+								dialog.querySelector(".dialog-buttons-action").innerHTML = "See it in Action";
+							}
+						});
+					});
+
+					clearTimeout(interval);
+				}
+			}, 500);
+		}
+	});
 })(jQuery);
