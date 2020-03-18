0270246a9df793ddb256169e4e86e1d4868340b8
Nazmul Sabuz <naz.sabuz@gmail.com>
2020-03-18 14:32:10 +0600

    Updated: PRO Button

--- assets/front-end/js/general/index.js
+++ assets/front-end/js/general/index.js
@@ -12,16 +12,34 @@
 							var dialog = parent.document.querySelector("#elementor-element--promotion__dialog");
 
 							if (dialog.querySelector("#elementor-element--promotion__dialog__title").innerHTML.match("EA")) {
-								dialog.querySelector(".dialog-buttons-action").innerHTML = "Go Pro";
+								dialog.querySelector(".dialog-buttons-action").style.display = "none";
 
-								dialog.querySelector(".dialog-action").addEventListener("click", function(e) {
-                                    return false
-                                    e.preventDefault();
-                                    
-                                    console.log(e)
-								});
+								if (dialog.querySelector(".ea-dialog-buttons-action") === null) {
+									var button = document.createElement("a");
+									var buttonText = document.createTextNode("GO PRO");
+
+									button.setAttribute("href", "https://essential-addons.com/elementor/#pricing");
+									button.setAttribute("target", "_blank");
+									button.classList.add(
+										"dialog-button",
+										"dialog-action",
+										"dialog-buttons-action",
+										"elementor-button",
+										"elementor-button-success",
+										"ea-dialog-buttons-action"
+									);
+									button.appendChild(buttonText);
+
+									dialog.querySelector(".dialog-buttons-action").insertAdjacentHTML("afterend", button.outerHTML);
+								} else {
+									dialog.querySelector(".ea-dialog-buttons-action").style.display = "";
+								}
 							} else {
-								dialog.querySelector(".dialog-buttons-action").innerHTML = "See it in Action";
+								dialog.querySelector(".dialog-buttons-action").style.display = "";
+
+								if (dialog.querySelector(".ea-dialog-buttons-action") !== null) {
+									dialog.querySelector(".ea-dialog-buttons-action").style.display = "none";
+								}
 							}
 						});
 					});
