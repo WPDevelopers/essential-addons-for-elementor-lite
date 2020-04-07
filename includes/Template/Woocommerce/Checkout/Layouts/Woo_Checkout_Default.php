01abfc422bdd63291290e23945d1fb0f35348fb8
Sumaiya Siddika <to.sumaiyasiddika@gmail.com>
2020-04-07 13:37:37 +0600

    Added some setting and design

--- includes/Template/Woocommerce/Checkout/Layouts/Woo_Checkout_Default.php
+++ includes/Template/Woocommerce/Checkout/Layouts/Woo_Checkout_Default.php
@@ -11,26 +11,30 @@ trait Woo_Checkout_Default {
     use \Essential_Addons_Elementor\Template\Woocommerce\Checkout\Checkout_Login;
     use \Essential_Addons_Elementor\Template\Woocommerce\Checkout\Checkout_Coupon;
 
-    public static function render_default_template_($checkout, $settings) { ?>
-        <h3 id="order_review_heading woo-checkout-section-title"><?php esc_html_e( 'Your order', 'woocommerce' ); ?></h3>
+    public static function render_default_template_($checkout, $settings) {
+        do_action('woocommerce_checkout_before_order_review_heading');
+        ?>
+        <h3 id="order_review_heading" class="woo-checkout-section-title"><?php esc_html_e( 'Your order', 'essential-addons-for-elementor-lite' ); ?></h3>
+
         <div class="eael-order-review-wapper">
-            <?php self::order_review_template(); ?>
+            <?php self::order_review_template($settings); ?>
         </div>
 
-        <?php //    Order Details Login
+        <?php
         // If checkout registration is disabled and not logged in, the user cannot checkout.
         if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_required() && ! is_user_logged_in() ) {
             echo esc_html( apply_filters( 'woocommerce_checkout_must_be_logged_in_message', __( 'You must be logged in to checkout.', 'woocommerce' ) ) );
             return;
         }
 
+        // Login
         self::checkout_login_template();
 
-        // Order Details Coupon
+        // Coupon
         self::checkout_coupon_template();
 
         ?>
-
+<!--        --><?php //do_action( 'woocommerce_before_checkout_form', $checkout ); ?>
         <form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url( wc_get_checkout_url() ); ?>" enctype="multipart/form-data">
 
             <?php if ( $checkout->get_checkout_fields() ) : ?>
@@ -51,13 +55,16 @@ trait Woo_Checkout_Default {
 
             <?php endif; ?>
 
-            <div id="order_review" class="woocommerce-checkout-review-order">
+            <div class="woo-checkout-payment">
+                <h3 id="payment-title" class="woo-checkout-section-title"><?php esc_html_e( 'Payment Methods', 'essential-addons-for-elementor-lite' ); ?></h3>
+
                 <?php woocommerce_checkout_payment(); ?>
             </div>
 
         </form>
 
         <?php
+        do_action('woocommerce_after_checkout_form', $checkout);
 
     }
 }
