998dbaf6f5ba8f72e7804271cbe78eb7ae8ac3a8
Nazmul Sabuz <naz.sabuz@gmail.com>
2020-04-12 17:35:12 +0600

    working on: implementing es6, js hooks

---
+++ src/common.js
@@ -0,0 +1,13 @@
+import { createHooks } from "@wordpress/hooks";
+
+window.ea = {
+	hooks: createHooks(),
+	isEditMode: false,
+};
+
+jQuery(window).on("elementor/frontend/init", function () {
+	window.ea.isEditMode = elementorFrontend.isEditMode();
+
+	// hooks
+	ea.hooks.doAction("ea.frontend.init", ea.isEditMode);
+});
