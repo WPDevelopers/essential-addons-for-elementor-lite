8c29e05202d301103c57511911394e47a0e62f69
Sumaiya Siddika <to.sumaiyasiddika@gmail.com>
2020-04-26 16:33:21 +0600

    Update coupon and login template

--- includes/Template/Woocommerce/Checkout/Woo_Checkout_Helper.php
+++ includes/Template/Woocommerce/Checkout/Woo_Checkout_Helper.php
@@ -238,7 +238,7 @@ trait Woo_Checkout_Helper {
 	/**
 	 * Show the coupon.
 	 */
-	public static function checkout_coupon_template() {
+	public static function ea_coupon_template() {
 		$settings = self::get_settings();
 
 		?>
@@ -253,21 +253,68 @@ trait Woo_Checkout_Helper {
 	/**
 	 * Show the login.
 	 */
-	public static function checkout_login_template() {
+	public static function ea_login_template(){
+		ob_start();
 		$settings = self::get_settings();
-
-		if ( is_user_logged_in() || 'no' === get_option( 'woocommerce_enable_checkout_login_reminder' ) ) {
+		$class = '';
+		if(\Elementor\Plugin::$instance->editor->is_edit_mode() || is_user_logged_in()){
+			$class = 'woo-checkout-login-editor';
+		}elseif ('no' === get_option( 'woocommerce_enable_checkout_login_reminder')){
 			return;
 		}
-
 		?>
-		<div class="woo-checkout-login">
-			<div class="ea-login-icon">
+        <div class="woo-checkout-login <?php echo $class; ?>">
+            <div class="ea-login-icon">
 				<?php Icons_Manager::render_icon( $settings['ea_woo_checkout_login_icon'], [ 'aria-hidden' => 'true' ] ); ?>
-			</div>
-			<?php wc_get_template('checkout/form-login.php', array('checkout' => WC()->checkout(),)); ?>
-		</div>
-	<?php }
+            </div>
+            <div class="woocommerce-form-login-toggle">
+				<?php wc_print_notice( apply_filters( 'woocommerce_checkout_login_message', esc_html__( 'Returning customer?', 'woocommerce' ) ) . ' <a href="#" class="showlogin">' . esc_html__( 'Click here to login', 'woocommerce' ) . '</a>', 'notice' ); ?>
+            </div>
+
+			<?php
+			$message  = esc_html__( 'If you have shopped with us before, please enter your details below. If you are a new customer, please proceed to the Billing section.', 'woocommerce' );
+			$redirect = wc_get_checkout_url();
+			$hidden   = true;
+			?>
+            <form class="woocommerce-form woocommerce-form-login login" method="post" <?php echo ( $hidden ) ? 'style="display:none;"' : ''; ?>>
+
+				<?php do_action( 'woocommerce_login_form_start' ); ?>
+
+				<?php echo ( $message ) ? wpautop( wptexturize( $message ) ) : ''; // @codingStandardsIgnoreLine ?>
+
+                <p class="form-row form-row-first">
+                    <label for="username"><?php esc_html_e( 'Username or email', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
+                    <input type="text" class="input-text" name="username" id="username" autocomplete="username" />
+                </p>
+                <p class="form-row form-row-last">
+                    <label for="password"><?php esc_html_e( 'Password', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
+                    <input class="input-text" type="password" name="password" id="password" autocomplete="current-password" />
+                </p>
+                <div class="clear"></div>
+
+				<?php do_action( 'woocommerce_login_form' ); ?>
+
+                <p class="form-row">
+                    <label class="woocommerce-form__label woocommerce-form__label-for-checkbox woocommerce-form-login__rememberme">
+                        <input class="woocommerce-form__input woocommerce-form__input-checkbox" name="rememberme" type="checkbox" id="rememberme" value="forever" /> <span><?php esc_html_e( 'Remember me', 'woocommerce' ); ?></span>
+                    </label>
+					<?php wp_nonce_field( 'woocommerce-login', 'woocommerce-login-nonce' ); ?>
+                    <input type="hidden" name="redirect" value="<?php echo esc_url( $redirect ); ?>" />
+                    <button type="submit" class="woocommerce-button button woocommerce-form-login__submit" name="login" value="<?php esc_attr_e( 'Login', 'woocommerce' ); ?>"><?php esc_html_e( 'Login', 'woocommerce' ); ?></button>
+                </p>
+                <p class="lost_password">
+                    <a href="<?php echo esc_url( wp_lostpassword_url() ); ?>"><?php esc_html_e( 'Lost your password?', 'woocommerce' ); ?></a>
+                </p>
+
+                <div class="clear"></div>
+
+				<?php do_action( 'woocommerce_login_form_end' ); ?>
+
+            </form>
+
+        </div>
+		<?php echo ob_get_clean();
+	}
 
 	/**
 	 * Show the login.
@@ -469,76 +516,12 @@ trait Woo_Checkout_Helper {
 	public function ea_woo_checkout_add_actions($settings) {
 
 		if( 'yes' == $settings['ea_section_woo_login_show'] ) :
-			add_action( 'woocommerce_before_checkout_form', [ $this, 'ea_render_login' ], 10 );
+			add_action( 'woocommerce_before_checkout_form', [ $this, 'ea_login_template' ], 10 );
 		endif;
 
-		add_action( 'woocommerce_before_checkout_form', [ $this, 'ea_render_login' ], 10 );
-		add_action( 'woocommerce_before_checkout_form', [ $this, 'checkout_coupon_template' ], 10 );
+		add_action( 'woocommerce_before_checkout_form', [ $this, 'ea_login_template' ], 10 );
+		add_action( 'woocommerce_before_checkout_form', [ $this, 'ea_coupon_template' ], 10 );
 		add_action( 'woocommerce_before_checkout_form', [ $this, 'checkout_order_review_template' ], 9 );
-	}
-
-
-	public static function ea_render_login(){
-		ob_start();
-		$settings = self::get_settings();
-		$class = '';
-		if(\Elementor\Plugin::$instance->editor->is_edit_mode() || is_user_logged_in()){
-		    $class = 'woo-checkout-login-editor';
-        }elseif ('no' === get_option( 'woocommerce_enable_checkout_login_reminder')){
-		    return;
-        }
-		?>
-        <div class="woo-checkout-login <?php echo $class; ?>">
-            <div class="ea-login-icon">
-				<?php Icons_Manager::render_icon( $settings['ea_woo_checkout_login_icon'], [ 'aria-hidden' => 'true' ] ); ?>
-            </div>
-			<div class="woocommerce-form-login-toggle">
-            <?php wc_print_notice( apply_filters( 'woocommerce_checkout_login_message', esc_html__( 'Returning customer?', 'woocommerce' ) ) . ' <a href="#" class="showlogin">' . esc_html__( 'Click here to login', 'woocommerce' ) . '</a>', 'notice' ); ?>
-        </div>
-
-        <?php
-        $message  = esc_html__( 'If you have shopped with us before, please enter your details below. If you are a new customer, please proceed to the Billing section.', 'woocommerce' );
-        $redirect = wc_get_checkout_url();
-        $hidden   = true;
-        ?>
-        <form class="woocommerce-form woocommerce-form-login login" method="post" <?php echo ( $hidden ) ? 'style="display:none;"' : ''; ?>>
-
-            <?php do_action( 'woocommerce_login_form_start' ); ?>
-
-            <?php echo ( $message ) ? wpautop( wptexturize( $message ) ) : ''; // @codingStandardsIgnoreLine ?>
-
-            <p class="form-row form-row-first">
-                <label for="username"><?php esc_html_e( 'Username or email', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
-                <input type="text" class="input-text" name="username" id="username" autocomplete="username" />
-            </p>
-            <p class="form-row form-row-last">
-                <label for="password"><?php esc_html_e( 'Password', 'woocommerce' ); ?>&nbsp;<span class="required">*</span></label>
-                <input class="input-text" type="password" name="password" id="password" autocomplete="current-password" />
-            </p>
-            <div class="clear"></div>
-
-            <?php do_action( 'woocommerce_login_form' ); ?>
-
-            <p class="form-row">
-                <label class="woocommerce-form__label woocommerce-form__label-for-checkbox woocommerce-form-login__rememberme">
-                    <input class="woocommerce-form__input woocommerce-form__input-checkbox" name="rememberme" type="checkbox" id="rememberme" value="forever" /> <span><?php esc_html_e( 'Remember me', 'woocommerce' ); ?></span>
-                </label>
-                <?php wp_nonce_field( 'woocommerce-login', 'woocommerce-login-nonce' ); ?>
-                <input type="hidden" name="redirect" value="<?php echo esc_url( $redirect ); ?>" />
-                <button type="submit" class="woocommerce-button button woocommerce-form-login__submit" name="login" value="<?php esc_attr_e( 'Login', 'woocommerce' ); ?>"><?php esc_html_e( 'Login', 'woocommerce' ); ?></button>
-            </p>
-            <p class="lost_password">
-                <a href="<?php echo esc_url( wp_lostpassword_url() ); ?>"><?php esc_html_e( 'Lost your password?', 'woocommerce' ); ?></a>
-            </p>
-
-            <div class="clear"></div>
-
-            <?php do_action( 'woocommerce_login_form_end' ); ?>
-
-        </form>
-
-        </div>
-		<?php echo ob_get_clean();
 	}
 
 }
