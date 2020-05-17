5731dfc11af3174bf2a4002768cfafdc2ac9d75b
Nazmul Sabuz <naz.sabuz@gmail.com>
2020-05-18 05:08:38 +0600

    fix: assets path

--- config.php
+++ config.php
@@ -760,10 +760,18 @@ $config = [
             'class'      => '\Essential_Addons_Elementor\Elements\Woo_Checkout',
             'dependency' => [
                 'css' => [
-                    EAEL_PLUGIN_PATH . DIRECTORY_SEPARATOR . 'assets/front-end/css/view/woo-checkout.css',
+                    [
+                        'file'    => EAEL_PLUGIN_PATH . 'assets/front-end/css/view/woo-checkout.min.css',
+                        'type'    => 'self',
+                        'context' => 'view',
+                    ],
                 ],
                 'js'  => [
-                    EAEL_PLUGIN_PATH . DIRECTORY_SEPARATOR . 'assets/front-end/js/view/woo-checkout.js',
+                    [
+                        'file'    => EAEL_PLUGIN_PATH . 'assets/front-end/js/view/woo-checkout.min.js',
+                        'type'    => 'self',
+                        'context' => 'view',
+                    ],
                 ],
             ],
         ],
