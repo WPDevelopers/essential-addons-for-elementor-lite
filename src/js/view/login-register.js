f4dcc93ba999593010c2f19d8debc47f4002923c
Kamal Ahmed <kamalacca@gmail.com>
2020-06-30 18:08:04 +0600

    added js toggle for the forms

---
+++ src/js/view/login-register.js
@@ -0,0 +1,22 @@
+jQuery(window).on("elementor/frontend/init", function () {
+    const EALoginRegister = function ($scope, $) {
+        const $regToggleBtn = $scope.find("#eael-lr-reg-toggle");
+        const $loginToggleBtn = $scope.find("#eael-lr-login-toggle");
+        const $loginFormWrapper = $scope.find("#eael-login-form-wrapper");
+        const $regFormWrapper = $scope.find("#eael-register-form-wrapper");
+        $(document).on('click', "#eael-lr-reg-toggle", function (e) {
+            // alert('got clicked on reg btn ');
+            e.preventDefault();
+            $loginFormWrapper.hide();
+            $regFormWrapper.fadeIn();
+        });
+        $(document).on('click', "#eael-lr-login-toggle", function (e) {
+            // alert('got clicked on login btn ');
+            e.preventDefault();
+            $regFormWrapper.hide();
+            $loginFormWrapper.fadeIn();
+        });
+    };
+    elementorFrontend.hooks.addAction("frontend/element_ready/eael-login-register.default", EALoginRegister);
+});
+
