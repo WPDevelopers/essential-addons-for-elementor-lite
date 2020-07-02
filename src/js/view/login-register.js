aaacb59d639a3e887240223afca551bf42c3e0ee
Kamal Ahmed <kamalacca@gmail.com>
2020-07-02 17:07:13 +0600

    completed initial widget for testing

--- src/js/view/login-register.js
+++ src/js/view/login-register.js
@@ -2,18 +2,23 @@ jQuery(window).on("elementor/frontend/init", function () {
     const EALoginRegister = function ($scope, $) {
         const $loginFormWrapper = $scope.find("#eael-login-form-wrapper");
         const $regFormWrapper = $scope.find("#eael-register-form-wrapper");
-        $(document).on('click', "#eael-lr-reg-toggle", function (e) {
-            // alert('got clicked on reg btn ');
-            e.preventDefault();
-            $loginFormWrapper.hide();
-            $regFormWrapper.fadeIn();
-        });
-        $(document).on('click', "#eael-lr-login-toggle", function (e) {
-            // alert('got clicked on login btn ');
-            e.preventDefault();
-            $regFormWrapper.hide();
-            $loginFormWrapper.fadeIn();
-        });
+        const regLinkAction = $('#eael-lr-reg-toggle').data('action');
+        const loginLinkAction = $('#eael-lr-login-toggle').data('action');
+
+        if ('form' === regLinkAction) {
+            $(document).on('click', '#eael-lr-reg-toggle', function (e) {
+                e.preventDefault();
+                $loginFormWrapper.hide();
+                $regFormWrapper.fadeIn();
+            });
+        }
+        if ('form' === loginLinkAction) {
+            $(document).on('click', '#eael-lr-login-toggle', function (e) {
+                e.preventDefault();
+                $regFormWrapper.hide();
+                $loginFormWrapper.fadeIn();
+            });
+        }
     };
     elementorFrontend.hooks.addAction("frontend/element_ready/eael-login-register.default", EALoginRegister);
 });
