126fef791851d5546758a6bbfeeb10b36380cc62
Nazmul Sabuz <naz.sabuz@gmail.com>
2020-07-06 12:37:04 +0600

    Updated: admin txt

--- includes/templates/admin/tools.php
+++ includes/templates/admin/tools.php
@@ -25,10 +25,10 @@
                         <div class="eael-admin-block-header-icon">
                             <img src="<?php echo EAEL_PLUGIN_URL . 'assets/admin/images/icon-clean-cache.svg'; ?>" alt="essential-addons-tools-clean-cache">
                         </div>
-                        <h4 class="eael-admin-title"><?php _e('Assets Embed method', 'essential-addons-for-elementor-lite');?></h4>
+                        <h4 class="eael-admin-title"><?php _e('Assets Embed Method', 'essential-addons-for-elementor-lite');?></h4>
                     </header>
                     <div class="eael-admin-block-content">
-                        <p><?php _e('Essential Addons styles & scripts are saved in Uploads folder. This option will clear all those generated files.', 'essential-addons-for-elementor-lite');?></p>
+                        <p><?php _e('Configure the Essential Addons assets embed method. Keep it as default (recommended).', 'essential-addons-for-elementor-lite');?></p>
 
 
                         <div class="eael-admin-inside-select-block">
@@ -37,7 +37,9 @@
                             </div>
 
                             <div class="eael-admin-select-block-select">
-                                <a href="<?php echo esc_url(admin_url('admin.php?page=elementor#tab-advanced')); ?>" target="_blank" class="button eael-btn"><?php _e('Configure Settings', 'essential-addons-for-elementor-lite');?></a>
+                                <a href="<?php echo esc_url(admin_url('admin.php?page=elementor#tab-advanced')); ?>" target="_blank" id="eael-css-print-method" class="button eael-btn"><?php _e('Configure Settings', 'essential-addons-for-elementor-lite');?></a>
+                                
+                                <p class="quick-tools-description"><?php _e('CSS Print Method is handled by Elementor Settings itself. Use External CSS Files for better performance (recommended).', 'essential-addons-for-elementor-lite');?></p>
                             </div>
                         </div>
 
@@ -51,7 +53,8 @@
                                     <option value="internal" <?php echo get_option('eael_js_print_method', 'external') == 'internal' ? 'selected' : '' ?>><?php _e('Internal Embedding', 'essential-addons-for-elementor-lite');?></option>
                                 </select>
 
-                                <p class="quick-tools-description"><?php _e('Use external CSS files for all generated stylesheets. Choose this setting for better performance (recommended).', 'essential-addons-for-elementor-lite');?></p>
+                                <p class="quick-tools-description"><?php _e('Use external JS files for all generated scripts. Choose this setting for better performance (recommended).', 'essential-addons-for-elementor-lite');?></p>
+                                <p class="quick-tools-description"><?php _e('Use internal JS that is embedded in the head of the page. For troubleshooting server configuration conflicts and managing development environments.', 'essential-addons-for-elementor-lite');?></p>
                             </div>
                         </div>
 
