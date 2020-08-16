706fbe43ae86f11b3aaa080bd4208107a12743ae
Kamal Ahmed <kamalacca@gmail.com>
2020-08-16 11:12:07 +0600

    fixed recaptcha not working for multiple forms

--- src/js/view/login-register.js
+++ src/js/view/login-register.js
@@ -1,5 +1,9 @@
 ea.hooks.addAction("init", "ea", () => {
     const EALoginRegister = function ($scope, $) {
+
+        const $wrap = $scope.find('.eael-login-registration-wrapper');// cache wrapper
+        //const recaptchaEnabled = $wrap.data('is-recaptcha');
+        const widgetId = $wrap.data('widget-id');
         const $loginFormWrapper = $scope.find("#eael-login-form-wrapper");
         const $regFormWrapper = $scope.find("#eael-register-form-wrapper");
         const $regLinkAction = $scope.find('#eael-lr-reg-toggle');
@@ -25,14 +29,39 @@ ea.hooks.addAction("init", "ea", () => {
         let pass_shown = false;
         $(document).on('click', '#wp-hide-pw', function (e) {
             let $icon = $(this).find('span');// cache
-            if (pass_shown){
+            if (pass_shown) {
                 $passField.attr('type', 'password');
                 $icon.removeClass('dashicons-hidden').addClass('dashicons-visibility');
                 pass_shown = false;
-            }else{
+            } else {
                 $passField.attr('type', 'text');
                 $icon.removeClass('dashicons-visibility').addClass('dashicons-hidden');
                 pass_shown = true;
+            }
+        });
+
+
+        // Recaptcha
+        function onloadLRcb() {
+            let loginRecaptchaNode = document.getElementById('login-recaptcha-node-' + widgetId);
+            let registerRecaptchaNode = document.getElementById('register-recaptcha-node-' + widgetId);
+
+            if (loginRecaptchaNode) {
+                grecaptcha.render(loginRecaptchaNode, {
+                    'sitekey': '6Le0KLsZAAAAAHjjwmR3xKsxzQ2toY35Z8ruhp3x',
+                });
+            }
+            if (registerRecaptchaNode) {
+                grecaptcha.render(registerRecaptchaNode, {
+                    'sitekey': '6Le0KLsZAAAAAHjjwmR3xKsxzQ2toY35Z8ruhp3x',
+                });
+            }
+        }
+
+
+        $(window).load(function () {
+            if (grecaptcha){
+                onloadLRcb();
             }
         });
     };
