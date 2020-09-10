ee7cd39bf34deb20cd253bc56dc403c764fce7fb
manzurahammed <manzur.ahammed236@gmail.com>
2020-09-10 15:27:16 +0600

    fixed group product add to cart issue

--- src/js/view/product-grid.js
+++ src/js/view/product-grid.js
@@ -2,37 +2,37 @@ var ProductGrid = function ($scope, $) {
 
 	// pagination
 
-	$('.eael-woo-pagination', $scope).on('click', 'a', function(e) {
+	$('.eael-woo-pagination', $scope).on('click', 'a', function (e) {
 		e.preventDefault();
 
-		var $this = $(this),
-			nth  = $this.data('pnumber'),
-			lmt  = $this.data('plimit'),
-			ajax_url = localize.ajaxurl,
-			args = $this.data('args'),
-			settings = $this.data('settings'),
-			widgetid = $this.data('widgetid'),
-			widgetclass = ".elementor-element-"+widgetid;
+		var $this       = $(this),
+		    nth         = $this.data('pnumber'),
+		    lmt         = $this.data('plimit'),
+		    ajax_url    = localize.ajaxurl,
+		    args        = $this.data('args'),
+		    settings    = $this.data('settings'),
+		    widgetid    = $this.data('widgetid'),
+		    widgetclass = ".elementor-element-" + widgetid;
 
 
 		$.ajax({
-			url		:ajax_url,
-			type	:'post',
-			data	:{
-				action:'woo_product_pagination_product',
-				number:nth,
-				limit:lmt,
-				args:args,
-				settings:settings
+			url: ajax_url,
+			type: 'post',
+			data: {
+				action: 'woo_product_pagination_product',
+				number: nth,
+				limit: lmt,
+				args: args,
+				settings: settings
 			},
 			// beforeSend	: function(){
 			// 	$(widgetclass+" .eael-product-grid .products").html("<li style='text-align:center;'>Loading please " +
 			// 		"wait...!</li>");
 			// },
-			success :function(response){
+			success: function (response) {
 				// console.log(response);
-				$(widgetclass+" .eael-product-grid .products").html(response);
-				$(widgetclass+" .woocommerce-product-gallery").each(function () {
+				$(widgetclass + " .eael-product-grid .products").html(response);
+				$(widgetclass + " .woocommerce-product-gallery").each(function () {
 					$(this).wc_product_gallery();
 				});
 
@@ -40,21 +40,21 @@ var ProductGrid = function ($scope, $) {
 		});
 
 		$.ajax({
-			url		:ajax_url,
-			type	:'post',
-			data	:{
-				action:'woo_product_pagination',
-				number:nth,
-				limit:lmt,
-				args:args,
-				settings:settings
+			url: ajax_url,
+			type: 'post',
+			data: {
+				action: 'woo_product_pagination',
+				number: nth,
+				limit: lmt,
+				args: args,
+				settings: settings
 			},
 			// beforeSend	: function(){
 			// 	$(widgetclass+" .eael-product-grid .products").html("<li style='text-align:center;'>Loading please " +
 			// 		"wait...!</li>");
 			// },
-			success :function(response){
-				$(widgetclass+" .eael-product-grid .eael-woo-pagination").html(response);
+			success: function (response) {
+				$(widgetclass + " .eael-product-grid .eael-woo-pagination").html(response);
 			}
 		});
 
@@ -64,27 +64,43 @@ var ProductGrid = function ($scope, $) {
 	// handle add to cart for quick view
 	$scope.on('click', '.eael-product-popup-details .single_add_to_cart_button', function (e) {
 		e.preventDefault();
-		var $this = $(this),
-		    product_id = $(this).val(),
+		var $this        = $(this),
+		    product_id   = $(this).val(),
 		    variation_id = $('input[name="variation_id"]').val() || '',
-		    group = [],
-		    quantity = $('input[name="quantity"]').val();
-
+		    quantity     = $('input[name="quantity"]').val(),
+		    items        = $this.closest('form.cart.grouped_form'),
+		    product_data = [];
+		items            = items.serializeArray();
+		if (items.length > 0) {
+			items.forEach((item, index) => {
+				var p_id = parseInt(item.name.replace(/[^\d.]/g, ''), 10);
+				if (item.name.indexOf('quantity[') >= 0 && item.value != '' && p_id > 0) {
+					product_data[product_data.length] = {
+						'product_id': p_id,
+						'quantity': item.value,
+						'variation_id': 0,
+					};
+				}
+			});
+		} else {
+			product_data[0] = {
+				'product_id': product_id,
+				'quantity': quantity,
+				'variation_id': variation_id,
+			}
+		}
 		$this.removeClass('eael-addtocart-added');
 		$this.addClass('eael-addtocart-loading');
 		$.ajax({
-			url		:localize.ajaxurl,
-			type	:'post',
-			data	:{
-				action:'eael_product_add_to_cart',
-				product_id:product_id,
-				variation_id:variation_id,
-				quantity:quantity,
-				group:group,
-				eael_add_to_cart_nonce:localize.nonce
+			url: localize.ajaxurl,
+			type: 'post',
+			data: {
+				action: 'eael_product_add_to_cart',
+				product_data: product_data,
+				eael_add_to_cart_nonce: localize.nonce
 			},
-			success :function(response){
-				if(response.success){
+			success: function (response) {
+				if (response.success) {
 					$(document.body).trigger('wc_fragment_refresh');
 					$this.removeClass('eael-addtocart-loading');
 					$this.addClass('eael-addtocart-added');
