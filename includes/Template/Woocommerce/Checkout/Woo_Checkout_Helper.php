f6b6414eb2b2a6b93347cbec1cf0e5a5b8009397
manzurahammed <manzur.ahammed236@gmail.com>
2020-09-24 16:13:59 +0600

    fix title issue and add message

--- includes/Template/Woocommerce/Checkout/Woo_Checkout_Helper.php
+++ includes/Template/Woocommerce/Checkout/Woo_Checkout_Helper.php
@@ -635,7 +635,7 @@ trait Woo_Checkout_Helper {
 
                 <h3 id="ship-to-different-address">
                     <label class="woocommerce-form__label woocommerce-form__label-for-checkbox checkbox">
-                        <input id="ship-to-different-address-checkbox" class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox" <?php checked( apply_filters( 'woocommerce_ship_to_different_address_checked', 'shipping' === get_option( 'woocommerce_ship_to_destination' ) ? 1 : 0 ), 1 ); ?> type="checkbox" name="ship_to_different_address" value="1" /> <span><?php echo $settings['ea_woo_checkout_billing_title']; ?></span>
+                        <input id="ship-to-different-address-checkbox" class="woocommerce-form__input woocommerce-form__input-checkbox input-checkbox" <?php checked( apply_filters( 'woocommerce_ship_to_different_address_checked', 'shipping' === get_option( 'woocommerce_ship_to_destination' ) ? 1 : 0 ), 1 ); ?> type="checkbox" name="ship_to_different_address" value="1" /> <span><?php echo $settings['ea_woo_checkout_shipping_title']; ?></span>
                     </label>
                 </h3>
 
