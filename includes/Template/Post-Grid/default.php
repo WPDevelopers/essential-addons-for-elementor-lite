8879f1edee9b35f5a72e84a5d226031e599b53e7
mahafuz <m.mahfuz.me@gmail.com>
2020-09-28 12:39:32 +0600

    Fixed: EA Post Grid: Array to String Conversion error

--- includes/Template/Post-Grid/default.php
+++ includes/Template/Post-Grid/default.php
@@ -335,7 +335,7 @@ if ( $settings['eael_post_grid_preset_style'] === 'two' ) {
                     if ( isset( $settings['eael_post_grid_bg_hover_icon_new']['url'] ) ) {
                         echo '<img src="' . esc_url( $settings['eael_post_grid_bg_hover_icon_new']['url'] ) . '" alt="' . esc_attr( get_post_meta( $settings['eael_post_grid_bg_hover_icon_new']['id'], '_wp_attachment_image_alt', true ) ) . '" />';
                     } else {
-                        if( !empty($settings['eael_post_grid_bg_hover_icon_new']['library']) ) {
+                        if( ($settings['eael_post_grid_bg_hover_icon_new']['library']) == 'svg' ) {
                             echo '<img src="' . esc_url($settings['eael_post_grid_bg_hover_icon_new']['value']['url']) . '" alt="' . esc_attr( get_post_meta( $settings['eael_post_grid_bg_hover_icon_new']['value']['id'], '_wp_attachment_image_alt', true ) ) . '" />';
                         }else {
                             echo '<i class="' . $settings['eael_post_grid_bg_hover_icon_new']['value'] . '" aria-hidden="true"></i>';
