6f16518665a076eecb9175acfe05d35d178c1995
mahafuz <m.mahfuz.me@gmail.com>
2020-09-28 12:15:23 +0600

    Fixed: EA Post Grid: Array to String Conversion error

--- includes/Template/Post-Grid/default.php
+++ includes/Template/Post-Grid/default.php
@@ -335,7 +335,11 @@ if ( $settings['eael_post_grid_preset_style'] === 'two' ) {
                     if ( isset( $settings['eael_post_grid_bg_hover_icon_new']['url'] ) ) {
                         echo '<img src="' . esc_url( $settings['eael_post_grid_bg_hover_icon_new']['url'] ) . '" alt="' . esc_attr( get_post_meta( $settings['eael_post_grid_bg_hover_icon_new']['id'], '_wp_attachment_image_alt', true ) ) . '" />';
                     } else {
-                        echo '<i class="' . $settings['eael_post_grid_bg_hover_icon_new']['value'] . '" aria-hidden="true"></i>';
+                        if( !empty($settings['eael_post_grid_bg_hover_icon_new']['library']) ) {
+                            echo '<img src="' . esc_url($settings['eael_post_grid_bg_hover_icon_new']['value']['url']) . '" alt="' . esc_attr( get_post_meta( $settings['eael_post_grid_bg_hover_icon_new']['id'], '_wp_attachment_image_alt', true ) ) . '" />';
+                        }else {
+                            echo '<i class="' . $settings['eael_post_grid_bg_hover_icon_new']['value'] . '" aria-hidden="true"></i>';
+                        }
                     }
                     echo '<a
                             href="' . get_the_permalink() . '"
