98888aa7b199f413c9c407871b1885703bbf8d23
Kamal Ahmed <kamalacca@gmail.com>
2020-10-01 16:48:17 +0600

    made compare table works like real data. only table data needs to be dynamic now

--- includes/Traits/Woo_Product_Comparable.php
+++ includes/Traits/Woo_Product_Comparable.php
@@ -8,6 +8,7 @@ use Elementor\Group_Control_Border;
 use Elementor\Group_Control_Box_Shadow;
 use Elementor\Group_Control_Typography;
 use Elementor\Repeater;
+use WC_Product;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit();
