7105af8366393e3fa50a0429d84df5db584ea16b
Kamal Ahmed <kamalacca@gmail.com>
2020-10-04 16:52:42 +0600

    cleaned up trait and added js to dedicated file for product grid, WIP: Ajax, 80% done

--- src/js/view/product-grid.js
+++ src/js/view/product-grid.js
@@ -1,7 +1,72 @@
 ea.hooks.addAction("init", "ea", () => {
     const productGrid = function ($scope, $) {
+        const $wrap = $scope.find('#eael-product-grid');// cache wrapper
+        const widgetId = $wrap.data('widget-id');
+        const pageId = $wrap.data('page-id');
+        const overlay = document.createElement("div");
+        overlay.classList.add('wcpc-overlay');
+        overlay.setAttribute('id', 'wcpc-overlay');
+        const body = document.getElementsByTagName('body')[0];
+        body.appendChild(overlay);
+        const overlayNode = document.getElementById('wcpc-overlay');
+        const modal = document.getElementsByClassName('eael-wcpc-modal')[0];
+        const ajaxData = [
+            {
+                name: "action",
+                value: 'eael-product-grid'
+            },
+            {
+                name: "widget_id",
+                value: widgetId
+            },
+            {
+                name: "page_id",
+                value: pageId
+            }
+        ];
+        const sendData = function sendData(ajaxData, successCb, errorCb) {
+            $.ajax({
+                url: localize.ajaxurl,
+                type: 'POST',
+                dataType: 'json',
+                data: ajaxData,
+                success: successCb,
+                error: errorCb
+            });
+        }
+
+        $(document).on('click', '.eael-wc-compare', function (e) {
+            e.preventDefault();
+            handleSuccess({'success': true})
+            //sendData(ajaxData, handleSuccess, handleError);
+        });
+
+        $(document).on('click', '.close-modal', function (e) {
+            modal.style.visibility = 'hidden';
+            modal.style.opacity = '0';
+            overlayNode.style.visibility = 'hidden';
+            overlayNode.style.opacity = '0';
+        });
 
 
+
+        function handleSuccess(data) {
+                console.log('logging data');
+                console.log(data);
+                const success = (data && data.success);
+
+                if (success) {
+                    modal.style.visibility = 'visible';
+                    modal.style.opacity = '1';
+                    overlayNode.style.visibility = 'visible';
+                    overlayNode.style.opacity = '1';
+                }
+
+        }
+
+        function handleError(xhr, err) {
+            let errorHtml = `<p class="eael-form-msg invalid">${err.toString()} </p>`;
+        }
     };
     elementorFrontend.hooks.addAction("frontend/element_ready/eicon-woocommerce.default", productGrid);
 });
