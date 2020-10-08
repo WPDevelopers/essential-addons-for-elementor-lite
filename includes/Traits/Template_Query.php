abfd8fb7795b0f116cfcea9d220861721bca1b0d
Nazmul Sabuz <naz.sabuz@gmail.com>
2020-10-08 13:07:43 +0600

    updated: version

--- includes/Traits/Template_Query.php
+++ includes/Traits/Template_Query.php
@@ -11,20 +11,19 @@ trait Template_Query
 
     /**
      * Retrives Template name from file header.
-     * 
+     *
      * @array
      */
     private $template_headers = [
         'Template Name' => 'Template Name',
     ];
 
-
     /**
      * Prepare the directory name from the following widget name.
-     * 
+     *
      * @access private
-     * 
-     * 
+     *
+     *
      * @return  string  $widget_name
      */
     private function process_directory_name()
@@ -37,16 +36,15 @@ trait Template_Query
         return $widget_name;
     }
 
-
     /**
      * Retrive `Theme Template Directory`
-     * 
+     *
      * @return string templates directory from the active theme.
      */
     private function theme_templates_dir()
     {
         $current_theme = wp_get_theme();
-        
+
         $dir = sprintf(
             '%s/%s/Template/%s',
             $current_theme->theme_root,
@@ -54,7 +52,7 @@ trait Template_Query
             $this->process_directory_name()
         );
 
-        if(is_dir($dir)) {
+        if (is_dir($dir)) {
             $file = scandir($dir);
             $file = array_pop($file);
 
@@ -64,10 +62,9 @@ trait Template_Query
         return false;
     }
 
-
     /**
      * Retrives the lite plugin template directory path.
-     * 
+     *
      * @return  string  templates directory path of lite version.
      */
     private function get_template_dir()
@@ -78,38 +75,36 @@ trait Template_Query
         );
     }
 
-
     /**
      * Retrives the pro plugin template directory path.
-     * 
+     *
      * @return  string  templates directory path of pro version.
      */
     private function get_pro_template_dir()
     {
         // ensure is_plugin_active() exists (not on frontend)
-        if( !function_exists('is_plugin_active') ) {
-            include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+        if (!function_exists('is_plugin_active')) {
+            include_once ABSPATH . 'wp-admin/includes/plugin.php';
         }
-        
-        if(!apply_filters('eael/active_plugins', 'essential-addons-elementor/essential_adons_elementor.php')) {
+
+        if (!apply_filters('eael/active_plugins', 'essential-addons-elementor/essential_adons_elementor.php')) {
             return false;
         }
-        
+
         return \sprintf(
             '%sincludes/Template/%s',
             EAEL_PRO_PLUGIN_PATH, $this->process_directory_name()
         );
     }
 
-
     /**
      * Collecting templates from different sources.
-     * 
+     *
      * @return array free templates+pro templates.
      */
     private function get_template_files()
     {
-        if($this->theme_templates_dir()) {
+        if ($this->theme_templates_dir()) {
             return ['theme' => scandir($this->theme_templates_dir(), 1)];
         }
 
@@ -119,17 +114,16 @@ trait Template_Query
             $templates['free'] = scandir($this->get_template_dir(), 1);
         }
 
-        if(is_dir($this->get_pro_template_dir())) {
+        if (is_dir($this->get_pro_template_dir())) {
             $pro_templates['pro'] = scandir($this->get_pro_template_dir(), 1);
         }
 
         return array_merge($templates, $pro_templates);
     }
 
-
     /**
      * Retrives template list from template directory.
-     * 
+     *
      * @return array template list.
      */
     protected function get_template_list()
@@ -138,25 +132,21 @@ trait Template_Query
 
         if ($this->get_template_files()) {
             foreach ($this->get_template_files() as $key => $handler) {
-                foreach($handler as $handle) {
+                foreach ($handler as $handle) {
                     if (strpos($handle, '.php') !== false) {
 
-                        if($key === 'free') {
+                        if ($key === 'free') {
                             $path = sprintf('%s/%s', $this->get_template_dir(), $handle);
-                        }
-
-                        else if($key === 'pro') {
+                        } else if ($key === 'pro') {
                             $path = sprintf('%s/%s', $this->get_pro_template_dir(), $handle);
-                        }
-
-                        else if($key === 'theme') {
+                        } else if ($key === 'theme') {
                             $path = sprintf('%s/%s', $this->theme_templates_dir(), $handle);
                         }
-                        
+
                         $template_info = get_file_data($path, $this->template_headers);
                         $template_name = $template_info['Template Name'];
 
-                        if($template_name) {
+                        if ($template_name) {
                             $files[$template_name] = $path;
                         }
                     }
@@ -167,18 +157,17 @@ trait Template_Query
         return $files;
     }
 
-
     /**
      * Preparing template options for frontend select
-     * 
+     *
      * @return array teplate select options.
      */
     private function get_template_options()
     {
         $files = [];
 
-        if($this->get_template_list()) {
-            foreach($this->get_template_list() as $filename => $path) {
+        if ($this->get_template_list()) {
+            foreach ($this->get_template_list() as $filename => $path) {
                 $filename = \str_replace(' ', '-', $filename);
 
                 $files[strtolower($filename)] = $path;
@@ -188,10 +177,9 @@ trait Template_Query
         return $files;
     }
 
-
     /**
      * Adding key value pairs in template options.
-     * 
+     *
      * @return array
      */
     private function template_options()
@@ -202,35 +190,31 @@ trait Template_Query
         return array_combine($keys, $values);
     }
 
-
     /**
      * Retrive template
-     * 
+     *
      * @param string $filename
-     * 
+     *
      * @return string include-able full template path.
      */
     public function get_template($filename)
     {
-        if(in_array($filename, array_keys($this->get_template_options()))) {
-
+        if (in_array($filename, array_keys($this->get_template_options()))) {
             return $this->get_template_options()[$filename];
-
         }
 
         return false;
     }
 
-
     /**
      * Set default option in frontend select control.
-     * 
+     *
      * @return string first option.
      */
     public function get_default()
     {
         $dt = array_reverse($this->template_options());
-        
+
         return strtolower(array_pop($dt));
     }
 
