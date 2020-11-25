e98c2b00a851b00d6c951735c99732256e87bb57
Sabuz <naz.sabuz@gmail.com>
2020-11-25 16:09:58 +0600

    Added: hashtag support for accordion

--- src/js/view/advanced-accordion.js
+++ src/js/view/advanced-accordion.js
@@ -1,60 +1,72 @@
-var AdvAccordionHandler = function($scope, $) {
-    var $advanceAccordion = $scope.find(".eael-adv-accordion"),
-        $accordionHeader = $scope.find(".eael-accordion-header"),
-        $accordionType = $advanceAccordion.data("accordion-type"),
-        $accordionSpeed = $advanceAccordion.data("toogle-speed");
+ea.hooks.addAction("init", "ea", () => {
+	elementorFrontend.hooks.addAction(
+		"frontend/element_ready/eael-adv-accordion.default",
+		function ($scope, $) {
+			let hashTag = window.location.hash.substr(1);
+			let hashTagExists = false;
+			var $advanceAccordion = $scope.find(".eael-adv-accordion"),
+				$accordionHeader = $scope.find(".eael-accordion-header"),
+				$accordionType = $advanceAccordion.data("accordion-type"),
+				$accordionSpeed = $advanceAccordion.data("toogle-speed");
 
-    // Open default actived tab
-    $accordionHeader.each(function() {
-        if ($(this).hasClass("active-default")) {
-            $(this).addClass("show active");
-            $(this)
-                .next()
-                .slideDown($accordionSpeed);
-        }
-    });
+			// Open default actived tab
+			if (hashTag) {
+				$accordionHeader.each(function () {
+					if ($(this).attr("id") == hashTag) {
+						hashTagExists = true;
 
-    // Remove multiple click event for nested accordion
-    $accordionHeader.unbind("click");
+						$(this).addClass("show active");
+						$(this).next().slideDown($accordionSpeed);
+					}
+				});
+			}
 
-    $accordionHeader.click(function(e) {
-        e.preventDefault();
+			if (hashTagExists === false) {
+				$accordionHeader.each(function () {
+					if ($(this).hasClass("active-default")) {
+						$(this).addClass("show active");
+						$(this).next().slideDown($accordionSpeed);
+					}
+				});
+			}
 
-        var $this = $(this);
+			// Remove multiple click event for nested accordion
+			$accordionHeader.unbind("click");
 
-        if ($accordionType === "accordion") {
-            if ($this.hasClass("show")) {
-                $this.removeClass("show active");
-                $this.next().slideUp($accordionSpeed);
-            } else {
-                $this
-                    .parent()
-                    .parent()
-                    .find(".eael-accordion-header")
-                    .removeClass("show active");
-                $this
-                    .parent()
-                    .parent()
-                    .find(".eael-accordion-content")
-                    .slideUp($accordionSpeed);
-                $this.toggleClass("show active");
-                $this.next().slideToggle($accordionSpeed);
-            }
-        } else {
-            // For acccordion type 'toggle'
-            if ($this.hasClass("show")) {
-                $this.removeClass("show active");
-                $this.next().slideUp($accordionSpeed);
-            } else {
-                $this.addClass("show active");
-                $this.next().slideDown($accordionSpeed);
-            }
-        }
-    });
-};
-jQuery(window).on("elementor/frontend/init", function() {
-    elementorFrontend.hooks.addAction(
-        "frontend/element_ready/eael-adv-accordion.default",
-        AdvAccordionHandler
-    );
+			$accordionHeader.click(function (e) {
+				e.preventDefault();
+
+				var $this = $(this);
+
+				if ($accordionType === "accordion") {
+					if ($this.hasClass("show")) {
+						$this.removeClass("show active");
+						$this.next().slideUp($accordionSpeed);
+					} else {
+						$this
+							.parent()
+							.parent()
+							.find(".eael-accordion-header")
+							.removeClass("show active");
+						$this
+							.parent()
+							.parent()
+							.find(".eael-accordion-content")
+							.slideUp($accordionSpeed);
+						$this.toggleClass("show active");
+						$this.next().slideToggle($accordionSpeed);
+					}
+				} else {
+					// For acccordion type 'toggle'
+					if ($this.hasClass("show")) {
+						$this.removeClass("show active");
+						$this.next().slideUp($accordionSpeed);
+					} else {
+						$this.addClass("show active");
+						$this.next().slideDown($accordionSpeed);
+					}
+				}
+			});
+		}
+	);
 });
