54c4d779cb441d84d148d314cf319b2b5a2dae7b
Khan-Rafin-Ahmed <khanrafinahmed@gmail.com>
2021-02-08 13:48:04 +0600

    changed NotificationX description

--- includes/Classes/WPDeveloper_Setup_Wizard.php
+++ includes/Classes/WPDeveloper_Setup_Wizard.php
@@ -398,7 +398,7 @@ c2.2,0,4.2-1.1,5.4-2.8L49.1,9.5C50.5,7.5,50.2,4.8,48.5,3.1z"/>
                 'basename' => 'notificationx/notificationx.php',
                 'logo'     => EAEL_PLUGIN_URL . 'assets/admin/images/nx-logo.png',
                 'title'    => __( 'NotificationX', 'essential-addons-for-elementor-lite' ),
-                'desc'     => __( 'Best FOMO, Social Proof, Sales Popup & WooCommerce Notification Bar With Elementor Support', 'essential-addons-for-elementor-lite' ),
+                'desc'     => __( 'Best FOMO Social Proof Plugin to boost your sales conversion. Create stunning Sales Popup & Notification Bar With Elementor Support', 'essential-addons-for-elementor-lite' ),
             ],
             [
                 'slug'     => 'easyjobs',
