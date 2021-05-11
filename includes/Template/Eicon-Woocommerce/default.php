e5736c78e913dd35bb9494d72c6cd80d9c5a4b9a
Md Rashedul Islam <rashedulislam.ruet@gmail.com>
2021-05-11 17:40:52 +0600

    fixed image blury/size issues

--- includes/Template/Eicon-Woocommerce/default.php
+++ includes/Template/Eicon-Woocommerce/default.php
@@ -1,11 +1,13 @@
 <?php
 
-use \Essential_Addons_Elementor\Classes\Helper;
+
 /**
  * Template Name: Default
  */
 
+use \Essential_Addons_Elementor\Classes\Helper;
 use Essential_Addons_Elementor\Elements\Product_Grid;
+use \Elementor\Group_Control_Image_Size;
 
 if ( ! defined( 'ABSPATH' ) ) {
     exit;
@@ -15,6 +17,17 @@ $product = wc_get_product( get_the_ID() );
 if ( ! $product ) {
     error_log( '$product not found in ' . __FILE__ );
     return;
+}
+
+//error_log(print_r( $settings, 1 ));
+
+
+if ( has_post_thumbnail() ) {
+	$settings[ 'eael_image_size_customize' ] = [
+		'id' => get_post_thumbnail_id(),
+	];
+	$settings['eael_image_size_customize_size'] = $settings['eael_product_grid_image_size_size'];
+	$thumbnail_html = Group_Control_Image_Size::get_attachment_image_html( $settings,'eael_image_size_customize' );
 }
 
 $title_tag = isset( $settings['eael_product_grid_title_html_tag'] ) ? Helper::eael_validate_html_tag($settings['eael_product_grid_title_html_tag'])  : 'h2';
@@ -38,7 +51,7 @@ $quick_view_setting = [
 if ( $grid_style_preset == 'eael-product-simple' || $grid_style_preset == 'eael-product-reveal' ) { ?>
     <li class="product">
         <a href="<?php echo esc_url( $product->get_permalink() ); ?>" class="woocommerce-LoopProduct-link woocommerce-loop-product__link">
-            <?php echo wp_kses_post( $product->get_image( 'woocommerce_thumbnail', ['loading' => 'eager'] ) );
+            <?php echo  $thumbnail_html;
             printf('<%1$s class="woocommerce-loop-product__title">%2$s</%1$s>', $title_tag, $product->get_title());
             if ( $should_print_rating ) {
                 echo wp_kses_post( wc_get_rating_html( $product->get_average_rating(), $product->get_rating_count() ) );
@@ -66,7 +79,7 @@ if ( $grid_style_preset == 'eael-product-simple' || $grid_style_preset == 'eael-
     ?>
     <li class="product">
         <div class="overlay">
-            <?php echo $product->get_image( 'woocommerce_thumbnail', ['loading' => 'eager'] ); ?>
+            <?php echo $thumbnail_html; ?>
             <div class="button-wrap clearfix">
                 <a href="<?php echo esc_url( $product->get_permalink() ); ?>" class="product-link"><span class="fas fa-link"></span></a>
                 <?php
@@ -103,7 +116,7 @@ if ( $grid_style_preset == 'eael-product-simple' || $grid_style_preset == 'eael-
                     <div class="image-wrap">
                         <?php
                         echo ( ! $product->managing_stock() && ! $product->is_in_stock() ? '<span class="eael-onsale outofstock '.$sale_badge_preset.' '.$sale_badge_align.'">'.__('Stock ', 'essential-addons-for-elementor-lite'). '<br />' . __('Out', 'essential-addons-for-elementor-lite').'</span>' : ($product->is_on_sale() ? '<span class="eael-onsale '.$sale_badge_preset.' '.$sale_badge_align.'">' . __('Sale!', 'essential-addons-for-elementor-lite') . '</span>' : '') );
-                        echo $product->get_image($settings['eael_product_grid_image_size_size'], ['loading' => 'eager']);
+                        echo $thumbnail_html;
                         ?>
                     </div>
                     <div class="image-hover-wrap">
@@ -208,7 +221,7 @@ if ( $grid_style_preset == 'eael-product-simple' || $grid_style_preset == 'eael-
                         <?php
                         echo '<a href="' . $product->get_permalink() . '" class="woocommerce-LoopProduct-link woocommerce-loop-product__link">';
                         echo ( ! $product->managing_stock() && ! $product->is_in_stock() ? '<span class="eael-onsale outofstock '.$sale_badge_preset.' '.$sale_badge_align.'">'.__('Stock ', 'essential-addons-for-elementor-lite'). '<br />' . __('Out', 'essential-addons-for-elementor-lite').'</span>' : ($product->is_on_sale() ? '<span class="eael-onsale '.$sale_badge_preset.' '.$sale_badge_align.'">' . __('Sale!', 'essential-addons-for-elementor-lite') . '</span>' : '') );
-                        echo $product->get_image($settings['eael_product_grid_image_size_size'], ['loading' => 'eager']);
+                        echo $thumbnail_html;
                         echo '</a>';
                         ?>
                     </div>
@@ -266,7 +279,7 @@ if ( $grid_style_preset == 'eael-product-simple' || $grid_style_preset == 'eael-
                         <?php
                         echo '<a href="' . $product->get_permalink() . '" class="woocommerce-LoopProduct-link woocommerce-loop-product__link">';
                         echo ( ! $product->managing_stock() && ! $product->is_in_stock() ? '<span class="eael-onsale outofstock '.$sale_badge_preset.' '.$sale_badge_align.'">'.__('Stock ', 'essential-addons-for-elementor-lite'). '<br />' . __('Out', 'essential-addons-for-elementor-lite').'</span>' : ($product->is_on_sale() ? '<span class="eael-onsale '.$sale_badge_preset.' '.$sale_badge_align.'">' . __('Sale!', 'essential-addons-for-elementor-lite') . '</span>' : '') );
-                        echo $product->get_image($settings['eael_product_grid_image_size_size'], ['loading' => 'eager']);
+                        echo $thumbnail_html;
                         echo '</a>';
                         ?>
                     </div>
@@ -390,6 +403,7 @@ if ( $grid_style_preset == 'eael-product-simple' || $grid_style_preset == 'eael-
         <?php
     }
 }else {
+
     if($settings['eael_product_grid_rating']!='yes'){
         remove_action('woocommerce_after_shop_loop_item_title','woocommerce_template_loop_rating',5);
     }
@@ -398,7 +412,7 @@ if ( $grid_style_preset == 'eael-product-simple' || $grid_style_preset == 'eael-
         global $product; 
         if(empty($product->is_on_sale())  && !$product->is_in_stock()  ){
           echo '<span class="outofstock-badge">'.__('Stock ', 'essential-addons-for-elementor-lite'). '<br />' . __('Out', 'essential-addons-for-elementor-lite').'</span>';
-        } 
+        }
     },9);
     
     if ( $should_print_compare_btn ) {
