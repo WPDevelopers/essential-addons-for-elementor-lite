ae8978a9623359654def6aa878fc8feab001e6a5
Linkon Miyan <rudlinkon@gmail.com>
2021-06-20 11:38:01 +0600

    Added no posts found message in smart post list

--- includes/Traits/Helper.php
+++ includes/Traits/Helper.php
@@ -152,6 +152,8 @@ trait Helper
                         $html .= HelperClass::include_with_variable( $file_path, [ 'settings' => $settings, 'link_settings' => $link_settings, 'iterator' => $iterator ] );
                         $iterator++;
                     }
+                } else {
+	                $html .= __( '<p class="no-posts-found">No posts found!</p>', 'essential-addons-for-elementor-lite' );
                 }
             }
         }
