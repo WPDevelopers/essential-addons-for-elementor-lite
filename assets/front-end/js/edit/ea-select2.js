b28cab1b57945d5afe60c5bc5f74096b8d4cf932
robinwpdeveloper <robin.wpdeveloper@gmail.com>
2021-07-15 18:19:19 +0600

    EA Select2 manual ordering changes

--- assets/front-end/js/edit/ea-select2.js
+++ assets/front-end/js/edit/ea-select2.js
@@ -23,6 +23,109 @@
 						})
 					}
 
+                    $("ul.select2-selection__rendered").sortable({
+                        containment: 'parent',
+                        update: function() {
+                            // jQuery('#elementor-control-default-c762').val();
+                            // console.log(elementor.settings.page.getItems('category_ids'));
+
+                            $select = $(this).closest(".select2-container").prev();
+                            var options = $select.find("option");
+                            var newoptions = [];
+                            // Clear option
+                            $(this).find(".select2-selection__choice").each(function (i, tag) {
+                                options.each(function (j, option) {
+                                    if($(tag).attr("title") == $(option).text()){
+                                        newoptions.push(option);
+                                    }
+                                });
+                            });
+                            $select.empty();
+                            $select.append(newoptions);
+                            $(ID).trigger('change');
+                            // jQuery(this).trigger('input');
+                        }
+                    });
+                    // console.log($(ID).select2('data'));
+                    //Drag and drop feature : implemented for select2
+                    // $("ul.select2-selection__rendered").sortable({
+                    //     containment: 'parent',
+                    //     update: function(event) {
+                    //         // jQuery('#elementor-control-default-c762').select2('data');
+                    //         jQuery(this).trigger('input');
+                    //         jQuery(this).trigger('select');
+                    //         jQuery(this).trigger('select2');
+                    //
+                    //         let selectedVal1 = jQuery(ID).select2('data');
+                    //
+                    //         console.log(selectedVal1);
+                    //
+                    //         // let selectedVal2 = $(ID).find(':selected');
+                    //
+                    //         // $('input[name=eae-file-link]').val(resp.data.url).trigger('input');
+                    //         // let select2Val = $(ID).select2().val();
+                    //         // console.log(selectedVal);
+                    //         //
+                    //         // console.log(obj.data);
+                    //         // $(ID).trigger('change');
+                    //         // let container = view.getContainer();
+                    //         // let settings = view.getContainer().settings.attributes;
+                    //         // console.log(container);
+                    //         // console.log('okk');
+                    //         // Object.keys(value).forEach((key) => {
+                    //         //     settings[key] = value[key];
+                    //         // });
+                    //         //
+                    //         // parent.window.$e.run("document/elements/settings", {
+                    //         //     ID,
+                    //         //     settings,
+                    //         //     options: {
+                    //         //         external: refresh,
+                    //         //     },
+                    //         // });
+                    //
+                    //         // $select = $(this).closest(".select2-container").prev();
+                    //         // var options = $select.find("option");
+                    //         // var newoptions = [];
+                    //         // // Clear option
+                    //         // $(this).find(".select2-selection__choice").each(function (i, tag) {
+                    //         //     options.each(function (j, option) {
+                    //         //         if($(tag).attr("title") == $(option).text()){
+                    //         //             newoptions.push(option);
+                    //         //         }
+                    //         //     });
+                    //         // });
+                    //         // $select.empty();
+                    //         // $select.append(newoptions);
+                    //
+                    //     }
+                    // });
+
+                    // $("ul.select2-selection__rendered").sortable({
+                    //     containment: 'parent',
+                    //     // stop: function(event, ui) {
+                    //     //     // event target would be the <ul> which also contains a list item for searching (which has to be excluded)
+                    //     //     var arr = Array.from($(event.target).find('li:not(.select2-search)').map(function () {
+                    //     //         return {name: $(this).data('data').text, value: $(this).data('data').id };
+                    //     //     }))
+                    //     //     console.log(arr);
+                    //     // }
+                    //
+                    //     // $select = $(this).closest(".select2-container").prev();
+                    //     //         // var options = $select.find("option");
+                    //     //         // var newoptions = [];
+                    //     //         // // Clear option
+                    //     //         // $(this).find(".select2-selection__choice").each(function (i, tag) {
+                    //     //         //     options.each(function (j, option) {
+                    //     //         //         if($(tag).attr("title") == $(option).text()){
+                    //     //         //             newoptions.push(option);
+                    //     //         //         }
+                    //     //         //     });
+                    //     //         // });
+                    //     //         // $select.empty();
+                    //     //         // $select.append(newoptions);
+                    // });
+
                     if (ids.length > 0) {
                         var label = $("label[for='elementor-control-default-" + obj.data._cid + "']");
                         label.after('<span class="elementor-control-spinner">&nbsp;<i class="eicon-spinner eicon-animation-spin"></i>&nbsp;</span>');
@@ -43,5 +146,12 @@
                 }
             });
         }, 100);
+
+        // function handleMenuItemColor ( newValue ) {
+        //     console.log( newValue );
+        //     elementor.reloadPreview();
+        // }
+        //
+        // elementor.settings.page.addChangeCallback( 'category_ids', handleMenuItemColor );
     });
 }(jQuery));
