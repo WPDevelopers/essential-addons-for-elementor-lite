d69f30f775bfbbf4f46cf80ef61d196f52887ec2
Manzur Ahammed <manzur.ahammed236@gmail.com>
2021-08-11 16:32:18 +0600

    added hooks

--- src/js/view/quick-view.js
+++ src/js/view/quick-view.js
@@ -1,18 +1,9 @@
 const QuickView = {
-	scope: null,
-	init: (scope) => {
-		this.scope = scope;
-	},
-	popupMarkup: () => {
-		return `<div style="display: none" class="eael-woocommerce-popup-view eael-product-popup eael-product-zoom-in woocommerce">
-                    			<div class="eael-product-modal-bg"></div>
-                    			<div class="eael-popup-details-render eael-woo-slider-popup"><div class="eael-preloader"></div></div>
-               				 </div>`;
-	},
-	appandPopup: () => {
-		$('body').prepend(this.popupMarkup);
-	},
-	test:() => {
-		console.log("quick view");
+	test:(a,b) => {
+		console.log(a)
+		console.log(b)
 	}
 }
+
+ea.hooks.addAction( 'quickViewAddMarkup', 'ea', QuickView.test)
+
