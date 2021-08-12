b003f6524c0ffd13412b37ccb5612ffd22d2d26b
Manzur Ahammed <manzur.ahammed236@gmail.com>
2021-08-12 17:16:59 +0600

    added add to cart method

--- src/js/view/woo-product-carousel.js
+++ src/js/view/woo-product-carousel.js
@@ -1,10 +1,5 @@
 ea.hooks.addAction("init", "ea", () => {
 	const wooProductCarousel = function ($scope, $) {
-		const popupMarkup = `<div style="display: none" class="eael-woocommerce-popup-view eael-product-popup eael-product-zoom-in woocommerce">
-                    			<div class="eael-product-modal-bg"></div>
-                    			<div class="eael-popup-details-render eael-woo-slider-popup"><div class="eael-preloader"></div></div>
-               				 </div>`;
-		$('body').prepend(popupMarkup);
 		ea.hooks.doAction("quickViewAddMarkup",$scope,$);
 		var $wooProductCarousel = $scope.find(".eael-woo-product-carousel").eq(0),
 			$type = $wooProductCarousel.data("type"),
@@ -230,91 +225,76 @@ ea.hooks.addAction("init", "ea", () => {
 		);
 
 		// handle add to cart for quick view
-		$(document).on(
-			"click",
-			".eael-woo-slider-popup .single_add_to_cart_button",
-			function (e) {
-				e.preventDefault();
-				e.stopImmediatePropagation();
-				var $this = $(this),
-					product_id = $(this).val(),
-					variation_id =
-						$this
-						.closest("form.cart")
-						.find('input[name="variation_id"]')
-						.val() || "",
-					quantity = $this
-					.closest("form.cart")
-					.find('input[name="quantity"]')
-					.val(),
-					items = $this.closest("form.cart.grouped_form"),
-					form = $this.closest("form.cart"),
-					product_data = [];
-				items = items.serializeArray();
-
-				if (form.hasClass("variations_form")) {
-					product_id = form.find('input[name="product_id"]').val();
-				}
-				if (items.length > 0) {
-					items.forEach((item, index) => {
-						var p_id = parseInt(item.name.replace(/[^\d.]/g, ""), 10);
-						if (
-							item.name.indexOf("quantity[") >= 0 &&
-							item.value != "" &&
-							p_id > 0
-						) {
-							product_data[product_data.length] = {
-								product_id: p_id,
-								quantity: item.value,
-								variation_id: 0,
-							};
-						}
-					});
-				} else {
-					product_data[0] = {
-						product_id: product_id,
-						quantity: quantity,
-						variation_id: variation_id,
-					};
-				}
-				$this.removeClass("eael-addtocart-added");
-				$this.addClass("eael-addtocart-loading");
-				$.ajax({
-						   url: localize.ajaxurl,
-						   type: "post",
-						   data: {
-							   action: "eael_product_add_to_cart",
-							   product_data: product_data,
-							   eael_add_to_cart_nonce: localize.nonce,
-							   cart_item_data: form.serializeArray(),
-						   },
-						   success: function (response) {
-							   if (response.success) {
-								   $(document.body).trigger("wc_fragment_refresh");
-								   $this.removeClass("eael-addtocart-loading");
-								   $this.addClass("eael-addtocart-added");
-							   }
-						   },
-					   });
-			}
-		);
-
-		$(document).on("click", ".eael-product-popup-close", function (event) {
-			event.stopPropagation();
-			remove_product_popup();
-		});
-		$(document).on("click", function (event) {
-			
-			if (event.target.closest(".eael-product-popup-details")) return;
-			console.log("test");
-			remove_product_popup();
-		});
+		// $(document).on(
+		// 	"click",
+		// 	".eael-woo-slider-popup .single_add_to_cart_button",
+		// 	function (e) {
+		// 		e.preventDefault();
+		// 		e.stopImmediatePropagation();
+		// 		var $this = $(this),
+		// 			product_id = $(this).val(),
+		// 			variation_id =
+		// 				$this
+		// 				.closest("form.cart")
+		// 				.find('input[name="variation_id"]')
+		// 				.val() || "",
+		// 			quantity = $this
+		// 			.closest("form.cart")
+		// 			.find('input[name="quantity"]')
+		// 			.val(),
+		// 			items = $this.closest("form.cart.grouped_form"),
+		// 			form = $this.closest("form.cart"),
+		// 			product_data = [];
+		// 		items = items.serializeArray();
+		//
+		// 		if (form.hasClass("variations_form")) {
+		// 			product_id = form.find('input[name="product_id"]').val();
+		// 		}
+		// 		if (items.length > 0) {
+		// 			items.forEach((item, index) => {
+		// 				var p_id = parseInt(item.name.replace(/[^\d.]/g, ""), 10);
+		// 				if (
+		// 					item.name.indexOf("quantity[") >= 0 &&
+		// 					item.value != "" &&
+		// 					p_id > 0
+		// 				) {
+		// 					product_data[product_data.length] = {
+		// 						product_id: p_id,
+		// 						quantity: item.value,
+		// 						variation_id: 0,
+		// 					};
+		// 				}
+		// 			});
+		// 		} else {
+		// 			product_data[0] = {
+		// 				product_id: product_id,
+		// 				quantity: quantity,
+		// 				variation_id: variation_id,
+		// 			};
+		// 		}
+		// 		$this.removeClass("eael-addtocart-added");
+		// 		$this.addClass("eael-addtocart-loading");
+		// 		$.ajax({
+		// 				   url: localize.ajaxurl,
+		// 				   type: "post",
+		// 				   data: {
+		// 					   action: "eael_product_add_to_cart",
+		// 					   product_data: product_data,
+		// 					   eael_add_to_cart_nonce: localize.nonce,
+		// 					   cart_item_data: form.serializeArray(),
+		// 				   },
+		// 				   success: function (response) {
+		// 					   if (response.success) {
+		// 						   $(document.body).trigger("wc_fragment_refresh");
+		// 						   $this.removeClass("eael-addtocart-loading");
+		// 						   $this.addClass("eael-addtocart-added");
+		// 					   }
+		// 				   },
+		// 			   });
+		// 	}
+		// );
 		
-		const remove_product_popup = () => {
-			let selector = $(".eael-product-popup.eael-product-zoom-in.eael-product-popup-ready")
-			selector.addClass("eael-product-modal-removing").removeClass("eael-product-popup-ready");
-			selector.find('.eael-popup-details-render').html('');
-		}
+		ea.hooks.doAction("quickViewPopupViewInit",$scope,$);
 
 		if (isEditMode) {
 			$(".eael-product-image-wrap .woocommerce-product-gallery").css(
