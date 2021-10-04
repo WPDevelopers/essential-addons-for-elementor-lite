95a66eecfaf7efc20c5300f3022e3330e6aa9703
Manzur Ahammed <manzur.ahammed236@gmail.com>
2021-10-04 15:20:26 +0600

    remove unused code

---
+++ assets/front-end/js/view/interactive-circle.js
@@ -0,0 +1,100 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/js/view/interactive-circle.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/js/view/interactive-circle.js":
+/*!*******************************************!*\
+  !*** ./src/js/view/interactive-circle.js ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("ea.hooks.addAction(\"init\", \"ea\", function () {\n  var interactiveCircle = function interactiveCircle($scope, $) {\n    var $circleWrap = $scope.find(\".eael-circle-wrapper\");\n    var $eventType = \"mouseenter\";\n\n    if ($circleWrap.hasClass('eael-interactive-circle-event-click')) {\n      $eventType = \"click\";\n    }\n\n    var $tabLinks = $circleWrap.find(\".eael-circle-btn\");\n    var $tabContents = $circleWrap.find(\".eael-circle-btn-content\");\n    $tabLinks.first().addClass(\"active\");\n    $tabContents.first().addClass(\"active\");\n    $tabLinks.each(function (element) {\n      $(this).on($eventType, handleEvent(element));\n    });\n\n    function handleEvent(element) {\n      return function () {\n        var $element = $(this);\n        var $activeTab = $(this).hasClass(\"active\");\n\n        if ($activeTab == false) {\n          $tabLinks.each(function (tabLink) {\n            $(this).removeClass(\"active\");\n          });\n          $element.addClass(\"active\");\n          $tabContents.each(function (tabContent) {\n            $(this).removeClass(\"active\");\n\n            if ($(this).hasClass($element.attr(\"id\"))) {\n              $(this).addClass(\"active\");\n            }\n          });\n        }\n      };\n    }\n  };\n\n  elementorFrontend.hooks.addAction(\"frontend/element_ready/eael-interactive-circle.default\", interactiveCircle);\n});\n\n//# sourceURL=webpack:///./src/js/view/interactive-circle.js?");
+
+/***/ })
+
+/******/ });
