ca7d3748e73c885bf5ce816b502f445ea0a8a0c0
Manzur Ahammed <manzur.ahammed236@gmail.com>
2021-11-23 12:21:41 +0600

    remove copy

--- includes/templates/admin/extensions.php
+++ includes/templates/admin/extensions.php
@@ -56,13 +56,6 @@ $extensions = [
                 'demo_link' => 'https://essential-addons.com/elementor/custom-js/',
                 'doc_link' => 'https://essential-addons.com/elementor/docs/custom-js/',
             ],
-	        [
-		        'key'       => 'xd-copy',
-		        'title'     => __( 'Cross-Domain Copy Paste', 'essential-addons-for-elementor-lite' ),
-		        'demo_link' => 'https://essential-addons.com/elementor/xd-copy/',
-		        'doc_link'  => 'https://essential-addons.com/elementor/docs/xd-copy/',
-		        'is_pro'    => true
-            ],
             [
                 'key'    => 'scroll-to-top',
                 'title'  => __( 'Scroll to Top', 'essential-addons-for-elementor-lite'),
