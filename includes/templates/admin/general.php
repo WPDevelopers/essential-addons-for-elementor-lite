557433f49eefa75f230f4a8bddeec6295716908b
Manzur Ahammed <manzur.ahammed236@gmail.com>
2021-12-20 11:53:53 +0600

    added license section

--- includes/templates/admin/general.php
+++ includes/templates/admin/general.php
@@ -9,52 +9,63 @@
                     <a href="#" class="eael-button">View Changelog</a>
                 </div>
             </div>
-            <div class="eael-block p45 eael-activate__license__block">
-                <div class="eael__flex eael__flex--wrap align__center mb30">
-                    <h3>Just one more step to go!</h3>
-                    <img src="<?php echo esc_url( EAEL_PLUGIN_URL . 'assets/admin/images/steps.svg' ) ?>" alt="">
+			<?php do_action( 'add_admin_license_markup' ); ?>
+			<?php if ( $this->installer->get_local_plugin_data( 'templately/templately.php' ) === false ) { ?>
+                <div class="template__block eael-block p45">
+                    <div class="template__logo">
+                        <img src="<?php echo esc_url( EAEL_PLUGIN_URL . 'assets/admin/images/quick-setup/templately-logo.svg' ) ?>"
+                             alt="">
+                    </div>
+                    <h2>Unlock <span class="color__secondary">1600+</span> Ready Templates Built With Elementor &
+                        Essential
+                        Addons From Templately.</h2>
+                    <p>Get Access to amazing features and boost your Elementor page building experience with Templately
+                        👇</p>
+                    <ul class="ls-none mb30">
+                        <li>🌟 Access Thousands Of Stunning, Ready Website Templates</li>
+                        <li>🔥 Save Your Design Anywhere With MyCloud Storage Space</li>
+                        <li>🚀 Add Team Members & Collaborate On Cloud With Templately WorkSpace</li>
+                    </ul>
+                    <a href="#" class="eael-button button__secondary hover__shadow wpdeveloper-plugin-installer"
+                       data-action="install" data-slug="templately">Install Templately</a>
                 </div>
-                <p>Enter your license key here, to activate Essential Addons for Elementor, and get automatic updates
-                    and
-                    premium support. <br> Visit the <a href="#">Validation Guide</a> for help.</p>
-                <ol>
-                    <li>Log in to <strong>your account</strong> to get your license key.</li>
-                    <li>If you don't yet have a license key, get <strong>Essential Addons for Elementor</strong> now.
-                    </li>
-                    <li>Copy the license key from your account and paste it below.</li>
-                    <li>Click on "Activate License" button.</li>
-                </ol>
-                <div class="license__form__block">
-                    <form action="">
-                        <input type="text" class="eael-form__control" placeholder="Place Your License Key and Active">
-                        <button class="eael-button button__themeColor">Active License</button>
-                    </form>
+			<?php } else { ?>
+                <div class="eael-block p45">
+                    <div class="eael-grid">
+                        <div class="eael-col-md-6">
+                            <div class="template__block">
+                                <h2>🚀 Get Started with Essential Addons for Elementor</h2>
+                                <p>Thank you for choosing Essential Addons for Elementor. Get ready to enhance your
+                                    Elementor site building experience by using 40+ Essential Addons elements for
+                                    FREE.</p>
+                                <a href="#" class="eael-button button__secondary hover__shadow">YouTube Tutorial</a>
+                            </div>
+                        </div>
+                        <div class="eael-col-md-6">
+                            <iframe src="https://www.youtube.com/embed/rc-5L7fek3M" title="YouTube video player"
+                                    frameborder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowfullscreen></iframe>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div class="template__block eael-block p45">
-                <div class="template__logo">
-                    <img src="<?php echo esc_url( EAEL_PLUGIN_URL . 'assets/admin/images/quick-setup/templately-logo.svg' ) ?>" alt="">
-                </div>
-                <h2>Unlock <span class="color__secondary">1600+</span> Ready Templates Built With Elementor & Essential
-                    Addons From Templately.</h2>
-                <p>Get Access to amazing features and boost your Elementor page building experience with Templately
-                    👇</p>
-                <ul class="ls-none mb30">
-                    <li>🌟 Access Thousands Of Stunning, Ready Website Templates</li>
-                    <li>🔥 Save Your Design Anywhere With MyCloud Storage Space</li>
-                    <li>🚀 Add Team Members & Collaborate On Cloud With Templately WorkSpace</li>
-                </ul>
-                <a href="#" class="eael-button button__secondary hover__shadow">Install Templately</a>
-            </div>
+			<?php } ?>
             <div class="eael-block px45 py25">
                 <div class="eael-basic__card eael__flex align__center justify__between">
                     <div class="eael__flex align__center">
                         <div class="mr20 fs-0">
-                            <img src="<?php echo esc_url( EAEL_PLUGIN_URL . 'assets/admin/images/quick-setup/ea.svg' ) ?>" alt="">
+                            <img src="<?php echo esc_url( EAEL_PLUGIN_URL . 'assets/admin/images/quick-setup/ea.svg' ) ?>"
+                                 alt="">
                         </div>
                         <h4>Manage your License for your sites from your WPDeveloper account</h4>
                     </div>
-                    <a href="#" class="eael-button button__themeColor">Manage License</a>
+					<?php
+					if ( !defined( 'EAEL_PRO_PLUGIN_BASENAME' ) ) {
+						printf( __( '<a class="eael-button button__themeColor" href="https://wpdeveloper.com/in/upgrade-essential-addons-elementor" target="_blank">%s</a>', 'essential-addons-for-elementor-lite' ), 'Upgrade to Pro' );
+					} else {
+						do_action( 'eael_manage_license_action_link' );
+					}
+					?>
                 </div>
             </div>
         </div>
