5b044ff6d641311e809d625b563941c012489e6e
Linkon Miyan <rudlinkon@gmail.com>
2022-02-14 17:38:18 +0600

    Added blank Conditional Logic extension

---
+++ includes/Extensions/Conditional_Logic.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Essential_Addons_Elementor\Extensions;
+
+use Elementor\Controls_Stack;
+use Elementor\Plugin;
+use Elementor\Utils;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+class Conditional_Logic {
+
+	/**
+	 * Initialize hooks
+	 */
+	public function __construct() {
+
+	}
+
+}
