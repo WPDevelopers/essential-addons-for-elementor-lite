0699c56bcf0545f1751078299011f2e93b3f135d
A. H. M. Nazmul Hasan Monshi <robin.wpdeveloper@gmail.com>
2022-02-22 16:28:25 +0600

    Improvement | Advanced Accordion - faq schema added

--- includes/Elements/Adv_Accordion.php
+++ includes/Elements/Adv_Accordion.php
@@ -197,53 +197,6 @@ class Adv_Accordion extends Widget_Base
             ]
         );
 
-        $this->add_control(
-            'eael_adv_accordion_faq_schema_type',
-            [
-                'label'       => esc_html__('Schema Type', 'essential-addons-for-elementor-lite'),
-                'type'        => Controls_Manager::SELECT,
-                'default'     => 'faq',
-                'label_block' => false,
-                'options'     => [
-                    'article' => esc_html__('Article', 'essential-addons-for-elementor-lite'),
-                    'book'    => esc_html__('Book', 'essential-addons-for-elementor-lite'),
-                    'breadcrumb'    => esc_html__('Breadcrumb', 'essential-addons-for-elementor-lite'),
-                    'carousel'    => esc_html__('Carousel', 'essential-addons-for-elementor-lite'),
-                    'course'    => esc_html__('Course', 'essential-addons-for-elementor-lite'),
-                    'covid-19'    => esc_html__('Covid 19', 'essential-addons-for-elementor-lite'),
-                    'announcements'    => esc_html__('Announcements', 'essential-addons-for-elementor-lite'),
-                    'dataset'    => esc_html__('Dataset', 'essential-addons-for-elementor-lite'),
-                    'employer-aggregate-rating'    => esc_html__('Employer Aggregate Rating', 'essential-addons-for-elementor-lite'),
-                    'estimated-salary'    => esc_html__('Estimated Salary', 'essential-addons-for-elementor-lite'),
-                    'event'    => esc_html__('Event', 'essential-addons-for-elementor-lite'),
-                    'fact-check'    => esc_html__('Fact Check', 'essential-addons-for-elementor-lite'),
-                    'faq'    => esc_html__('FAQ', 'essential-addons-for-elementor-lite'),
-                    'home-activities'    => esc_html__('Home Activities', 'essential-addons-for-elementor-lite'),
-                    'how-to'    => esc_html__('How To', 'essential-addons-for-elementor-lite'),
-                    'image-license'    => esc_html__('Image License', 'essential-addons-for-elementor-lite'),
-                    'job-posting'    => esc_html__('Job Posting', 'essential-addons-for-elementor-lite'),
-                    'job-training'    => esc_html__('Job Training', 'essential-addons-for-elementor-lite'),
-                    'local-business'    => esc_html__('Local Business', 'essential-addons-for-elementor-lite'),
-                    'logo'    => esc_html__('Logo', 'essential-addons-for-elementor-lite'),
-                    'math-solvers'    => esc_html__('Math Solvers', 'essential-addons-for-elementor-lite'),
-                    'movie'    => esc_html__('Movie', 'essential-addons-for-elementor-lite'),
-                    'practice-problems'    => esc_html__('Practice Problems', 'essential-addons-for-elementor-lite'),
-                    'product'    => esc_html__('Product', 'essential-addons-for-elementor-lite'),
-                    'q-and-a'    => esc_html__('Q&A', 'essential-addons-for-elementor-lite'),
-                    'recipe'    => esc_html__('Recipe', 'essential-addons-for-elementor-lite'),
-                    'review-snippet'    => esc_html__('Review Snippet', 'essential-addons-for-elementor-lite'),
-                    'sitelinks-search-box'    => esc_html__('Sitelinks Search Box', 'essential-addons-for-elementor-lite'),
-                    'software-app'    => esc_html__('Software App', 'essential-addons-for-elementor-lite'),
-                    'speakable'    => esc_html__('Speakable', 'essential-addons-for-elementor-lite'),
-                    'subscription-and-paywalled-content'    => esc_html__('Subscription And Paywalled Content', 'essential-addons-for-elementor-lite'),
-                    'video'    => esc_html__('Video', 'essential-addons-for-elementor-lite'),
-                ],
-                'condition'    => [
-                    'eael_adv_accordion_faq_schema_show' => 'yes',
-                ],
-            ]
-        );
-
         $this->end_controls_section();
     }
 
@@ -1159,51 +1112,31 @@ class Adv_Accordion extends Widget_Base
         echo '</div>';
         ?>
 
-        <!-- FAQ Shema : Starts-->
+        <!-- FAQ Schema : Starts-->
         <?php if( !empty( $settings['eael_adv_accordion_faq_schema_show'] ) && 'yes' === $settings['eael_adv_accordion_faq_schema_show'] ): ?>
         <script type="application/ld+json">
         {
-        "@context": "https://schema.org",
-        "@type": "FAQPage",
-        "mainEntity": [{
-            "@type": "Question",
-            "name": "What is the return policy?",
-            "acceptedAnswer": {
-            "@type": "Answer",
-            "text": "<p>Most unopened items in new condition and returned within <b>90 days</b> will receive a refund or exchange. Some items have a modified return policy noted on the receipt or packing slip. Items that are opened or damaged or do not have a receipt may be denied a refund or exchange. Items purchased online or in-store may be returned to any store.</p><p>Online purchases may be returned via a major parcel carrier. <a href=http://example.com/returns> Click here </a> to initiate a return.</p>"
-            }
-        }, {
-            "@type": "Question",
-            "name": "How long does it take to process a refund?",
-            "acceptedAnswer": {
-            "@type": "Answer",
-            "text": "We will reimburse you for returned items in the same way you paid for them. For example, any amounts deducted from a gift card will be credited back to a gift card. For returns by mail, once we receive your return, we will process it within 4–5 business days. It may take up to 7 days after we process the return to reflect in your account, depending on your financial institution's processing time."
-            }
-        }, {
-            "@type": "Question",
-            "name": "What is the policy for late/non-delivery of items ordered online?",
-            "acceptedAnswer": {
-            "@type": "Answer",
-            "text": "<p>Our local teams work diligently to make sure that your order arrives on time, within our normaldelivery hours of 9AM to 8PM in the recipient's time zone. During  busy holiday periods like Christmas, Valentine's and Mother's Day, we may extend our delivery hours before 9AM and after 8PM to ensure that all gifts are delivered on time. If for any reason your gift does not arrive on time, our dedicated Customer Service agents will do everything they can to help successfully resolve your issue.</p><p><a href=https://example.com/orders/>Click here</a> to complete the form with your order-related question(s).</p>"
-            }
-        }, {
-            "@type": "Question",
-            "name": "When will my credit card be charged?",
-            "acceptedAnswer": {
-            "@type": "Answer",
-            "text": "We'll attempt to securely charge your credit card at the point of purchase online. If there's a problem, you'll be notified on the spot and prompted to use another card. Once we receive verification of sufficient funds, your payment will be completed and transferred securely to us. Your account will be charged in 24 to 48 hours."
-            }
-        }, {
-            "@type": "Question",
-            "name": "Will I be charged sales tax for online orders?",
-            "acceptedAnswer": {
-            "@type": "Answer",
-            "text":"Local and State sales tax will be collected if your recipient's mailing address is in: <ul><li>Arizona</li><li>California</li><li>Colorado</li></ul>"}
-            }]
+            "@context": "https://schema.org",
+            "@type": "FAQPage",
+            "mainEntity": [
+                <?php foreach( $settings['eael_adv_accordion_tab'] as $index => $tab ): ?>
+                {
+                    "@type": "Question",
+                    "name": "<?php echo Helper::eael_wp_kses($tab['eael_adv_accordion_tab_title']); ?>",
+                    "acceptedAnswer": {
+                        "@type": "Answer",
+                        "text": "<?php echo ('content' === $tab['eael_adv_accordion_text_type']) ? '<p>' . do_shortcode($tab['eael_adv_accordion_tab_content']) . '</p>' : ''; ?>"
+                    }
+                }
+                <?php if( count($settings['eael_adv_accordion_tab']) > $index + 1): ?>
+                ,
+                <?php endif; ?>
+                <?php endforeach; ?>
+            ]
         }
         </script>
         <?php endif; ?>
-        <!-- FAQ Shema : Ends-->
+        <!-- FAQ Schema : Ends-->
 
         <?php 
     }
