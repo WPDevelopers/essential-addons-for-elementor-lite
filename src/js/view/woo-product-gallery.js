7aae570a818686987d2aa30d39f2f8dcfaa55c57
jakaria-wpdeveloper <jakaria.wpdeveloper@gmail.com>
2022-03-10 10:34:17 +0600

    popup qty issue fixed

--- src/js/view/woo-product-gallery.js
+++ src/js/view/woo-product-gallery.js
@@ -105,38 +105,6 @@ ea.hooks.addAction("init", "ea", () => {
 				"1"
 			);
 		}
-
-		$(document).on('click', '.theme-savoy .nm-qty-minus, .theme-savoy .nm-qty-plus', function(e) {
-			// Get elements and values
-			var $this		= $(this),
-				$qty		= $this.closest('.quantity').find('.qty'),
-				currentVal	= parseFloat($qty.val()),
-				max			= parseFloat($qty.attr('max')),
-				min			= parseFloat($qty.attr('min')),
-				step		= $qty.attr('step');
-
-			// Format values
-			if (!currentVal || currentVal === '' || currentVal === 'NaN') currentVal = 0;
-			if (max === '' || max === 'NaN') max = '';
-			if (min === '' || min === 'NaN') min = 0;
-			if (step === 'any' || step === '' || step === undefined || parseFloat(step) === 'NaN') step = 1;
-            
-			// Change the value
-			if ($this.hasClass('nm-qty-plus')) {
-				if (max && (max == currentVal || currentVal > max)) {
-					$qty.val(max);
-				} else {
-					$qty.val(currentVal + parseFloat(step));
-				}
-			} 
-			else {
-				if (min && (min == currentVal || currentVal < min)) {
-					$qty.val(min);
-				} else if (currentVal > 0) {
-					$qty.val(currentVal - parseFloat(step));
-				}
-			}
-		});
 	};
 
 	elementorFrontend.hooks.addAction(
