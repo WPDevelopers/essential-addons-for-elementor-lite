7fe58325dc4b97c8f25e1e296a0febbd3e577f66
Manzur Ahammed <manzur.ahammed236@gmail.com>
2022-03-23 12:26:46 +0600

    fixed php error

--- includes/Traits/Elements.php
+++ includes/Traits/Elements.php
@@ -20,7 +20,7 @@ trait Elements
 	 */
 	public function register_controls( $controls_manager ) {
 		if ( version_compare( ELEMENTOR_VERSION, '3.5.0', '>=' ) ) {
-			$controls_manager->register( new \Essential_Addons_Elementor\Controls\Select2(), 'eael-select2' );
+			$controls_manager->register( new \Essential_Addons_Elementor\Controls\Select2() );
 		} else {
 			$controls_manager->register_control( 'eael-select2', new \Essential_Addons_Elementor\Controls\Select2() );
 		}
