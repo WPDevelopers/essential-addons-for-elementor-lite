63bbefc4cfdfd245749033ea873c6b806829a703
Manzur Ahammed <manzur.ahammed236@gmail.com>
2022-03-31 17:17:00 +0600

    added new class

---
+++ includes/Classes/Elements_Manager.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Essential_Addons_Elementor\Classes;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // Exit if accessed directly
+
+use Elementor\Plugin;
+
+class Elements_Manager {
+
+}
