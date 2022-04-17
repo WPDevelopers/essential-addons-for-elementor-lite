d72e2be8b5bf1015012d21c4aa2e6a4b3d9be127
Linkon Miyan <rudlinkon@gmail.com>
2022-04-17 16:15:59 +0600

    fix glitch on quick-view gallery items

--- src/js/view/quick-view.js
+++ src/js/view/quick-view.js
@@ -41,7 +41,7 @@ const QuickView = {
 					
 					       popup_view.find(".eael-popup-details-render").html(popup_details);
 					       const product_gallery = popup_view.find(".woocommerce-product-gallery");
-					       product_gallery.css("opacity",1);
+
 					       popup_view_render.addClass("elementor-" + quickview_setting.page_id)
 					       popup_view_render.children().addClass("elementor-element elementor-element-" + quickview_setting.widget_id)
 					
@@ -50,7 +50,12 @@ const QuickView = {
 					       } else {
 						       popup_details.css("height", "auto");
 					       }
-					       setTimeout(function(){ product_gallery.wc_product_gallery(); }, 1000);
+						   setTimeout(function () {
+							   product_gallery.wc_product_gallery();
+						   }, 1000);
+						   setTimeout(function () {
+							   product_gallery.css("opacity", 1);
+						   }, 1500);
 				       }
 			       },
 		       });
