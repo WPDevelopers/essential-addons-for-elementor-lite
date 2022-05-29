47d4392bd44d91a5a2bfa81a62033473d836de58
Manzur Ahammed <manzur.ahammed236@gmail.com>
2022-05-29 15:52:30 +0600

    Fix Security token did not match issue
    - create new nonce token through AJAX request
    so cache problem not happened again

--- src/js/view/general.js
+++ src/js/view/general.js
@@ -70,6 +70,8 @@ ea.hooks.addAction("widgets.reinit", "ea", ($content) => {
 	}
 });
 
+
+
 jQuery(window).on("elementor/frontend/init", function () {
 	window.isEditMode = elementorFrontend.isEditMode();
 	window.ea.isEditMode = elementorFrontend.isEditMode();
@@ -83,7 +85,28 @@ jQuery(window).on("elementor/frontend/init", function () {
 	}
 });
 
+
+
 (function ($) {
+
+	ea.getToken = () => {
+		if (localize.nonce && !ea.noncegenerated) {
+			ea.noncegenerated = true;
+			$.ajax({
+				url: localize.ajaxurl,
+				type: "post",
+				data: {
+					action: "eael_get_token",
+				},
+				success: function (response) {
+					if (response.success) {
+						localize.nonce = response.data.nonce
+					}
+				}
+			});
+		}
+	}
+
 	$(document).on('click', 'a', function (e) {
 		var hashURL = $(this).attr('href'),
 			isStartWithHash;
