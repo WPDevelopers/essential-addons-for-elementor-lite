7f71bf057670ffe3c6e57262cfab25cede504298
Linkon Miyan <rudlinkon@gmail.com>
2022-06-26 16:49:24 +0600

    fix event link not showing properly

--- src/js/view/event-calendar.js
+++ src/js/view/event-calendar.js
@@ -223,6 +223,8 @@ var EventCalendar = function ($scope, $) {
 						}
 						if (event.url == "") {
 							$(".eaelec-modal-footer a").css("display", "none");
+						} else {
+							$(".eaelec-modal-footer a").css("display", "block");
 						}
 						
 						// Popup color
