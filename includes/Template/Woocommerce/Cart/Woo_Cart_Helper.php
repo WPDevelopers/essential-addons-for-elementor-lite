52a3553acf6fb1fe4c2b9f0488fada1f87203dd5
Linkon Miyan <rudlinkon@gmail.com>
2022-07-31 13:02:11 +0600

    update remove_woocommerce_cross_sell_display method with $settings parameter

--- includes/Template/Woocommerce/Cart/Woo_Cart_Helper.php
+++ includes/Template/Woocommerce/Cart/Woo_Cart_Helper.php
@@ -346,10 +346,10 @@ trait Woo_Cart_Helper {
 		}
 	}
 
-    public function remove_woocommerce_cross_sell_display() {
-	    // Remove default 'woocommerce_cross_sell_display' callback from 'woocommerce_cart_collaterals'
-	    remove_action( 'woocommerce_cart_collaterals', 'woocommerce_cross_sell_display' );
-    }
+	public function remove_woocommerce_cross_sell_display( $settings ) {
+		// Remove default 'woocommerce_cross_sell_display' callback from 'woocommerce_cart_collaterals'
+		remove_action( 'woocommerce_cart_collaterals', 'woocommerce_cross_sell_display' );
+	}
 
 	public static function eael_cart_button_proceed_to_checkout( $settings ) {
 		if ( ! empty( $settings['ea_woo_cart_layout'] ) ) {
