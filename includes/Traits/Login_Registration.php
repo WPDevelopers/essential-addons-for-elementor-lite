35fb43ce34a3beb56363ca3992e4eb8748b520d4
Manzur Ahammed <manzur.ahammed236@gmail.com>
2022-08-03 15:06:09 +0600

    fixed login-register token issue

--- includes/Traits/Login_Registration.php
+++ includes/Traits/Login_Registration.php
@@ -88,7 +88,7 @@ trait Login_Registration {
             }
 		}
 
-		if ( ! wp_verify_nonce( $_POST['eael-login-nonce'], 'eael-login-action' ) ) {
+		if ( ! wp_verify_nonce( $_POST['eael-login-nonce'], 'essential-addons-elementor' ) ) {
 			$err_msg = __( 'Security token did not match', 'essential-addons-for-elementor-lite' );
 			if ( $ajax ) {
 				wp_send_json_error( $err_msg );
@@ -205,7 +205,7 @@ trait Login_Registration {
                 exit();
             }
 		}
-		if ( ! wp_verify_nonce( $_POST['eael-register-nonce'], 'eael-register-action' ) ) {
+		if ( ! wp_verify_nonce( $_POST['eael-register-nonce'], 'essential-addons-elementor' ) ) {
 			if ( $ajax ) {
 				wp_send_json_error( __( 'Security token did not match', 'essential-addons-for-elementor-lite' ) );
 			}
