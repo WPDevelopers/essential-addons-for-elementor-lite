8db19f63befd3d3fbdc91d3ec62b2ab3a9990d9c
Linkon Miyan <rudlinkon@gmail.com>
2022-10-13 11:37:32 +0600

    added content change functionality based on pagination click

--- includes/Traits/Helper.php
+++ includes/Traits/Helper.php
@@ -525,13 +525,13 @@ trait Helper
 
         <script id="eael-gb-eb-popup-template" type="text/html">
             <div class="eael-gb-eb-popup">
-                <div class="eael-gb-eb-popup-content">
+                <div class="eael-gb-eb-popup-content --page-1">
                     <div class="eael-gb-eb-header">
                         <img src="<?php echo esc_url( $eb_promo_cross ); ?>" class="eael-gb-eb-dismiss" alt="">
                     </div>
                     <div class="eael-gb-eb-content">
                         <div class="eael-gb-eb-content-image">
-                            <img src="<?php echo esc_url( $eb_promo_img2 ); ?>" alt="">
+                            <img src="<?php echo esc_url( $eb_promo_img1 ); ?>" alt="">
                         </div>
                         <div class="eael-gb-eb-content-pagination">
                             <span class="active" data-page="1"></span>
