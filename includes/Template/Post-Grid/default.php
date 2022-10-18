f979932a7c458a1a6d760e8d7f5bfab8b00e3a70
Robin <robin@wpdeveloper.com>
2022-10-18 18:45:08 +0600

    Fix: Post Grid | Style 2 and default layout updated

--- includes/Template/Post-Grid/default.php
+++ includes/Template/Post-Grid/default.php
@@ -68,13 +68,13 @@ if ($settings['eael_post_grid_preset_style'] === 'two') {
 
                     if ($settings['meta_position'] == 'meta-entry-header') {
                         echo '<div class="eael-entry-header-after">';
-                        if ($settings['eael_show_avatar'] === 'yes') {
+                        if ($settings['eael_show_avatar_two'] === 'yes') {
                             echo '<div class="eael-author-avatar"><a href="' . get_author_posts_url(get_the_author_meta('ID')) . '">' . get_avatar(get_the_author_meta('ID'), 96) . '</a></div>';
                         }
 
                         if ($settings['eael_show_meta']) {
                             echo '<div class="eael-entry-meta">';
-                            if ($settings['eael_show_author'] === 'yes') {
+                            if ($settings['eael_show_author_two'] === 'yes') {
                                 echo '<span class="eael-posted-by">' . get_the_author_posts_link() . '</span>';
                             }
                             if ($settings['eael_show_date'] === 'yes') {
@@ -107,45 +107,54 @@ if ($settings['eael_post_grid_preset_style'] === 'two') {
                     }
 
                     if ($settings['meta_position'] == 'meta-entry-footer') {
-                        echo '<div class="eael-entry-meta">';
-                        if ($settings['eael_show_date'] === 'yes') {
-                            echo '<span class="eael-meta-posted-on"><i class="far fa-clock"></i><time datetime="' . get_the_date() . '">' . get_the_date() . '</time></span>';
-                        }
-                        if ($settings['eael_show_post_terms'] === 'yes') {
-                            if ($settings['eael_post_terms'] === 'category') {
-                                $terms = get_the_category();
+                        echo '<div class="eael-entry-header-after style-two">';
+                            if ($settings['eael_show_avatar_two'] === 'yes') {
+                                echo '<div class="eael-author-avatar"><a href="' . get_author_posts_url(get_the_author_meta('ID')) . '">' . get_avatar(get_the_author_meta('ID'), 96) . '</a></div>';
                             }
-                            if ($settings['eael_post_terms'] === 'tags') {
-                                $terms = get_the_tags();
-                            }
-                            if (!empty($terms)) {
-                                $html = '<ul class="post-meta-categories">';
-                                $count = 0;
-                                
-                                $eael_post_terms_separator = ! empty( $settings['eael_post_terms_separator'] ) ? wp_strip_all_tags( $settings['eael_post_terms_separator'] ) : '';
-                                foreach ($terms as $term) {
-                                    if ($count === intval($settings['eael_post_terms_max_length'])) {
-                                        break;
-                                    }
-                                    if ($count === 0) {
-                                        $html .= '<li class="meta-cat-icon"><i class="far fa-folder-open"></i></li>';
-                                    }
 
-                                    $is_last_item = $count + 1 === intval($settings['eael_post_terms_max_length']) || $count + 1 === count($terms);
-                                    $eael_post_terms_separator = $is_last_item  ? '' : $eael_post_terms_separator; 
-                                
-                                    $link = ($settings['eael_post_terms'] === 'category') ? get_category_link($term->term_id) : get_tag_link($term->term_id);
-                                    $html .= '<li>';
-                                    $html .= '<a href="' . esc_url($link) . '">';
-                                    $html .= $term->name . esc_html( $eael_post_terms_separator );
-                                    $html .= '</a>';
-                                    $html .= '</li>';
-                                    $count++;
-                                }
-                                $html .= '</ul>';
-                                echo $html;
+                            echo '<div class="eael-entry-meta">';
+                            if ($settings['eael_show_author_two'] === 'yes') {
+                                echo '<span class="eael-posted-by style-two-footer">' . get_the_author_posts_link() . '</span>';
                             }
-                        }
+                            if ($settings['eael_show_date'] === 'yes') {
+                                echo '<span class="eael-meta-posted-on"><i class="far fa-clock"></i><time datetime="' . get_the_date() . '">' . get_the_date() . '</time></span>';
+                            }
+                            if ($settings['eael_show_post_terms'] === 'yes') {
+                                if ($settings['eael_post_terms'] === 'category') {
+                                    $terms = get_the_category();
+                                }
+                                if ($settings['eael_post_terms'] === 'tags') {
+                                    $terms = get_the_tags();
+                                }
+                                if (!empty($terms)) {
+                                    $html = '<ul class="post-meta-categories">';
+                                    $count = 0;
+                                    
+                                    $eael_post_terms_separator = ! empty( $settings['eael_post_terms_separator'] ) ? wp_strip_all_tags( $settings['eael_post_terms_separator'] ) : '';
+                                    foreach ($terms as $term) {
+                                        if ($count === intval($settings['eael_post_terms_max_length'])) {
+                                            break;
+                                        }
+                                        if ($count === 0) {
+                                            $html .= '<li class="meta-cat-icon"><i class="far fa-folder-open"></i></li>';
+                                        }
+
+                                        $is_last_item = $count + 1 === intval($settings['eael_post_terms_max_length']) || $count + 1 === count($terms);
+                                        $eael_post_terms_separator = $is_last_item  ? '' : $eael_post_terms_separator; 
+                                    
+                                        $link = ($settings['eael_post_terms'] === 'category') ? get_category_link($term->term_id) : get_tag_link($term->term_id);
+                                        $html .= '<li>';
+                                        $html .= '<a href="' . esc_url($link) . '">';
+                                        $html .= $term->name . esc_html( $eael_post_terms_separator );
+                                        $html .= '</a>';
+                                        $html .= '</li>';
+                                        $count++;
+                                    }
+                                    $html .= '</ul>';
+                                    echo $html;
+                                }
+                            }
+                            echo '</div>';
                         echo '</div>';
                     }
                     echo '</div>';
@@ -209,7 +218,7 @@ if ($settings['eael_post_grid_preset_style'] === 'two') {
         if ($settings['meta_position'] == 'meta-entry-footer') {
             if ($settings['eael_show_meta']) {
                 echo '<div class="eael-entry-meta">';
-                if ($settings['eael_show_author'] === 'yes') {
+                if ($settings['eael_show_author_three'] === 'yes') {
                     echo '<span class="eael-posted-by">' . get_the_author_posts_link() . '</span>';
                 }
                 if ($settings['eael_show_date'] === 'yes') {
