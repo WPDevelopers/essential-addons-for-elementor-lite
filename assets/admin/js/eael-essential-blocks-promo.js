8372863a3f7c6dc61cedb4e29f98bf6478fd5951
Linkon Miyan <rudlinkon@gmail.com>
2022-10-19 16:33:09 +0600

    functionality only run if eb not active or client not interested to hide

--- assets/admin/js/eael-essential-blocks-promo.js
+++ assets/admin/js/eael-essential-blocks-promo.js
@@ -1,5 +1,6 @@
 (function ($) {
     let $gb_editor_panel = $('#editor');
+    var $is_popup_button_added = false;
 
     wp.data.subscribe(function () {
         setTimeout(function () {
@@ -8,8 +9,9 @@
     });
 
     function essential_block_button_init() {
-        if (!$('#eael-eb-popup-button').length) {
+        if (!$('#eael-eb-popup-button').length && !$is_popup_button_added) {
             $gb_editor_panel.find('.edit-post-header__settings').prepend($('#eael-gb-eb-button-template').html());
+            $is_popup_button_added = true;
         }
     }
 
