d80a00315c0a8050e6fa7cd2b994fedfe1235348
Robin <robin@wpdeveloper.com>
2022-10-25 09:47:08 +0600

    New Element: NFT Gallery | Initialized

---
+++ includes/Elements/NFT_Gallery.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace Essential_Addons_Elementor\Elements;
+
+// If this file is called directly, abort.
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+use \Elementor\Controls_Manager;
+use \Elementor\Group_Control_Border;
+use \Elementor\Group_Control_Box_Shadow;
+use \Elementor\Group_Control_Image_Size;
+use \Elementor\Group_Control_Typography;
+use Elementor\Icons_Manager;
+use \Elementor\Plugin;
+use \Elementor\Utils;
+use \Elementor\Widget_Base;
+use \Essential_Addons_Elementor\Classes\Helper;
+
+class NFT_Gallery extends Widget_Base
+{
+    public function get_name()
+    {
+        return 'eael-nft-gallery';
+    }
+
+    public function get_title()
+    {
+        return esc_html__('NFT Gallery', 'essential-addons-for-elementor-lite');
+    }
+
+    public function get_icon()
+    {
+        return 'eaicon-info-box';
+    }
+
+    public function get_categories()
+    {
+        return ['essential-addons-elementor'];
+    }
+
+    public function get_keywords()
+    {
+        return [
+            'gallery',
+            'nft gallery',
+            'ea nft gallery',
+            'image gallery',
+            'photo gallery',
+            'portfolio',
+            'ea portfolio',
+            'image grid',
+            'photo grid',
+            'responsive gallery',
+            'ea',
+            'essential addons'
+        ];
+    }
+
+    public function get_custom_help_url()
+    {
+        return 'https://essential-addons.com/elementor/docs/nft-gallery/';
+    }
+
+    protected function register_controls()
+    {
+
+        /**
+         * NFT Image Settings
+         */
+        $this->start_controls_section(
+            'eael_section_nft_gallery_settings',
+            [
+                'label' => esc_html__('NFT Gallery', 'essential-addons-for-elementor-lite'),
+            ]
+        );
+
+        $this->end_controls_section();
+
+        /**
+         * -------------------------------------------
+         * Tab Style (NFT Gallery Style)
+         * -------------------------------------------
+         */
+        $this->start_controls_section(
+            'eael_section_nft_gallery_style_settings',
+            [
+                'label' => esc_html__('NFT Gallery', 'essential-addons-for-elementor-lite'),
+                'tab' => Controls_Manager::TAB_STYLE,
+            ]
+        );
+
+        $this->end_controls_section();
+    }
+
+    public function print_nft_gallery(){
+        ob_start();
+        ?>
+        <div class="ep_nft_content_wrap ep_nft_grid nft_items preset-1">
+            <div class="ep_nft_item">
+                <div class="ep_nft_thumbnail"><img
+                        src="https://lh3.googleusercontent.com/z2cLJdA7S3i6y2GYHvMDhDTsmL0UtXzVaIdj7aXZ8Y2gr_2MxoPH1RIcJrUlwulmDggw4xMEcDrY_GL6eK6C9R-VvI7ZgpUm_EYn"
+                        alt="Angry Cat #3297"></div>
+                <div class="ep_nft_content">
+                    <h3 class="ep_nft_title">Angry Cat #3297</h3>
+                    <div class="ep_nft_content_body">
+                        <div class="ep_nft_owner_wrapper">
+                            <div class="ep_nft_creator"><img
+                                    src="https://i.seadn.io/gcs/files/856e56e379fcd78b5ad956a3eb2d7247.png?w=500&amp;auto=format"
+                                    alt="AngryCat_dev"><span>Created by <a target="_blank"
+                                        href="https://opensea.io/AngryCat_dev">AngryCat_dev</a></span>
+                            </div>
+                        </div>
+                        <div class="ep_nft_price_wrapper">
+
+                            <div class="ep_nft_price ep_current_price">
+                                <span class="eb_nft_label">Price</span>
+                                <span class="eb_nft_currency">
+                                    <svg width="1200" height="450" viewBox="0 0 256 417" xmlns="http://www.w3.org/2000/svg"
+                                        preserveAspectRatio="xMidYMid">
+                                        <path fill="#343434" d="m127.961 0-2.795 9.5v275.668l2.795 2.79 127.962-75.638z"></path>
+                                        <path fill="#8C8C8C" d="M127.962 0 0 212.32l127.962 75.639V154.158z"></path>
+                                        <path fill="#3C3C3B" d="m127.961 312.187-1.575 1.92v98.199l1.575 4.6L256 236.587z">
+                                        </path>
+                                        <path fill="#8C8C8C" d="M127.962 416.905v-104.72L0 236.585z"></path>
+                                        <path fill="#141414" d="m127.961 287.958 127.96-75.637-127.96-58.162z"></path>
+                                        <path fill="#393939" d="m0 212.32 127.96 75.638v-133.8z"></path>
+                                    </svg>
+                                </span>
+                                <span class="eb_nft_price">0</span>
+                            </div>
+
+
+                            <div class="ep_nft_price ep_nft_last_sale">
+                                <span class="eb_nft_label">Last Sale</span>
+                                <span class="eb_nft_currency">
+                                    <svg width="1200" height="450" viewBox="0 0 256 417" xmlns="http://www.w3.org/2000/svg"
+                                        preserveAspectRatio="xMidYMid">
+                                        <path fill="#343434" d="m127.961 0-2.795 9.5v275.668l2.795 2.79 127.962-75.638z"></path>
+                                        <path fill="#8C8C8C" d="M127.962 0 0 212.32l127.962 75.639V154.158z"></path>
+                                        <path fill="#3C3C3B" d="m127.961 312.187-1.575 1.92v98.199l1.575 4.6L256 236.587z">
+                                        </path>
+                                        <path fill="#8C8C8C" d="M127.962 416.905v-104.72L0 236.585z"></path>
+                                        <path fill="#141414" d="m127.961 287.958 127.96-75.637-127.96-58.162z"></path>
+                                        <path fill="#393939" d="m0 212.32 127.96 75.638v-133.8z"></path>
+                                    </svg>
+                                </span>
+                                <span class="eb_nft_price">0.298</span>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="ep_nft_button">
+                        <a target="_blank"
+                            href="https://opensea.io/assets/ethereum/0xdcf68c8ebb18df1419c7dff17ed33505faf8a20c/9999">Sea
+                            Details</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <?php 
+        echo ob_get_clean();
+    }
+
+    protected function render()
+    {
+        $this->print_nft_gallery();
+    }
+}
