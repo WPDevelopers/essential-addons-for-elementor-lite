c0c9c114eb56218f9eb2981bd5db541990156f86
Jakaria Istauk <jakaria.wpdeveloper@gmail.com>
2022-11-28 11:47:39 +0600

    Woo Checkout: Form row width issue fixed

--- src/js/view/woo-checkout.js
+++ src/js/view/woo-checkout.js
@@ -109,8 +109,9 @@ jQuery(document.body).on('country_to_state_changing', function(event, country, w
 	let reorder_fields = function( type, _wrapper ){
 		let $selector = $(`.woocommerce-${type}-fields__field-wrapper`);
 		_wrapper = typeof _wrapper !== 'undefined' ? _wrapper : wrapper;
-		$.each(checkout_keys[type], function (index, field){
-			let $fieldHtml = _wrapper.find(`#${field}_field`);
+		$.each(checkout_keys[type], function (field_key, form_class){
+			let $fieldHtml = _wrapper.find(`#${field_key}_field`);
+			$fieldHtml.removeClass('form-row-first form-row-last form-row-wide').addClass(form_class);
 			$(`#eael-wc-${type}-reordered-fields .eael-woo-${type}-fields`).append($fieldHtml);
 		});
 		$selector.replaceWith($(`#eael-wc-${type}-reordered-fields`).contents());
