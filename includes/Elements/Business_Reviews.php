219354fee4a356365a74b37fd454860005be9a9b
Robin <robin@wpdeveloper.com>
2022-12-18 23:43:11 +0600

    fetch google reviews from api wip

--- includes/Elements/Business_Reviews.php
+++ includes/Elements/Business_Reviews.php
@@ -120,7 +120,135 @@ class Business_Reviews extends Widget_Base {
      * API Call to Get Business Reviews
      */
 	public function fetch_business_reviews_from_api(){
-		return [];
+		$settings = $this->get_settings();
+
+		$response                        = [];
+		$nft_gallery                     = [];
+		$nft_gallery['source']           = ! empty( $settings['eael_nft_gallery_sources'] ) ? esc_html( $settings['eael_nft_gallery_sources'] ) : 'opensea';
+		$nft_gallery['api_key']          = ! empty( $settings['eael_nft_gallery_source_key'] ) ? esc_html( $settings['eael_nft_gallery_source_key'] ) : 'b61c8a54123d4dcb9acc1b9c26a01cd1';
+		$nft_gallery['opensea_type']     = ! empty( $settings['eael_nft_gallery_opensea_type'] ) ? esc_html( $settings['eael_nft_gallery_opensea_type'] ) : 'assets';
+		$nft_gallery['opensea_filterby'] = ! empty( $settings['eael_nft_gallery_opensea_filterby'] ) ? esc_html( $settings['eael_nft_gallery_opensea_filterby'] ) : 'none';
+		$nft_gallery['order']            = ! empty( $settings['eael_nft_gallery_opensea_order'] ) ? esc_html( $settings['eael_nft_gallery_opensea_order'] ) : 'desc';
+		$nft_gallery['item_limit']       = ! empty( $settings['eael_nft_gallery_opensea_item_limit'] ) ? esc_html( $settings['eael_nft_gallery_opensea_item_limit'] ) : 9;
+
+		$expiration = ! empty( $settings['eael_nft_gallery_opensea_data_cache_time'] ) ? absint( $settings['eael_nft_gallery_opensea_data_cache_time'] ) * MINUTE_IN_SECONDS : DAY_IN_SECONDS;
+		$md5        = md5( $nft_gallery['api_key'] . $nft_gallery['opensea_type'] . $nft_gallery['opensea_filterby'] . $settings['eael_nft_gallery_opensea_filterby_slug'] . $settings['eael_nft_gallery_opensea_filterby_wallet'] . $nft_gallery['item_limit'] . $nft_gallery['order'] . $this->get_id() );
+		$cache_key  = "{$nft_gallery['source']}_{$expiration}_{$md5}_nftg_cache";
+		$items      = get_transient( $cache_key );
+
+		$error_message = '';
+
+		if ( false === $items && 'opensea' === $nft_gallery['source'] ) {
+			$nft_gallery['filterby_slug']   = ! empty( $settings['eael_nft_gallery_opensea_filterby_slug'] ) ? $settings['eael_nft_gallery_opensea_filterby_slug'] : '';
+			$nft_gallery['filterby_wallet'] = ! empty( $settings['eael_nft_gallery_opensea_filterby_wallet'] ) ? $settings['eael_nft_gallery_opensea_filterby_wallet'] : '';
+
+			$url   = "https://api.opensea.io/api/v1";
+			$param = array();
+
+			if ( 'collections' === $nft_gallery['opensea_type'] ) {
+				$url .= "/collections";
+
+				$args = array(
+					'limit'  => $nft_gallery['item_limit'],
+					'offset' => 0,
+				);
+
+				if ( ! empty( $nft_gallery['filterby_wallet'] ) ) {
+					$args['asset_owner'] = sanitize_text_field( $nft_gallery['filterby_wallet'] );
+				}
+
+				$param = array_merge( $param, $args );
+			} elseif ( 'assets' === $nft_gallery['opensea_type'] ) {
+				$url  .= "/assets";
+				$args = array(
+					'include_orders'  => true,
+					'limit'           => $nft_gallery['item_limit'],
+					'order_direction' => $nft_gallery['order'],
+				);
+
+				if ( ! empty( $nft_gallery['filterby_slug'] ) && 'collection-slug' === $nft_gallery['opensea_filterby'] ) {
+					$args['collection_slug'] = sanitize_text_field( $nft_gallery['filterby_slug'] );
+				}
+
+				if ( ! empty( $nft_gallery['filterby_wallet'] ) && 'wallet-address' === $nft_gallery['opensea_filterby'] ) {
+					$args['owner'] = sanitize_text_field( $nft_gallery['filterby_wallet'] );
+				}
+
+				$param = array_merge( $param, $args );
+			} else {
+				$error_message = esc_html__( 'Please provide a valid Type!', 'essential-addons-for-elementor-lite' );
+			}
+
+			$headers = array(
+				'headers' => array(
+					'Content-Type' => 'application/json',
+					'X-API-KEY'    => $nft_gallery['api_key'],
+				)
+			);
+			$options = array(
+				'timeout' => 240
+			);
+
+			$options = array_merge( $headers, $options );
+
+			if ( empty( $error_message ) ) {
+				$response = wp_remote_get(
+					esc_url_raw( add_query_arg( $param, $url ) ),
+					$options
+				);
+
+				$body     = json_decode( wp_remote_retrieve_body( $response ) );
+				$response = 'assets' === $nft_gallery['opensea_type'] && ! empty( $body->assets ) ? $body->assets : $body;
+				$response = 'collections' === $nft_gallery['opensea_type'] && ! empty( $response->collections ) ? $response->collections : $response;
+
+				if ( is_array( $response ) ) {
+					$response = array_splice( $response, 0, absint( $settings['eael_nft_gallery_opensea_item_limit'] ) );
+					set_transient( $cache_key, $response, $expiration );
+					$this->nft_gallery_items_count = count( $response );
+				} else {
+					$error_message_text_wallet = $error_message_text_slug = '';
+
+					if ( isset( $body->assets ) && is_array( $body->assets ) && 0 === count( $body->assets ) ) {
+						$error_message_text_slug = __( 'Please provide a valid collection slug!', 'essential-addons-for-elementor-lite' );
+					}
+
+					if ( ! empty( $body->asset_owner ) && isset( $body->asset_owner[0] ) ) {
+						$error_message_text_wallet = ! empty( $body->asset_owner[0] ) ? $body->asset_owner[0] : __( 'Please provide a valid wallet address!', 'essential-addons-for-elementor-lite' );
+					} else if ( ! empty( $body->owner ) && isset( $body->owner[0] ) ) {
+						$error_message_text_wallet = ! empty( $body->owner[0] ) ? $body->owner[0] : __( 'Please provide a valid wallet address!', 'essential-addons-for-elementor-lite' );
+					}
+
+					if ( 'assets' === $nft_gallery['opensea_type'] && 'collection-slug' === $nft_gallery['opensea_filterby'] ) {
+						$error_message_text = $error_message_text_slug;
+					}
+
+					if ( 'collections' === $nft_gallery['opensea_type'] || ( 'assets' === $nft_gallery['opensea_type'] && 'wallet-address' === $nft_gallery['opensea_filterby'] ) ) {
+						$error_message_text = $error_message_text_wallet;
+					}
+
+					if ( ! empty( $error_message_text ) ) {
+						$error_message = esc_html( $error_message_text );
+					}
+				}
+			}
+
+			$data = [
+				'items'         => $response,
+				'error_message' => $error_message,
+			];
+
+			return $data;
+		}
+
+		$response                      = $items ? $items : $response;
+		$this->nft_gallery_items_count = count( $response );
+
+		$data = [
+			'items'         => $response,
+			'error_message' => $error_message,
+		];
+
+		return $data;
 	}
 
 	protected function render() {
