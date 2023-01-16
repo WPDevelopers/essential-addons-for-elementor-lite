fe61cea2eac3c5e4e01637756fc88721838bcbc7
Jakaria Istauk <jakaria.wpdeveloper@gmail.com>
2023-01-16 11:58:25 +0600

    SVG Draw: Drowning line animation.......

---
+++ assets/front-end/js/view/svg-draw.js
@@ -0,0 +1,100 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./src/js/view/svg-draw.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./src/js/view/svg-draw.js":
+/*!*********************************!*\
+  !*** ./src/js/view/svg-draw.js ***!
+  \*********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("function _typeof(obj) { \"@babel/helpers - typeof\"; return _typeof = \"function\" == typeof Symbol && \"symbol\" == typeof Symbol.iterator ? function (obj) { return typeof obj; } : function (obj) { return obj && \"function\" == typeof Symbol && obj.constructor === Symbol && obj !== Symbol.prototype ? \"symbol\" : typeof obj; }, _typeof(obj); }\nvar SVGDraw_old = function SVGDraw_old($scope, $) {\n  var svg_icon = $('svg', $scope),\n    paths = $('path', svg_icon),\n    count = 50,\n    path_count = 0,\n    draw_interval;\n  var draw_line = function draw_line() {\n    var step = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : .5;\n    if (count < 0 && path_count <= paths.length) {\n      count = 50;\n      $(paths[path_count]).css({\n        'stroke-dasharray': 'none'\n      });\n      path_count++;\n    } else if (path_count === paths.length) {\n      clearInterval(draw_interval);\n      return;\n    }\n    if (_typeof(paths[path_count]) !== undefined) {\n      var count_1 = 50 - count;\n      $(paths[path_count]).css({\n        'stroke-dasharray': count_1 + 'px, ' + count + 'px'\n      });\n    }\n    count -= step;\n  };\n  svg_icon.hover(function () {\n    draw_interval = window.setInterval(draw_line, .001);\n  }, function () {\n    window.clearInterval(draw_interval);\n  });\n  $(window).on('scroll', function (e) {\n    console.log('scroll');\n    draw_line(1);\n  });\n};\nvar SVGDraw = function SVGDraw($scope, $) {\n  var svg_icon = $('svg', $scope),\n    paths = $('path', svg_icon),\n    count = 50,\n    path_count = 0,\n    elemID = $scope.data(\"id\"),\n    draw_interval;\n  var animRev = 'pause play reverse',\n    timeLine = new TimelineMax({\n      repeat: 0,\n      yoyo: true,\n      scrollTrigger: {\n        trigger: '.elementor-element-' + elemID,\n        toggleActions: \"play \" + animRev,\n        start: \"top 100\" //when the top of the element hits that offset of the viewport.\n      }\n    });\n\n  console.log(timeLine);\n};\njQuery(window).on(\"elementor/frontend/init\", function () {\n  // if (ea.elementStatusCheck('eaelSVGDraw')) {\n  //     return false;\n  // }\n\n  elementorFrontend.hooks.addAction(\"frontend/element_ready/eael-svg-draw.default\", SVGDraw);\n});\n\n//# sourceURL=webpack:///./src/js/view/svg-draw.js?");
+
+/***/ })
+
+/******/ });
--- src/js/view/svg-draw.js
+++ src/js/view/svg-draw.js
@@ -1,24 +1,59 @@
-var SVGDraw = function ($scope, $) {
-    let svg_icon = $('svg', $scope);
-    let count = 50;
-    function draw_line() {
-        // your original if-else wasn't allowing count to increment past 1
-        if (count < 0) {
+var SVGDraw_old = function ($scope, $) {
+    let svg_icon = $('svg', $scope),
+        paths = $('path', svg_icon),
+            count = 50,
+        path_count = 0,
+        draw_interval;
+
+    let draw_line = function (step=.5){
+        if (count < 0 && path_count <= paths.length ){
+            count = 50;
+            $(paths[path_count]).css({'stroke-dasharray': 'none'});
+            path_count++;
+        }
+        else if (path_count===paths.length){
+            clearInterval(draw_interval);
             return;
         }
-        let count_1 = 50-count;
-        $('path', svg_icon).css({'stroke-dashoffset': '0px', 'stroke-dasharray': count_1+'px, '+count+'px'});
-        count--;
+        if ( typeof paths[path_count] !== undefined ){
+            let count_1 = 50-count;
+            $(paths[path_count]).css({'stroke-dasharray': count_1+'px, '+count+'px'});
+        }
+        count-= step;
     }
 
-    setInterval(draw_line,90);
-}
-// stroke-dashoffset: 63;
-// stroke-dasharray: 0px, 38px;
-// stroke: #c36;
-// stroke-width: 1px;
-// fill: transparent;
+    svg_icon.hover(function (){
+        draw_interval = window.setInterval(draw_line, .001);
+    },function (){
+        window.clearInterval(draw_interval);
+    });
 
+    $(window).on('scroll', function (e){
+        console.log('scroll')
+        draw_line(1);
+    })
+
+}
+var SVGDraw = function ($scope, $) {
+    let svg_icon = $('svg', $scope),
+        paths = $('path', svg_icon),
+        count = 50,
+        path_count = 0,
+    elemID = $scope.data("id"),
+        draw_interval;
+
+    var animRev = 'pause play reverse',
+        timeLine = new TimelineMax({
+            repeat: 0,
+            yoyo: true,
+            scrollTrigger: {
+                trigger: '.elementor-element-' + elemID,
+                toggleActions: "play " + animRev,
+                start: "top 100", //when the top of the element hits that offset of the viewport.
+            }
+        });
+    console.log(timeLine );
+}
 jQuery(window).on("elementor/frontend/init", function () {
 
     // if (ea.elementStatusCheck('eaelSVGDraw')) {
