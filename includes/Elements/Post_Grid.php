798cdd808b2b9fe7d67fcc5e65aea55630031373
Jakaria Istauk <jakaria.wpdeveloper@gmail.com>
2023-01-19 19:20:48 +0600

    Post Grid: Load more daynamic value issue fixed

--- includes/Elements/Post_Grid.php
+++ includes/Elements/Post_Grid.php
@@ -1088,6 +1088,9 @@ class Post_Grid extends Widget_Base
 	    $dir_name = $this->get_temp_dir_name($settings['loadable_file_name']);
 	    $found_posts = 0;
         $posts_per_page = isset($args['posts_per_page']) && $args['posts_per_page'] > 0 ? $args['posts_per_page'] : -1 ;
+
+        set_transient( 'eael_post_grid_read_more_button_text_'. $this->get_id(), $this->get_settings_for_display('read_more_button_text'), DAY_IN_SECONDS );
+        set_transient( 'eael_post_grid_excerpt_expanison_indicator_'. $this->get_id(), $this->get_settings_for_display('excerpt_expanison_indicator'), DAY_IN_SECONDS );
         $settings['read_more_button_text'] = $this->get_settings_for_display('read_more_button_text');
         $settings['excerpt_expanison_indicator'] = $this->get_settings_for_display('excerpt_expanison_indicator');
 
