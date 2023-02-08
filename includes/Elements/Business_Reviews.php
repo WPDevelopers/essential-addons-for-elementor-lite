016d2233b02cdde328c7b7d3e21988cc4d88233b
Robin <robin@wpdeveloper.com>
2023-02-08 17:11:24 +0600

    spacing issue fixed

--- includes/Elements/Business_Reviews.php
+++ includes/Elements/Business_Reviews.php
@@ -442,7 +442,7 @@ class Business_Reviews extends Widget_Base {
 				'label_on'     => __( 'Show', 'essential-addons-for-elementor-lite' ),
 				'label_off'    => __( 'Hide', 'essential-addons-for-elementor-lite' ),
 				'return_value' => 'yes',
-				'default'      => '',
+				'default'      => 'yes',
 			]
 		);
 
@@ -628,6 +628,7 @@ class Business_Reviews extends Widget_Base {
 				'label'      => esc_html__( 'Padding', 'essential-addons-for-elementor-lite' ),
 				'type'       => Controls_Manager::DIMENSIONS,
 				'size_units' => [ 'px', 'em' ],
+				'default'     => [ 'size' => 15 ],
 				'selectors'  => [
 					'{{WRAPPER}} .eael-business-reviews-wrapper .eael-business-reviews-items' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
 				],
@@ -896,7 +897,7 @@ class Business_Reviews extends Widget_Base {
 				'label'     => __( 'Logo Size', 'essential-addons-for-elementor-lite' ),
 				'type'      => Controls_Manager::SLIDER,
 				'default'   => [
-					'size' => 15,
+					'size' => 100,
 				],
 				'range'     => [
 					'px' => [
@@ -2597,21 +2598,21 @@ class Business_Reviews extends Widget_Base {
 						<div class="eael-google-reviews-slider-header">
 							<?php if( $business_reviews['business_logo'] ): ?>
 							<div class="eael-google-reviews-business-logo">
-								<?php if ( $business_reviews['business_logo_icon_migrated'] || $business_reviews['business_logo_icon_new'] ) { ?>
-									<?php if ( isset($business_reviews['business_logo_icon_new_data']['value']['url'])) : ?>
-										<!-- <img class="eael-google-reviews-business-logo-icon" src="<?php //echo esc_url( $business_reviews['business_logo_icon_new_data']['value']['url'] ); ?>" alt="<?php //echo esc_attr( get_post_meta( $business_reviews['business_logo_icon_new_data']['value']['id'], '_wp_attachment_image_alt', true ) ); ?>" /> -->
-
-										<?php Icons_Manager::render_icon( $business_reviews['business_logo_icon_new_data'], [ 'aria-hidden' => 'true' ] ); ?>
-									<?php elseif ( isset($business_reviews['business_logo_icon_new_data']['value'])) : 
-											if( empty( $business_reviews['business_logo_icon_new_data']['value'] ) ){
-												?> <svg xmlns="http://www.w3.org/2000/svg" height="100" width="100"><rect width="100" height="100" fill="#fff" rx="15%"/><path fill="#4285f4" d="M386 400c45-42 65-112 53-179H260v74h102c-4 24-18 44-38 57z"/><path fill="#34a853" d="M90 341a192 192 0 0 0 296 59l-62-48c-53 35-141 22-171-60z"/><path fill="#fbbc02" d="M153 292c-8-25-8-48 0-73l-63-49c-23 46-30 111 0 171z"/><path fill="#ea4335" d="M153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55z"/></svg>	<?php 
-											}
-											?>
-											<span class="eael-google-reviews-business-logo-icon <?php echo esc_attr( $business_reviews['business_logo_icon_new_data']['value'] ); ?>" aria-hidden="true"></span>
-									<?php endif; ?>
-								<?php } else { ?>
-									<span class="eael-google-reviews-business-logo-icon <?php echo esc_attr( $business_reviews['business_logo_icon_data'] ); ?>" aria-hidden="true"></span>
-								<?php } ?>
+								<?php 
+								if ( $business_reviews['business_logo_icon_migrated'] || $business_reviews['business_logo_icon_new'] ) {
+									if ( isset($business_reviews['business_logo_icon_new_data']['value']['url'])) {
+										Icons_Manager::render_icon( $business_reviews['business_logo_icon_new_data'], [ 'aria-hidden' => 'true' ] );
+									} elseif ( isset($business_reviews['business_logo_icon_new_data']['value'])) {
+										if( empty( $business_reviews['business_logo_icon_new_data']['value'] ) ){
+											?> <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48 48" width="480px" height="480px"><path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"/><path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"/><path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"/><path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"/></svg> <?php 
+										} else {
+											printf('<span class="eael-google-reviews-business-logo-icon %s" aria-hidden="true"></span>', esc_attr( $business_reviews['business_logo_icon_new_data']['value'] ));
+										}
+									}
+								} else {
+									printf('<span class="eael-google-reviews-business-logo-icon %s" aria-hidden="true"></span>', esc_attr( $business_reviews['business_logo_icon_data'] ) ); 
+								} 
+								?>
 							</div>
 							<?php endif; ?>
 							
