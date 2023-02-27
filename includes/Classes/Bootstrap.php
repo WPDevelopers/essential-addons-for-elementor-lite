54d26ae8c8e6c5fe39f34256403f8afdf6b2972b
Linkon Miyan <rudlinkon@gmail.com>
2023-02-27 17:52:43 +0600

    remove black friday notice

--- includes/Classes/Bootstrap.php
+++ includes/Classes/Bootstrap.php
@@ -283,9 +283,9 @@ class Bootstrap
 	        add_action( 'eael_admin_page_setting', [ $this, 'eael_show_admin_menu_notice' ] );
 
 	        // Black Friday Optin
-	        add_action( 'admin_notices', [ $this, 'eael_black_friday_optin' ] );
-	        add_action( 'eael_admin_notices', [ $this, 'eael_black_friday_optin' ] );
-	        add_action( 'wp_ajax_eael_black_friday_optin_dismiss', [ $this, 'eael_black_friday_optin_dismiss' ] );
+//	        add_action( 'admin_notices', [ $this, 'eael_black_friday_optin' ] );
+//	        add_action( 'eael_admin_notices', [ $this, 'eael_black_friday_optin' ] );
+//	        add_action( 'wp_ajax_eael_black_friday_optin_dismiss', [ $this, 'eael_black_friday_optin_dismiss' ] );
         }
 
 	    // beehive theme compatibility
