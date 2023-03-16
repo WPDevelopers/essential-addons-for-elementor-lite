2dcd4523874720e3992852b1dd2d014ad8a7e59f
Linkon Miyan <rudlinkon@gmail.com>
2023-03-16 18:29:54 +0600

    removed business review widget from basic section on setup wizard

--- includes/Classes/WPDeveloper_Setup_Wizard.php
+++ includes/Classes/WPDeveloper_Setup_Wizard.php
@@ -691,7 +691,6 @@ class WPDeveloper_Setup_Wizard {
 					[
 						'key'         => 'business-reviews',
 						'title'       => __( 'Business Reviews', 'essential-addons-for-elementor-lite' ),
-						'preferences' => 'basic',
 					],
 				]
 			],
