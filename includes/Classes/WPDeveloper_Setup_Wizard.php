7283048e9fef6a582ad861886b43b25976cfff9e
Linkon Miyan <rudlinkon@gmail.com>
2023-03-16 18:24:27 +0600

    fixed -- business review is being inactive when completing setup wizard

--- includes/Classes/WPDeveloper_Setup_Wizard.php
+++ includes/Classes/WPDeveloper_Setup_Wizard.php
@@ -688,6 +688,11 @@ class WPDeveloper_Setup_Wizard {
 						'title'       => __( 'NFT Gallery', 'essential-addons-for-elementor-lite' ),
 						'preferences' => 'basic',
 					],
+					[
+						'key'         => 'business-reviews',
+						'title'       => __( 'Business Reviews', 'essential-addons-for-elementor-lite' ),
+						'preferences' => 'basic',
+					],
 				]
 			],
 			'creative-elements'        => [
