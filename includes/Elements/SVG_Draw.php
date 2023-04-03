a61f82ca79e3a39b54dd4371c5697a52448203fc
Linkon Miyan <rudlinkon@gmail.com>
2023-04-03 14:37:45 +0600

    update svg draw widget icon

--- includes/Elements/SVG_Draw.php
+++ includes/Elements/SVG_Draw.php
@@ -33,7 +33,7 @@ class SVG_Draw Extends Widget_Base
 
     public function get_icon()
     {
-        return 'eaicon-advanced-tabs';
+        return 'eaicon-svg-draw';
     }
 
     public function get_categories()
