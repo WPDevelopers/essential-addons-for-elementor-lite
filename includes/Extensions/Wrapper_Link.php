c6571aaebb882723b42e339508979f63ba9fe3a0
Linkon Miyan <rudlinkon@gmail.com>
2023-04-30 18:01:41 +0600

    added wrapper link controllers

--- includes/Extensions/Wrapper_Link.php
+++ includes/Extensions/Wrapper_Link.php
@@ -2,9 +2,8 @@
 
 namespace Essential_Addons_Elementor\Extensions;
 
-use Elementor\Controls_Stack;
-use Elementor\Plugin;
-use Elementor\Utils;
+use Elementor\Controls_Manager;
+
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
@@ -16,7 +15,33 @@ class Wrapper_Link {
 	 * Initialize hooks
 	 */
 	public function __construct() {
+		add_action( 'elementor/element/common/_section_style/after_section_end', [ $this, 'register_controls' ] );
+		add_action( 'elementor/element/column/section_advanced/after_section_end', [ $this, 'register_controls' ] );
+		add_action( 'elementor/element/section/section_advanced/after_section_end', [ $this, 'register_controls' ] );
+		add_action( 'elementor/element/container/section_layout/after_section_end', [ $this, 'register_controls' ] );
+	}
 
+	public function register_controls( $element ) {
+		$element->start_controls_section(
+			'eael_wrapper_link_section',
+			[
+				'label' => __( '<i class="eaicon-logo"></i> Wrapper Link', 'essential-addons-for-elementor-lite' ),
+				'tab'   => Controls_Manager::TAB_ADVANCED
+			]
+		);
+
+		$element->add_control(
+			'wrapper_link',
+			[
+				'label'   => __( 'Link', 'essential-addons-for-elementor-lite' ),
+				'type'    => Controls_Manager::URL,
+				'dynamic' => [
+					'active' => true,
+				]
+			]
+		);
+
+		$element->end_controls_section();
 	}
 
 }
