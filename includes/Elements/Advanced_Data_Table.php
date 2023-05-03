14205a28f13976da6397c991a745d69d47f954de
Robin <robin@wpdeveloper.com>
2023-05-03 16:08:15 +0600

    recommended filter added

--- includes/Elements/Advanced_Data_Table.php
+++ includes/Elements/Advanced_Data_Table.php
@@ -33,7 +33,7 @@ class Advanced_Data_Table extends Widget_Base
 
     public function get_categories()
     {
-        return ['essential-addons-elementor'];
+        return apply_filters('eael/elements/categories/' . $this->get_name() , ['essential-addons-elementor']);
     }
 
     public function get_keywords()
