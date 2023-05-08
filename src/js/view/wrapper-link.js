12c741b8b38de4bd18dd3934810ca4253f28b945
Linkon Miyan <rudlinkon@gmail.com>
2023-05-08 14:47:05 +0600

    get the wrapper links data

--- src/js/view/wrapper-link.js
+++ src/js/view/wrapper-link.js
@@ -1,1 +1,13 @@
-console.log('wrapper link');
+let EaelWrapperLink = function ($scope, $) {
+    if ($scope.data("eael-wrapper-link") !== undefined) {
+        let wrapperData = $scope.data("eael-wrapper-link"),
+            url = wrapperData.url,
+            target = wrapperData.is_external,
+            nofollow = wrapperData.nofollow;
+
+        console.log('URL:'+url, 'Target:'+target, 'NoFollow:'+nofollow);
+    }
+};
+jQuery(window).on('elementor/frontend/init', function () {
+    elementorFrontend.hooks.addAction('frontend/element_ready/global', EaelWrapperLink);
+});
