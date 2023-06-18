56ba99d2a018595229ce80c6b84bfb4a13dec216
Linkon Miyan <rudlinkon@gmail.com>
2023-06-18 14:18:02 +0600

    fixed strpos null error

--- includes/Elements/Filterable_Gallery.php
+++ includes/Elements/Filterable_Gallery.php
@@ -3227,7 +3227,8 @@ class Filterable_Gallery extends Widget_Base
             $gallery_store[$counter]['link'] = $gallery['eael_fg_gallery_img_link'];
             
             $gallery_store[$counter]['video_gallery_switch'] = $gallery['fg_video_gallery_switch'];
-            
+
+            $gallery['eael_fg_gallery_item_video_link'] = empty( $gallery['eael_fg_gallery_item_video_link'] ) ? '' : $gallery['eael_fg_gallery_item_video_link'];
             if (strpos($gallery['eael_fg_gallery_item_video_link'], 'youtu.be') != false) {
                 preg_match('/(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/\s]{11})/', $gallery['eael_fg_gallery_item_video_link'], $matches);
                 $video_link = !empty($matches) ? sprintf('https://www.youtube.com/watch?v=%s', $matches[1]) : '';
