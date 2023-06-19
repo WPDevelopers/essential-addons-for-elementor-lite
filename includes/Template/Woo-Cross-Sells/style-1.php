7103e891714949539d3d88183649e5ce2523ece7
Linkon Miyan <rudlinkon@gmail.com>
2023-06-19 13:58:43 +0600

    added style 1 markup

--- includes/Template/Woo-Cross-Sells/style-1.php
+++ includes/Template/Woo-Cross-Sells/style-1.php
@@ -8,8 +8,23 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-
-echo $cs_product->get_title();
-echo $cs_product->get_image('medium');
-echo $cs_product->get_price();
-echo '<br>';
+?>
+<div class="eael-cs-single-product">
+    <div class="eael-cs-product-image">
+		<?php echo $cs_product->get_image( 'medium' ); ?>
+    </div>
+    <div class="eael-cs-product-info">
+        <div class="eael-cs-product-title-price">
+            <div class="eael-cs-product-title">
+				<?php echo esc_html( $cs_product->get_title() ); ?>
+            </div>
+            <div class="eael-cs-product-price">
+				<?php echo esc_html( $cs_product->get_price() ); ?>
+            </div>
+        </div>
+        <div class="eael-cs-product-buttons">
+            <button><i class="far fa-eye"></i> <?php esc_html_e( 'View Product', 'essential-addons-for-elementor-lite' ); ?></button>
+            <button><i class="fas fa-shopping-cart"></i><?php esc_html_e( 'Add to Cart', 'essential-addons-for-elementor-lite' ); ?></button>
+        </div>
+    </div>
+</div>
