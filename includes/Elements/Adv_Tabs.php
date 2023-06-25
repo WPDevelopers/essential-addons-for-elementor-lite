fcd64be0d1fb3cd114a2113b51c2a3416df5f361
Robin <robin@wpdeveloper.com>
2023-06-25 11:00:28 +0600

    Fix: Advanced Tabs and Advanced Accordion | Write with AI Improvements

--- includes/Elements/Adv_Tabs.php
+++ includes/Elements/Adv_Tabs.php
@@ -260,6 +260,9 @@ class Adv_Tabs extends Widget_Base
                 'type' => Controls_Manager::TEXT,
                 'default' => esc_html__('Tab Title', 'essential-addons-for-elementor-lite'),
                 'dynamic' => ['active' => true],
+                'ai' => [
+					'active' => false,
+				],
             ]
         );
 
@@ -332,6 +335,9 @@ class Adv_Tabs extends Widget_Base
                 'type' => Controls_Manager::TEXT,
                 'description' => esc_html__( 'Custom ID will be added as an anchor tag. For example, if you add ‘test’ as your custom ID, the link will become like the following: https://www.example.com/#test and it will open the respective tab directly.', 'essential-addons-for-elementor-lite' ),
                 'default' => '',
+                'ai' => [
+					'active' => false,
+				],
             ]
         );
 
