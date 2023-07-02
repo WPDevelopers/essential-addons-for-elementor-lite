bab8b4fc6a5cebfd000339e14feaea03564a05fd
Robin <robin@wpdeveloper.com>
2023-07-02 21:29:54 +0600

    border radius control added

--- includes/Elements/Data_Table.php
+++ includes/Elements/Data_Table.php
@@ -603,14 +603,14 @@ class Data_Table extends Widget_Base {
 					],
 				],
 				'selectors' => [
-					'{{WRAPPER}} .eael-data-table thead tr th:first-child' => 'border-radius: {{SIZE}}px 0px 0px 0px;',
-					'{{WRAPPER}} .eael-data-table thead tr th:last-child' => 'border-radius: 0px {{SIZE}}px 0px 0px;',
+					'{{WRAPPER}} .eael-data-table thead tr th:first-child' 	=> 'border-radius: {{SIZE}}px 0px 0px 0px;',
+					'{{WRAPPER}} .eael-data-table thead tr th:last-child' 	=> 'border-radius: 0px {{SIZE}}px 0px 0px;',
                     '.rtl {{WRAPPER}} .eael-data-table thead tr th:first-child' => 'border-radius: 0px {{SIZE}}px 0px 0px;',
-                    '.rtl {{WRAPPER}} .eael-data-table thead tr th:last-child' => 'border-radius: {{SIZE}}px 0px 0px 0px;',
-					'{{WRAPPER}} .eael-data-table tbody tr:first-child th:first-child' => 'border-radius: {{SIZE}}px 0px 0px 0px;',
-					'{{WRAPPER}} .eael-data-table tbody tr:last-child th:first-child' => 'border-radius: 0px {{SIZE}}px 0px 0px;',
+                    '.rtl {{WRAPPER}} .eael-data-table thead tr th:last-child' 	=> 'border-radius: {{SIZE}}px 0px 0px 0px;',
+					'{{WRAPPER}} .eael-data-table tbody tr:first-child th:first-child' 	=> 'border-radius: {{SIZE}}px 0px 0px 0px;',
+					'{{WRAPPER}} .eael-data-table tbody tr:last-child th:first-child' 	=> 'border-radius: 0px {{SIZE}}px 0px 0px;',
                     '.rtl {{WRAPPER}} .eael-data-table tbody tr:first-child th:first-child' => 'border-radius: 0px {{SIZE}}px 0px 0px;',
-                    '.rtl {{WRAPPER}} .eael-data-table tbody tr:last-child th:first-child' => 'border-radius: {{SIZE}}px 0px 0px 0px;',
+                    '.rtl {{WRAPPER}} .eael-data-table tbody tr:last-child th:first-child' 	=> 'border-radius: {{SIZE}}px 0px 0px 0px;',
 				],
 			]
 		);
