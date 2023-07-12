d67f0921163251a4b4a34e93a53841e8320f1bf3
Linkon Miyan <rudlinkon@gmail.com>
2023-07-12 12:09:13 +0600

    fixed caldera forms deprecations

--- includes/Elements/Caldera_Forms.php
+++ includes/Elements/Caldera_Forms.php
@@ -10,7 +10,7 @@ use \Elementor\Controls_Manager;
 use \Elementor\Group_Control_Border;
 use \Elementor\Group_Control_Box_Shadow;
 use \Elementor\Group_Control_Typography;
-use \Elementor\Core\Schemes\Typography;
+use \Elementor\Core\Kits\Documents\Tabs\Global_Typography;
 use \Elementor\Widget_Base;
 use \Essential_Addons_Elementor\Classes\Helper;
 
@@ -348,7 +348,7 @@ class Caldera_Forms extends Widget_Base
             [
                 'name' => 'form_description_typography',
                 'label' => __('Typography', 'essential-addons-for-elementor-lite'),
-                'scheme' => Typography::TYPOGRAPHY_4,
+                'scheme' => Global_Typography::TYPOGRAPHY_ACCENT,
                 'selector' => '{{WRAPPER}} .eael-contact-form-description',
                 'condition' => [
                     'custom_title_description' => 'yes',
