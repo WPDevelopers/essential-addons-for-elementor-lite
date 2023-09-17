e29e617fd6782d86187a5a37c7d46b9ad7c16f50
Jakaria Istauk <jakariamd35@gmail.com>
2023-09-17 16:39:50 +0600

    add filter for eael_product_wrapper_class

--- includes/Classes/Bootstrap.php
+++ includes/Classes/Bootstrap.php
@@ -236,6 +236,7 @@ class Bootstrap
 		    add_action( 'eael_woo_single_product_summary', 'woocommerce_template_single_meta', 30 );
 
 		    add_filter( 'woocommerce_product_get_rating_html', [ $this, 'eael_rating_markup' ], 10, 3 );
+		    add_filter( 'eael_product_wrapper_class', [ $this, 'eael_product_wrapper_class' ], 10, 3 );
 
             add_action('wp_ajax_eael_checkout_cart_qty_update', [$this, 'eael_checkout_cart_qty_update'] );
     		add_action('wp_ajax_nopriv_eael_checkout_cart_qty_update', [$this, 'eael_checkout_cart_qty_update'] );
