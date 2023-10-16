6b03a717aea77dc8934d4d542e0264c3266a49db
Himadree Shekhar Halder <himadree12@gmail.com>
2023-10-16 13:24:28 +0600

    Add script for editor page

--- includes/Elements/Filterable_Gallery.php
+++ includes/Elements/Filterable_Gallery.php
@@ -3975,6 +3975,20 @@ class Filterable_Gallery extends Widget_Base
 
                                 }
                             }
+                        },
+                        iframe: {
+                            markup: `<div class="mfp-iframe-scaler">
+                                        <div class="mfp-close"></div>
+                                        <iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>
+                                        <div class="mfp-title eale-privacy-message"></div>
+                                    </div>`
+                        },
+                        callbacks: {
+                            markupParse: function(template, values, item) {
+                                if( item.el.attr('title') !== "" ) {
+                                    values.title = item.el.attr('title');
+                                }
+                            }
                         }
                     });
 
