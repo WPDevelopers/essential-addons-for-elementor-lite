249162b1d0df27d65856e01b9e46d241899210b4
Linkon Miyan <rudlinkon@gmail.com>
2023-11-10 21:53:07 +0600

    remove default add to cart button if not default layout

--- includes/Classes/Bootstrap.php
+++ includes/Classes/Bootstrap.php
@@ -252,6 +252,7 @@ class Bootstrap
 			    remove_action( 'woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open' );
 			    remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close' );
 			    remove_action( 'woocommerce_after_shop_loop_item', 'astra_woo_woocommerce_shop_product_content' );
+			    remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart' );
 		    } );
 	    }
 
