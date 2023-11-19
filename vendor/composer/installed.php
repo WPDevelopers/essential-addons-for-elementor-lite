bdc925faccaad8a3a2040efd096cb6dff785cc77
Priyo Mukul <17237437+priyomukul@users.noreply.github.com>
2023-11-19 15:17:25 +0600

    Added: Composer Package

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'mukul/essential-addons-for-elementor-lite',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '6fdcc1aa05d0e5b0bba9800e652b3518d0222277',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'mukul/essential-addons-for-elementor-lite' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '6fdcc1aa05d0e5b0bba9800e652b3518d0222277',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'priyomukul/wp-notice' => array(
+            'pretty_version' => 'v2.x-dev',
+            'version' => '2.9999999.9999999.9999999-dev',
+            'reference' => 'ee9059cfd8471fe62ae68044a2456aec5e71bb61',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../priyomukul/wp-notice',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
