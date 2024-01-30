9ac38c0557faaf9fa982460608935df17a6c5fcd
Linkon Miyan <rudlinkon@gmail.com>
2024-01-30 13:41:58 +0600

    sanitize the url from javascript

--- src/js/view/wrapper-link.js
+++ src/js/view/wrapper-link.js
@@ -1,4 +1,26 @@
 let EaelWrapperLink = function ($scope, $) {
+    function sanitizeURL(url) {
+        try {
+            const urlObject = new URL(url);
+
+            // Check if the protocol is valid (allowing only 'http' and 'https')
+            if (urlObject.protocol !== 'http:' && urlObject.protocol !== 'https:') {
+                throw new Error('Invalid protocol');
+            }
+
+            // Ensure that the host is not empty
+            if (!urlObject.host) {
+                throw new Error('Invalid host');
+            }
+
+            // If all checks pass, return the sanitized URL
+            return urlObject.toString();
+        } catch (error) {
+            console.error('Error sanitizing URL:', error.message);
+            return '#';
+        }
+    }
+
     if ($scope.data("eael-wrapper-link") !== undefined) {
         let wrapperData = $scope.data("eael-wrapper-link"),
             target = wrapperData.is_external === 'on' ? '_blank' : '_self';
@@ -6,7 +28,7 @@ let EaelWrapperLink = function ($scope, $) {
         $scope.on('click', function () {
             let anchor = document.createElement('a');
 
-            anchor.href = wrapperData.url;
+            anchor.href = sanitizeURL(wrapperData.url);
             anchor.target = target;
 
             if (wrapperData.nofollow === 'on') {
