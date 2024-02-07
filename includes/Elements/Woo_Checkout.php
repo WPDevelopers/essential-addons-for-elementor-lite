7631d8840046486ebfc78aaf4de8b4eaa2171242
Himadree Shekhar Halder <himadree12@gmail.com>
2024-02-07 17:06:57 +0600

    Fix returning customer icon position

--- includes/Elements/Woo_Checkout.php
+++ includes/Elements/Woo_Checkout.php
@@ -1452,6 +1452,26 @@ class Woo_Checkout extends Widget_Base {
 				],
 			]
 		);
+
+		$this->add_responsive_control(
+			'ea_woo_checkout_login_icon_position',
+			[
+				'label'              => esc_html__( 'Icon Position', 'essential-addons-for-elementor-lite' ),
+				'type'               => Controls_Manager::DIMENSIONS,
+				'size_units'         => [ 'px', 'em', '%' ],
+				'allowed_dimensions' => [ 'top', 'left' ],
+				'default' 			 => [
+					'top'      => 56,
+					'left'     => 35,
+					'unit'     => 'px',
+					'isLinked' =>  true,
+				],
+				'selectors'          => [
+					'{{WRAPPER}} .ea-woo-checkout .ea-login-icon' => 'top: {{TOP}}{{UNIT}}; left: {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+
 		$this->add_control(
 			'ea_woo_checkout_login_links_color',
 			[
@@ -1490,7 +1510,6 @@ class Woo_Checkout extends Widget_Base {
 				'size_units' => [ 'px', 'em', '%' ],
 				'selectors' => [
 					'{{WRAPPER}} .woo-checkout-login' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
-					'{{WRAPPER}} .ea-woo-checkout .ea-login-icon' => 'top: {{TOP}}{{UNIT}}; left: {{LEFT}}{{UNIT}};',
 				],
 			]
 		);
@@ -1823,7 +1842,7 @@ class Woo_Checkout extends Widget_Base {
 					'top'      => 56,
 					'left'     => 35,
 					'unit'     => 'px',
-					'isLinked' =>  false,
+					'isLinked' =>  true,
 				],
 				'selectors'          => [
 					'{{WRAPPER}} .ea-woo-checkout .ea-coupon-icon' => 'top: {{TOP}}{{UNIT}}; left: {{LEFT}}{{UNIT}};',
