58ce0404dcb8cb639005cf7deabb23278f8efddb
Linkon Miyan <rudlinkon@gmail.com>
2024-02-19 11:08:48 +0600

    url and attribute escaped for all other places

--- includes/Template/Betterdocs-Category-Grid/Layout_2.php
+++ includes/Template/Betterdocs-Category-Grid/Layout_2.php
@@ -143,7 +143,7 @@ echo '<article class="eael-better-docs-category-grid-post layout-2" data-id="' .
                     $button_link = get_term_link($term->slug, 'doc_category');
                 }
 
-                echo '<a class="eael-bd-cg-button" href="' . $button_link . '">';
+                echo '<a class="eael-bd-cg-button" href="' . esc_url( $button_link ) . '">';
 
                 if ($settings['icon_position'] === 'before') {
                     if (isset($settings['button_icon']['value']['url']) && !empty($settings['button_icon']['value']['url'])) {
