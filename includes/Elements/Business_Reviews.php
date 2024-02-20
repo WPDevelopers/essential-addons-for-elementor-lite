e33abc77868b727587f959908139129eb7e5c291
Jakaria Istauk <jakariamd35@gmail.com>
2024-02-20 12:44:44 +0600

    Business Reviews: accessibilty controll added

--- includes/Elements/Business_Reviews.php
+++ includes/Elements/Business_Reviews.php
@@ -753,6 +753,46 @@ class Business_Reviews extends Widget_Base {
 		$this->end_controls_section();
 
 		/**
+		 * Accessibilty Controller
+		 */
+		$this->start_controls_section(
+			'eael_section_business_reviews_accessibilty',
+			[
+				'label' => esc_html__( 'Accessibilty', 'essential-addons-for-elementor-lite' ),
+			]
+		);
+
+		$this->add_control(
+			'eael_business_reviews_enable_accessibilty',
+			[
+				'label'        => __( 'Enable Accessibilty', 'essential-addons-for-elementor-lite' ),
+				'type'         => Controls_Manager::SWITCHER,
+				'label_on'     => __( 'Yes', 'essential-addons-for-elementor-lite' ),
+				'label_off'    => __( 'No', 'essential-addons-for-elementor-lite' ),
+				'return_value' => 'yes',
+				'default'      => 'no',
+			]
+		);
+
+		$this->add_control(
+			'eael_business_reviews_link_in_same_tab',
+			[
+				'label'        => __( 'Open in same window', 'essential-addons-for-elementor-lite' ),
+				'type'         => Controls_Manager::SWITCHER,
+				'label_on'     => __( 'Yes', 'essential-addons-for-elementor-lite' ),
+				'label_off'    => __( 'No', 'essential-addons-for-elementor-lite' ),
+				'return_value' => 'yes',
+				'default'      => 'no',
+				'description'  => __( 'Recommended to open link in same tab instead on a new tab', 'essential-addons-for-elementor-lite' ),
+				'condition'    => [
+					'eael_business_reviews_enable_accessibilty' => 'yes'
+				]
+			]
+		);
+
+		$this->end_controls_section();
+
+		/**
 		 * -------------------------------------------
 		 * Tab Style (Container Style)
 		 * -------------------------------------------
@@ -2542,6 +2582,8 @@ class Business_Reviews extends Widget_Base {
 			$business_reviews['columns'] 					= 3;
 		}
 
+		$business_reviews['link_in_same_tab'] = $settings['eael_business_reviews_enable_accessibilty'] === 'yes' && $settings['eael_business_reviews_link_in_same_tab'] === 'yes';
+
 		return $business_reviews;
 	}
 
@@ -2834,7 +2876,7 @@ class Business_Reviews extends Widget_Base {
 							<?php if ( $business_reviews['business_name'] ): ?>
                                 <div class="eael-google-reviews-business-name">
 									<?php $business_reviews['business_name_label'] = $business_reviews['business_name_label'] ? $business_reviews['business_name_label'] : $google_reviews_data['name']; ?>
-                                    <a href="<?php echo esc_url( $google_reviews_data['website'] ); ?>" target="_blank"><?php echo esc_html( $business_reviews['business_name_label'] ); ?></a>
+                                    <a href="<?php echo esc_url( $google_reviews_data['website'] ); ?>" <?php echo !$business_reviews['link_in_same_tab'] ? 'target="_blank"' : ''; ?>><?php echo esc_html( $business_reviews['business_name_label'] ); ?></a>
                                 </div>
 							<?php endif; ?>
 
@@ -2842,7 +2884,7 @@ class Business_Reviews extends Widget_Base {
                                 <div class="eael-google-reviews-business-rating">
                                     <p><?php echo esc_html( $google_reviews_data['rating'] ); ?></p>
                                     <p><?php $this->print_business_reviews_ratings( $google_reviews_data['rating'] ); ?></p>
-                                    <p><a href="<?php echo esc_url( $google_reviews_data['url'] ); ?>" target="_blank"><?php echo esc_html( number_format( $google_reviews_data['user_ratings_total'] ) . ' ' . $business_reviews['google_reviews_label'] ); ?></a></p>
+                                    <p><a href="<?php echo esc_url( $google_reviews_data['url'] ); ?>" <?php echo !$business_reviews['link_in_same_tab'] ? 'target="_blank"' : ''; ?>><?php echo esc_html( number_format( $google_reviews_data['user_ratings_total'] ) . ' ' . $business_reviews['google_reviews_label'] ); ?></a></p>
                                 </div>
 							<?php endif; ?>
 
@@ -2921,13 +2963,13 @@ class Business_Reviews extends Widget_Base {
 	public function print_google_reviews_slider_preset_1( $business_reviews, $single_review_data ) {
 		if ( $business_reviews['reviewer_photo'] ): ?>
             <div class="eael-google-review-reviewer-photo">
-                <img src="<?php echo esc_url_raw( $single_review_data['profile_photo_url'] ); ?>" alt="">
+                <img src="<?php echo esc_url_raw( $single_review_data['profile_photo_url'] ); ?>" alt="<?php echo  $single_review_data['author_name'] ? esc_attr(  $single_review_data['author_name'] ) : esc_html__( 'Reviewer', 'essential-addons-for-elementor-lite' ); ?>">
             </div>
 		<?php endif;
 
 		if ( $business_reviews['reviewer_name'] ): ?>
             <div class="eael-google-review-reviewer-name">
-                <a href="<?php echo ! empty ( $single_review_data['author_url'] ) ? esc_url_raw( $single_review_data['author_url'] ) : '#'; ?>" target="_blank"><?php echo esc_html( $single_review_data['author_name'] ); ?></a>
+                <a href="<?php echo ! empty ( $single_review_data['author_url'] ) ? esc_url_raw( $single_review_data['author_url'] ) : '#'; ?>" <?php echo !$business_reviews['link_in_same_tab'] ? 'target="_blank"' : ''; ?>><?php echo esc_html( $single_review_data['author_name'] ); ?></a>
             </div>
 		<?php endif;
 
@@ -2965,7 +3007,7 @@ class Business_Reviews extends Widget_Base {
                 <div class="preset-content-footer-photo">
 					<?php if ( $business_reviews['reviewer_photo'] ): ?>
                         <div class="eael-google-review-reviewer-photo">
-                            <img src="<?php echo esc_url_raw( $single_review_data['profile_photo_url'] ); ?>" alt="">
+                            <img src="<?php echo esc_url_raw( $single_review_data['profile_photo_url'] ); ?>" alt="<?php echo  $single_review_data['author_name'] ? esc_attr(  $single_review_data['author_name'] ) : esc_html__( 'Reviewer', 'essential-addons-for-elementor-lite' ); ?>">
                         </div>
 					<?php endif; ?>
                 </div>
@@ -2973,7 +3015,7 @@ class Business_Reviews extends Widget_Base {
                 <div class="preset-content-footer-reviewer-name">
 					<?php if ( $business_reviews['reviewer_name'] ): ?>
                         <div class="eael-google-review-reviewer-name">
-                            <a href="<?php echo ! empty ( $single_review_data['author_url'] ) ? esc_url_raw( $single_review_data['author_url'] ) : '#'; ?>" target="_blank"><?php echo esc_html( $single_review_data['author_name'] ); ?></a>
+                            <a href="<?php echo ! empty ( $single_review_data['author_url'] ) ? esc_url_raw( $single_review_data['author_url'] ) : '#'; ?>" <?php echo !$business_reviews['link_in_same_tab'] ? 'target="_blank"' : ''; ?>><?php echo esc_html( $single_review_data['author_name'] ); ?></a>
                         </div>
 					<?php endif;
 
@@ -3036,7 +3078,7 @@ class Business_Reviews extends Widget_Base {
             <div>
 				<?php if ( $business_reviews['reviewer_photo'] ): ?>
                     <div class="eael-google-review-reviewer-photo">
-                        <img src="<?php echo esc_url_raw( $single_review_data['profile_photo_url'] ); ?>" alt="">
+                        <img src="<?php echo esc_url_raw( $single_review_data['profile_photo_url'] ); ?>" alt="<?php echo $single_review_data['author_name'] ? esc_attr(  $single_review_data['author_name'] ) : esc_html__( 'Reviewer', 'essential-addons-for-elementor-lite' ); ?>">
                     </div>
 				<?php endif; ?>
             </div>
@@ -3044,7 +3086,7 @@ class Business_Reviews extends Widget_Base {
             <div>
 				<?php if ( $business_reviews['reviewer_name'] ): ?>
                     <div class="eael-google-review-reviewer-name">
-                        <a href="<?php echo ! empty ( $single_review_data['author_url'] ) ? esc_url_raw( $single_review_data['author_url'] ) : '#'; ?>" target="_blank"><?php echo esc_html( $single_review_data['author_name'] ); ?></a>
+                        <a href="<?php echo ! empty ( $single_review_data['author_url'] ) ? esc_url_raw( $single_review_data['author_url'] ) : '#'; ?>" <?php echo !$business_reviews['link_in_same_tab'] ? 'target="_blank"' : ''; ?>><?php echo esc_html( $single_review_data['author_name'] ); ?></a>
                     </div>
 				<?php endif;
 
@@ -3112,7 +3154,7 @@ class Business_Reviews extends Widget_Base {
 							<?php if ( $business_reviews['business_name'] ): ?>
                                 <div class="eael-google-reviews-business-name">
 									<?php $business_reviews['business_name_label'] = $business_reviews['business_name_label'] ? $business_reviews['business_name_label'] : $google_reviews_data['name']; ?>
-                                    <a href="<?php echo esc_url( $google_reviews_data['website'] ); ?>" target="_blank"><?php echo esc_html( $business_reviews['business_name_label'] ); ?></a>
+                                    <a href="<?php echo esc_url( $google_reviews_data['website'] ); ?>" <?php echo !$business_reviews['link_in_same_tab'] ? 'target="_blank"' : ''; ?>><?php echo esc_html( $business_reviews['business_name_label'] ); ?></a>
                                 </div>
 							<?php endif; ?>
 
@@ -3120,7 +3162,7 @@ class Business_Reviews extends Widget_Base {
                                 <div class="eael-google-reviews-business-rating">
                                     <p><?php echo esc_html( $google_reviews_data['rating'] ); ?></p>
                                     <p><?php $this->print_business_reviews_ratings( $google_reviews_data['rating'] ); ?></p>
-                                    <p><a href="<?php echo esc_url( $google_reviews_data['url'] ); ?>" target="_blank"><?php echo esc_html( number_format( $google_reviews_data['user_ratings_total'] ) . ' ' . $business_reviews['google_reviews_label'] ); ?></a></p>
+                                    <p><a href="<?php echo esc_url( $google_reviews_data['url'] ); ?>" <?php echo !$business_reviews['link_in_same_tab'] ? 'target="_blank"' : ''; ?>><?php echo esc_html( number_format( $google_reviews_data['user_ratings_total'] ) . ' ' . $business_reviews['google_reviews_label'] ); ?></a></p>
                                 </div>
 							<?php endif; ?>
 
