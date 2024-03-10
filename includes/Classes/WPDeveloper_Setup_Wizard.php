efe3111cf98de6dab33e7bd7a32d82260343128a
Linkon Miyan <rudlinkon@gmail.com>
2024-03-10 16:00:16 +0600

    update content and icon

--- includes/Classes/WPDeveloper_Setup_Wizard.php
+++ includes/Classes/WPDeveloper_Setup_Wizard.php
@@ -330,11 +330,12 @@ class WPDeveloper_Setup_Wizard {
 				<?php printf( __( '<span class="eael-quick-setup-highlighted-red">%s</span> %s', 'essential-addons-for-elementor-lite' ), '5000+', 'Ready Templates' ); ?>
 			</div>
 			<div class="eael-quick-setup-text">
-				<?php _e( 'Get Access to amazing features and boost your Elementor page building experience with Templately', 'essential-addons-for-elementor-lite' ); ?>
+				<?php _e( 'Unlock an extensive collection of ready WordPress templates, along with full site import & cloud collaboration features', 'essential-addons-for-elementor-lite' ); ?>
 			</div>
 			<ul class="eael-quick-setup-list">
 				<li class="eael-quick-setup-list-item">
-                    <span class="eael-quick-setup-icon"><svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <span class="eael-quick-setup-icon">
+						<svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M0.700012 1.60117C0.700012 1.36248 0.794833 1.13356 0.963616 0.964776C1.1324 0.795993 1.36132 0.701172 1.60001 0.701172H12.4C12.6387 0.701172 12.8676 0.795993 13.0364 0.964776C13.2052 1.13356 13.3 1.36248 13.3 1.60117V3.40117C13.3 3.63987 13.2052 3.86878 13.0364 4.03757C12.8676 4.20635 12.6387 4.30117 12.4 4.30117H1.60001C1.36132 4.30117 1.1324 4.20635 0.963616 4.03757C0.794833 3.86878 0.700012 3.63987 0.700012 3.40117V1.60117ZM0.700012 7.00117C0.700012 6.76248 0.794833 6.53356 0.963616 6.36478C1.1324 6.19599 1.36132 6.10117 1.60001 6.10117H7.00001C7.23871 6.10117 7.46763 6.19599 7.63641 6.36478C7.80519 6.53356 7.90001 6.76248 7.90001 7.00117V12.4012C7.90001 12.6399 7.80519 12.8688 7.63641 13.0376C7.46763 13.2064 7.23871 13.3012 7.00001 13.3012H1.60001C1.36132 13.3012 1.1324 13.2064 0.963616 13.0376C0.794833 12.8688 0.700012 12.6399 0.700012 12.4012V7.00117ZM10.6 6.10117C10.3613 6.10117 10.1324 6.19599 9.96362 6.36478C9.79483 6.53356 9.70001 6.76248 9.70001 7.00117V12.4012C9.70001 12.6399 9.79483 12.8688 9.96362 13.0376C10.1324 13.2064 10.3613 13.3012 10.6 13.3012H12.4C12.6387 13.3012 12.8676 13.2064 13.0364 13.0376C13.2052 12.8688 13.3 12.6399 13.3 12.4012V7.00117C13.3 6.76248 13.2052 6.53356 13.0364 6.36478C12.8676 6.19599 12.6387 6.10117 12.4 6.10117H10.6Z"
 	  fill="url(#paint0_linear_810_832)"/>
 <defs>
@@ -349,18 +350,17 @@ class WPDeveloper_Setup_Wizard {
 				</li>
 				<li class="eael-quick-setup-list-item">
                     <span class="eael-quick-setup-icon">
-						<svg width="14" height="15" viewBox="0 0 14 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M13.75 5.125V7.375C13.75 9.23898 10.728 10.75 7 10.75C3.27208 10.75 0.25 9.23898 0.25 7.375V5.125C0.25 6.98898 3.27208 8.5 7 8.5C10.728 8.5 13.75 6.98898 13.75 5.125ZM0.25 8.875C0.25 10.739 3.27208 12.25 7 12.25C10.728 12.25 13.75 10.739 13.75 8.875V11.125C13.75 12.989 10.728 14.5 7 14.5C3.27208 14.5 0.25 12.989 0.25 11.125V8.875ZM7 7C3.27208 7 0.25 5.48896 0.25 3.625C0.25 1.76104 3.27208 0.25 7 0.25C10.728 0.25 13.75 1.76104 13.75 3.625C13.75 5.48896 10.728 7 7 7Z"
-	  fill="url(#paint0_linear_810_852)"/>
+						<svg width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path fill-rule="evenodd" clip-rule="evenodd" d="M3 0.75C1.75736 0.75 0.75 1.75736 0.75 3V14.25C0.75 15.4927 1.75736 16.5 3 16.5H15C16.2427 16.5 17.25 15.4927 17.25 14.25V5.25C17.25 4.00736 16.2427 3 15 3H9L7.40901 1.40901C6.98705 0.987053 6.41476 0.75 5.81802 0.75H3ZM9 5.25C9.41422 5.25 9.75 5.58578 9.75 6V10.9394L10.7197 9.96968C11.0126 9.6768 11.4874 9.6768 11.7803 9.96968C12.0732 10.2626 12.0732 10.7374 11.7803 11.0303L9.53032 13.2803C9.23745 13.5732 8.76255 13.5732 8.46968 13.2803L6.21967 11.0303C5.92678 10.7374 5.92678 10.2626 6.21967 9.96968C6.51256 9.6768 6.98744 9.6768 7.28033 9.96968L8.25 10.9394V6C8.25 5.58578 8.58577 5.25 9 5.25Z" fill="url(#paint0_linear_922_1148)"/>
 <defs>
-<linearGradient id="paint0_linear_810_852" x1="7" y1="0.25" x2="7" y2="14.5" gradientUnits="userSpaceOnUse">
-<stop stop-color="#FFB45A"/>
-<stop offset="1" stop-color="#F3A342"/>
+<linearGradient id="paint0_linear_922_1148" x1="9" y1="0.75" x2="9" y2="16.5" gradientUnits="userSpaceOnUse">
+<stop stop-color="#FFCD91"/>
+<stop offset="1" stop-color="#FAAD50"/>
 </linearGradient>
 </defs>
 </svg>
 					</span>
-					<?php _e( 'Design With MyCloud Storage Space', 'essential-addons-for-elementor-lite' ); ?>
+					<?php _e( 'One-Click Full Site Import', 'essential-addons-for-elementor-lite' ); ?>
 				</li>
 				<li class="eael-quick-setup-list-item">
                     <span class="eael-quick-setup-icon">
@@ -390,7 +390,7 @@ class WPDeveloper_Setup_Wizard {
 </defs>
 </svg>
 					</span>
-					<?php _e( 'Cloud With Templately WorkSpace', 'essential-addons-for-elementor-lite' ); ?>
+					<?php _e( 'Templates Cloud with Workspace', 'essential-addons-for-elementor-lite' ); ?>
 				</li>
 			</ul>
 			</div><img src="<?php echo esc_url( EAEL_PLUGIN_URL . 'assets/admin/images/quick-setup/templately-qs-img.png' )?>" alt="">
