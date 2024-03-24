de10a43ee6a92d7575a74ca013b14902d4b176ef
Linkon Miyan <rudlinkon@gmail.com>
2024-03-24 13:12:37 +0600

    move sanitizeURL function from wrapper link to general

--- src/js/view/general.js
+++ src/js/view/general.js
@@ -122,6 +122,26 @@ jQuery(window).on("elementor/frontend/init", function () {
 			});
 		}
 	}
+	ea.sanitizeURL = function (url) {
+		if (url.startsWith('/') || url.startsWith('#')) {
+			return url;
+		}
+
+		try {
+			const urlObject = new URL(url);
+
+			// Check if the protocol is valid (allowing only 'http' and 'https')
+			if (!['http:', 'https:', 'ftp:', 'ftps:', 'mailto:', 'news:', 'irc:', 'irc6:', 'ircs:', 'gopher:', 'nntp:', 'feed:', 'telnet:', 'mms:', 'rtsp:', 'sms:', 'svn:', 'tel:', 'fax:', 'xmpp:', 'webcal:', 'urn:'].includes(urlObject.protocol)) {
+				throw new Error('Invalid protocol');
+			}
+
+			// If all checks pass, return the sanitized URL
+			return urlObject.toString();
+		} catch (error) {
+			console.error('Error sanitizing URL:', error.message);
+			return '#';
+		}
+	}
 
 	//Add hashchange code form advanced-accordion
 	let  isTriggerOnHashchange = true;
