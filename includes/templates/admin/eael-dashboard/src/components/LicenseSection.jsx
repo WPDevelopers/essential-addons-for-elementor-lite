7921b520bf51b8ad1ca7a10f1e2c6effc6a2412c
Linkon Miyan <rudlinkon@gmail.com>
2024-05-28 13:46:04 +0600

    simplify the LicenseSection component with divided sub components

--- includes/templates/admin/eael-dashboard/src/components/LicenseSection.jsx
+++ includes/templates/admin/eael-dashboard/src/components/LicenseSection.jsx
@@ -2,6 +2,8 @@ import consumer from "../context";
 import LicenseSteps from "./LicenseSteps.jsx";
 import LicenseForm from "./LicenseForm.jsx";
 import LicenseOtpForm from "./LicenseOtpForm.jsx";
+import LicenseUnlockBox from "./LicenseUnlockBox.jsx";
+import LicenseActivatedBox from "./LicenseActivatedBox.jsx";
 
 function LicenseSection() {
     const licenseData = typeof wpdeveloperLicenseData === 'undefined' ? {} : wpdeveloperLicenseData,
@@ -14,26 +16,7 @@ function LicenseSection() {
     return (
         <>
             <div className="ea__general-content-item relative">
-                {licenseData?.license_status !== 'valid' ? <div className="ea__unlock-license flex gap-4">
-                    <div className="ea__others-icon eaicon-1">
-                        <i className="ea-dash-icon ea-lock"></i>
-                    </div>
-                    <div className="max-w-454">
-                        <h4>Unlock With Your License Key</h4>
-                        <p>Enter your license key here, to activate BetterDocs Pro, and get automatic updates
-                            and premium support.</p>
-                    </div>
-                </div> : <div className="ea__active-license flex gap-4">
-                    <div className="ea__others-icon eaicon-active">
-                        <i className="ea-dash-icon ea-lock"></i>
-                    </div>
-                    <div className="max-w-454">
-                        <h4>Enjoy the pro features & Supports!</h4>
-                        <p>You have already activated Essential Blocks Pro. You will able to update the plugin
-                            right from your WP dashboard.</p>
-                        <span className="activated-btn"><i className="ea-dash-icon  ea-check"></i>Activated</span>
-                    </div>
-                </div>}
+                {licenseData?.license_status !== 'valid' ? <LicenseUnlockBox/> : <LicenseActivatedBox/>}
                 <div className="ea__license-wrapper">
                     {licenseData?.license_status !== 'valid' &&
                         <div className="ea__license-content" onClick={clickHandler}>
@@ -41,21 +24,22 @@ function LicenseSection() {
                                 How to get license key?
                             </h5>
                             <i className="ea-dash-icon ea-dropdown"></i>
-                        </div>}
+                        </div>
+                    }
+
                     {licenseData?.license_status === 'valid' &&
                         <div className="ea__license-options-wrapper">
                             <LicenseForm/>
                         </div>
                     }
-                    {(licenseData?.license_status !== 'valid' && isOpenForm) && <>
+
+                    {(licenseData?.license_status !== 'valid' && isOpenForm) &&
                         <div className="ea__license-options-wrapper">
                             <LicenseSteps/>
                             <LicenseForm/>
                             <LicenseOtpForm/>
                         </div>
-                    </>
                     }
-
                 </div>
             </div>
         </>
