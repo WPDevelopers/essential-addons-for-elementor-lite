5c4d830b00f267434c26e3a3d9d4d8ddbee8feb3
Linkon Miyan <rudlinkon@gmail.com>
2024-05-29 16:23:47 +0600

    dynamic email address for otp verification

--- includes/templates/admin/eael-dashboard/src/components/LicenseOtpForm.jsx
+++ includes/templates/admin/eael-dashboard/src/components/LicenseOtpForm.jsx
@@ -1,11 +1,13 @@
+import consumer from "../context/index.js";
+
 function LicenseOtpForm() {
+    const {eaState, eaDispatch} = consumer();
+
     return (
         <>
             <div className="ea__license-verify">
-                <p>Licence Verification Code has been sent to this
-                    <span>emo***@wpdeveloper.com</span> mail. Please check your
-                    email. copy the code
-                    and insert it bellow:
+                <p>Licence Verification Code has been sent to this <span>{eaState.otpEmail}</span> mail. Please check
+                    your email. copy the code and insert it bellow:
                 </p>
                 <div className="license-key-items flex items-center">
                     <input className="input-api" type="text"
@@ -18,12 +20,12 @@ function LicenseOtpForm() {
                     <span className="resend-text">resend button</span>
                     your email. copy the code and insert it meanutes.
                     <i className="ea-dash-icon  ea-info">
-                                            <span className="tooltip-api">
-                                                Check out this <span className="color-ex">guide</span> to verify your
-                                                license key. If you need any
-                                                assistance with retrieving your License Verification Key, please <span
-                                                className="color-ex">contact support.</span>
-                                            </span>
+                    <span className="tooltip-api">
+                        Check out this <span className="color-ex">guide</span> to verify your
+                        license key. If you need any
+                        assistance with retrieving your License Verification Key, please <span
+                        className="color-ex">contact support.</span>
+                    </span>
                     </i>
                 </p>
             </div>
