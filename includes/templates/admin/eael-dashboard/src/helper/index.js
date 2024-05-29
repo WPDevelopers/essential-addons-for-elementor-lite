8a05cf2af6c1494ec92e6bb11eb375f7b225b9db
Linkon Miyan <rudlinkon@gmail.com>
2024-05-29 15:36:38 +0600

    Added eaAjax helper function to handle the Ajax request

--- includes/templates/admin/eael-dashboard/src/helper/index.js
+++ includes/templates/admin/eael-dashboard/src/helper/index.js
@@ -1,11 +1,31 @@
 const debouncer = (callback, delay) => {
-    let timer
-    return function () {
-        clearTimeout(timer)
-        timer = setTimeout(() => {
-            callback();
-        }, delay)
-    }
-}
+        let timer
+        return function () {
+            clearTimeout(timer)
+            timer = setTimeout(() => {
+                callback();
+            }, delay)
+        }
+    },
+    encodeURI = (obj) => {
+        let result = '',
+            splitter = '';
+
+        if (typeof obj === 'object') {
+            Object.keys(obj).forEach(function (key) {
+                result += splitter + key + '=' + encodeURIComponent(obj[key]);
+                splitter = '&';
+            });
+        }
+        return result;
+    },
+    eaXMLHttpRequest = (params) => {
+        const request = new XMLHttpRequest();
+        request.open('POST', localize.ajaxurl, false);
+        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+        request.send(encodeURI(params));
+        return JSON.parse(request.responseText);
+    };
 
 export const debounce = debouncer;
+export const eaAjax = eaXMLHttpRequest;
