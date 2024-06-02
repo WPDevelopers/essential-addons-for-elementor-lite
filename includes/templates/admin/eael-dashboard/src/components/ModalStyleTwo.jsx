796bd089d3a56bfaf6d273309eb112af9b50328a
Linkon Miyan <rudlinkon@gmail.com>
2024-06-02 17:36:01 +0600

    dynamic ModalStyleTwo component

--- includes/templates/admin/eael-dashboard/src/components/ModalStyleTwo.jsx
+++ includes/templates/admin/eael-dashboard/src/components/ModalStyleTwo.jsx
@@ -9,13 +9,10 @@ function ModalStyleTwo() {
             <h4 className="mb-4">{eaData.title}</h4>
             <div className="select-option-wrapper">
                 <select name={eaData.name} id="select-option" className="form-select">
-                    <option value="">Select team member</option>
-                    <option value="1">All</option>
-                    <option value="2">Post</option>
-                    <option value="3">Page</option>
-                    <option value="2">e-landing-page</option>
-                    <option value="3">Products</option>
-                    <option value="3">Docs</option>
+                    <option value="all">All</option>
+                    {Object.keys(eaData.options).map((item, index) => {
+                        return <option value={item} key={index}>{eaData.options[item]}</option>;
+                    })}
                 </select>
             </div>
         </>
