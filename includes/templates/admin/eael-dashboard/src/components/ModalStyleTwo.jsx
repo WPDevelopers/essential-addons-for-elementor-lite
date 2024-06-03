6eede51548a971e1daa89cdb54f3a656b6a9f78a
Linkon Miyan <rudlinkon@gmail.com>
2024-06-04 03:57:33 +0600

    selected the item from db

--- includes/templates/admin/eael-dashboard/src/components/ModalStyleTwo.jsx
+++ includes/templates/admin/eael-dashboard/src/components/ModalStyleTwo.jsx
@@ -2,7 +2,8 @@ import consumer from "../context/index.js";
 
 function ModalStyleTwo() {
     const {eaState} = consumer(),
-        eaData = localize.eael_dashboard.modal[eaState.modalID];
+        eaData = localize.eael_dashboard.modal[eaState.modalID],
+        selectedVal = eaState.modals[eaData.name];
 
     return (
         <>
@@ -11,7 +12,8 @@ function ModalStyleTwo() {
                 <select name={eaData.name} id="select-option" className="form-select">
                     <option value="all">All</option>
                     {Object.keys(eaData.options).map((item, index) => {
-                        return <option value={item} key={index}>{eaData.options[item]}</option>;
+                        return <option value={item} key={index}
+                                       selected={selectedVal === item}>{eaData.options[item]}</option>;
                     })}
                 </select>
             </div>
