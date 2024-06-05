06b4b19a761f894c5f9b030698dd936a21d71b14
Robin <86726198+robinwpdeveloper@users.noreply.github.com>
2024-06-05 15:25:02 +0600

    React init

---
+++ assets/admin/js/quick-setup.js
@@ -0,0 +1,16 @@
+export default function QuickSetup(){
+	return (
+		<div>
+			<h1>Welcome to my app</h1>
+      		<MyButton />
+		</div>
+	);
+}
+
+function MyButton(){
+	return (
+		<button>
+			I'm a button
+		</button>
+	);
+}
