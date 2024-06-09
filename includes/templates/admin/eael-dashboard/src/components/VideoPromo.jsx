daee8c8ac745fa6b2c95dee612c5382192c170ea
Kazi Sourav <32020708+khmsourav@users.noreply.github.com>
2024-06-09 17:10:42 +0600

    video-Promo UI related work

--- includes/templates/admin/eael-dashboard/src/components/VideoPromo.jsx
+++ includes/templates/admin/eael-dashboard/src/components/VideoPromo.jsx
@@ -1,7 +1,26 @@
+import React from "react";
+
 function VideoPromo(props) {
     return (
         <>
-            <div>Video Promo</div>
+            <div className="ea__general-content-item video-promo">
+                <div className='video-promo-wrapper flex justify-between items-center gap-4'>
+                    <div className="templates-content">
+                        <h2>Get Started with Essential Addons for Elementor</h2>
+                        <p className='mb-6'>Get ready to enhance your Elementor site building experience by using 90+ Essential Addons elements & extensions.</p>
+                        <a href="#">
+                            <button className="primary-btn install-btn">
+                                YouTube Tutorials
+                            </button>
+                        </a>
+                    </div>
+                    <div className="templates-img">
+                        <a href="https://www.youtube.com/watch?v=ZISSbnHo0rE" target="_blank">
+                            <img src="../../public/images/video-promo.png" alt="video promo"/>
+                        </a>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
