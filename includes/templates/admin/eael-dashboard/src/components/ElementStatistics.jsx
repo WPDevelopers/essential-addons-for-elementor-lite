24284e4447c11a4fddd43e2d1d1ee8f08e66b849
Kazi Sourav <32020708+khmsourav@users.noreply.github.com>
2024-06-09 14:49:52 +0600

    Elements related markup done

--- includes/templates/admin/eael-dashboard/src/components/ElementStatistics.jsx
+++ includes/templates/admin/eael-dashboard/src/components/ElementStatistics.jsx
@@ -1,8 +1,22 @@
 function ElementStatistics(props) {
     return (
         <>
-            <div>
-                Statistics
+            <div className="ea__connect-others">
+                <div className="ea__elements-wrapper elements-1">
+                    <i className="ea-elements ea-dash-icon"></i>
+                    <h4>57</h4>
+                    <span>Total Elements</span>
+                </div>
+                <div className="ea__elements-wrapper elements-2">
+                    <i className="ea-active ea-dash-icon"></i>
+                    <h4>28</h4>
+                    <span>Active</span>
+                </div>
+                <div className="ea__elements-wrapper elements-3">
+                    <i className="ea-inactive ea-dash-icon"></i>
+                    <h4>21</h4>
+                    <span>Inactive</span>
+                </div>
             </div>
         </>
     );
