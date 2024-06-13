769de48ae8e86bfe9c294c2509f3f2024adf3b9b
Kazi Sourav <32020708+khmsourav@users.noreply.github.com>
2024-06-13 15:07:26 +0600

    toaster animation added

--- includes/templates/admin/eael-dashboard/src/components/Toasts.jsx
+++ includes/templates/admin/eael-dashboard/src/components/Toasts.jsx
@@ -10,20 +10,6 @@ function Toasts() {
                         </div>
                         <i className='ea-dash-icon ea-close'></i>
                     </div>
-                    <div className='flex items-center justify-between gap-2 flex-1'>
-                        <div className='flex gap-2 items-center'>
-                            <img src="../../public/images/warning.svg" alt="logo icon"/>
-                            <h5>This is warning message</h5>
-                        </div>
-                        <i className='ea-dash-icon ea-close'></i>
-                    </div>
-                    <div className='flex items-center justify-between gap-2 flex-1'>
-                        <div className='flex gap-2 items-center'>
-                            <img src="../../public/images/error.svg" alt="logo icon"/>
-                            <h5>This is error message</h5>
-                        </div>
-                        <i className='ea-dash-icon ea-close'></i>
-                    </div>
                 </div>
             </div>
         </>
