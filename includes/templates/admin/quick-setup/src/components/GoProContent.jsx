0a8d946792bcab7f691e13be40292dcac808a797
Kazi Sourav <32020708+khmsourav@users.noreply.github.com>
2024-06-25 14:16:31 +0600

    GoPro UI updated

--- includes/templates/admin/quick-setup/src/components/GoProContent.jsx
+++ includes/templates/admin/quick-setup/src/components/GoProContent.jsx
@@ -1,6 +1,6 @@
 import { __ } from "@wordpress/i18n";
 
-function GoProContent({activeTab, handleTabChange}) {
+function GoProContent({ activeTab, handleTabChange }) {
   let eaelQuickSetup = localize?.eael_quick_setup_data;
   let go_pro_content = eaelQuickSetup?.go_pro_content;
   let feature_items = go_pro_content?.feature_items;
@@ -55,30 +55,27 @@ function GoProContent({activeTab, handleTabChange}) {
                 "Discover the premium features of the most popular elements library for Elementor. Experience the web building experience with:",
                 "essential-addons-for-elementor-lite"
               )}
-            </p>            
-            <ul className="mb-7">
-              <li>
-                -
-                {__(
-                "Greater Customization Flexibility in Design with a Collection of Premium Creative Elements.",
-                "essential-addons-for-elementor-lite"
-                )}
-              </li>
-              <li>
-                -
-                {__(
-                "More Advanced WooCommerce Widgets like Woo Product Slider, Woo Cross Sells, and more.",
-                "essential-addons-for-elementor-lite"
-                )}
-              </li>
-              <li>
-                -
-                {__(
-                "Cutting-edge Extensions Like Custom JS, Conditional Display, Content Protection, and more.",
-                "essential-addons-for-elementor-lite"
-                )}
-              </li>
-            </ul>
+            </p>
+            <div className="eael-feature-list-wrap mb-6">
+              <div className="eael-feature-list-item flex gap-2 items-center mb-4">
+                <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M5 7L6.33333 8.33333L9 5.66667M1 7C1 7.78793 1.15519 8.56815 1.45672 9.2961C1.75825 10.0241 2.20021 10.6855 2.75736 11.2426C3.31451 11.7998 3.97595 12.2417 4.7039 12.5433C5.43185 12.8448 6.21207 13 7 13C7.78793 13 8.56815 12.8448 9.2961 12.5433C10.0241 12.2417 10.6855 11.7998 11.2426 11.2426C11.7998 10.6855 12.2417 10.0241 12.5433 9.2961C12.8448 8.56815 13 7.78793 13 7C13 6.21207 12.8448 5.43185 12.5433 4.7039C12.2417 3.97595 11.7998 3.31451 11.2426 2.75736C10.6855 2.20021 10.0241 1.75825 9.2961 1.45672C8.56815 1.15519 7.78793 1 7 1C6.21207 1 5.43185 1.15519 4.7039 1.45672C3.97595 1.75825 3.31451 2.20021 2.75736 2.75736C2.20021 3.31451 1.75825 3.97595 1.45672 4.7039C1.15519 5.43185 1 6.21207 1 7Z" stroke="#750EF4" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+                <p>EA Fancy Chart  Symbol display option in the fancy chart</p>
+              </div>
+              <div className="eael-feature-list-item flex gap-2 items-center mb-4">
+                <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M5 7L6.33333 8.33333L9 5.66667M1 7C1 7.78793 1.15519 8.56815 1.45672 9.2961C1.75825 10.0241 2.20021 10.6855 2.75736 11.2426C3.31451 11.7998 3.97595 12.2417 4.7039 12.5433C5.43185 12.8448 6.21207 13 7 13C7.78793 13 8.56815 12.8448 9.2961 12.5433C10.0241 12.2417 10.6855 11.7998 11.2426 11.2426C11.7998 10.6855 12.2417 10.0241 12.5433 9.2961C12.8448 8.56815 13 7.78793 13 7C13 6.21207 12.8448 5.43185 12.5433 4.7039C12.2417 3.97595 11.7998 3.31451 11.2426 2.75736C10.6855 2.20021 10.0241 1.75825 9.2961 1.45672C8.56815 1.15519 7.78793 1 7 1C6.21207 1 5.43185 1.15519 4.7039 1.45672C3.97595 1.75825 3.31451 2.20021 2.75736 2.75736C2.20021 3.31451 1.75825 3.97595 1.45672 4.7039C1.15519 5.43185 1 6.21207 1 7Z" stroke="#750EF4" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+                <p>EA Instagram Feed  Feed is getting broken on the front-end view</p>
+              </div>
+              <div className="eael-feature-list-item flex gap-2 items-center mb-4">
+                <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M5 7L6.33333 8.33333L9 5.66667M1 7C1 7.78793 1.15519 8.56815 1.45672 9.2961C1.75825 10.0241 2.20021 10.6855 2.75736 11.2426C3.31451 11.7998 3.97595 12.2417 4.7039 12.5433C5.43185 12.8448 6.21207 13 7 13C7.78793 13 8.56815 12.8448 9.2961 12.5433C10.0241 12.2417 10.6855 11.7998 11.2426 11.2426C11.7998 10.6855 12.2417 10.0241 12.5433 9.2961C12.8448 8.56815 13 7.78793 13 7C13 6.21207 12.8448 5.43185 12.5433 4.7039C12.2417 3.97595 11.7998 3.31451 11.2426 2.75736C10.6855 2.20021 10.0241 1.75825 9.2961 1.45672C8.56815 1.15519 7.78793 1 7 1C6.21207 1 5.43185 1.15519 4.7039 1.45672C3.97595 1.75825 3.31451 2.20021 2.75736 2.75736C2.20021 3.31451 1.75825 3.97595 1.45672 4.7039C1.15519 5.43185 1 6.21207 1 7Z" stroke="#750EF4" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+                <p>EA Woo Product Carouse Option to hide and show add to cart</p>
+              </div>
+            </div>
             <a
               href="https://essential-addons.com/demos/"
               target="_blank"
@@ -119,23 +116,23 @@ function GoProContent({activeTab, handleTabChange}) {
       </div>
       <div className="eael-section-wrapper flex flex-end gap-4">
         <button
-            className="previous-btn flex gap-2 items-center eael-setup-next-btn"
-            type="button"
-            data-next="elements"
-            onClick={handleTabChange}
+          className="previous-btn flex gap-2 items-center eael-setup-next-btn"
+          type="button"
+          data-next="elements"
+          onClick={handleTabChange}
         >
           <i className="ea-dash-icon ea-left-arrow-long"></i>
           {__('Previous', "essential-addons-for-elementor-lite")}
         </button>
         <button
-            className="primary-btn install-btn flex gap-2 items-center eael-setup-next-btn"
-            type="button"
-            data-next={
-              !templately_local_plugin_data !== false
-                  ? "templately"
-                  : "integrations"
-            }
-            onClick={handleTabChange}
+          className="primary-btn install-btn flex gap-2 items-center eael-setup-next-btn"
+          type="button"
+          data-next={
+            !templately_local_plugin_data !== false
+              ? "templately"
+              : "integrations"
+          }
+          onClick={handleTabChange}
         >
           {__('Next', "essential-addons-for-elementor-lite")}
           <i className="ea-dash-icon ea-right-arrow-long"></i>
