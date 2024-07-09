f3e1780ad3665d50d723dbd60e756a315a11073b
Linkon Miyan <rudlinkon@gmail.com>
2024-07-09 18:13:05 +0600

    dynamic image and content

--- includes/templates/admin/eael-dashboard/src/components/Search404.jsx
+++ includes/templates/admin/eael-dashboard/src/components/Search404.jsx
@@ -1,16 +1,13 @@
-import consumer from "../context";
-import React from "react";
-
 function Search404() {
-    const {eaState} = consumer(),
+    const reactPath = localize.eael_dashboard.reactPath,
         i18n = localize.eael_dashboard.i18n;
 
     return (
         <>
             <div className="ea__contents">
                 <div className="ea__not-found-wrapper">
-                    <img src='../../dist/images/not-found.png' alt="img"/>
-                    <h5>No elements found with these keyword </h5>
+                    <img src={reactPath + 'images/not-found.png'} alt="img"/>
+                    <h5>{i18n.search_not_found}</h5>
                 </div>
             </div>
         </>
