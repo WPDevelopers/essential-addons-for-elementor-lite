ebaff7f139e72e291a4981f4b179ca8d2c6bb3e8
Jakaria Istauk <jakariamd35@gmail.com>
2024-07-09 15:38:17 +0600

    Wrapper Link: popup & custom atrribute issue fixed

--- includes/Extensions/Wrapper_Link.php
+++ includes/Extensions/Wrapper_Link.php
@@ -20,6 +20,7 @@ class Wrapper_Link {
 		add_action( 'elementor/element/section/section_advanced/after_section_end', [ $this, 'register_controls' ] );
 		add_action( 'elementor/element/container/section_layout/after_section_end', [ $this, 'register_controls' ] );
 		add_action( 'elementor/frontend/before_render', [ $this, 'before_render' ], 1 );
+		add_action( 'elementor/frontend/after_render', [ $this, 'after_render' ], 1 );
 	}
 
 	public function register_controls( $element ) {
@@ -60,14 +61,16 @@ class Wrapper_Link {
 		$wrapper_link_settings = $element->get_settings_for_display( 'eael_wrapper_link' );
 
 		if ( ! empty( $element->get_settings_for_display( 'eael_wrapper_link_switch' ) ) && ! empty( $wrapper_link_settings['url'] ) ) {
-			$element->add_render_attribute( '_wrapper',
-				'data-eael-wrapper-link',
-				wp_json_encode( [
-					'url'         => esc_url( $wrapper_link_settings['url'] ),
-					'is_external' => esc_attr( $wrapper_link_settings['is_external'] ),
-					'nofollow'    => esc_attr( $wrapper_link_settings['nofollow'] )
-				] )
-			);
+			$element->add_link_attributes( 'eael_wrapper_link', $wrapper_link_settings );
+			echo "<a "; $element->print_render_attribute_string( 'eael_wrapper_link' ); echo ">";
+		}
+	}
+
+	public function after_render( $element ){
+		$wrapper_link_settings = $element->get_settings_for_display( 'eael_wrapper_link' );
+
+		if ( ! empty( $element->get_settings_for_display( 'eael_wrapper_link_switch' ) ) && ! empty( $wrapper_link_settings['url'] ) ) {
+			echo "</a>";
 		}
 	}
 
