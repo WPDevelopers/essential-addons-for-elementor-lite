9a29b63957fcd61f07881fbb7007566f11cc208a
Linkon Miyan <rudlinkon@gmail.com>
2024-07-11 19:10:46 +0600

    adjust the offset

--- includes/templates/admin/eael-dashboard/src/components/ElementCategoryBox.jsx
+++ includes/templates/admin/eael-dashboard/src/components/ElementCategoryBox.jsx
@@ -7,7 +7,7 @@ function ElementCategoryBox(props) {
             e.preventDefault();
 
             window.scrollTo({
-                top: props.subCatRef.current.children['ID-' + props.index]?.offsetTop + 1 + 32 + eaState.scrollOffset,
+                top: props.subCatRef.current.children['ID-' + props.index]?.offsetTop + 35 + eaState.scrollOffset,
                 behavior: 'smooth'
             });
         };
