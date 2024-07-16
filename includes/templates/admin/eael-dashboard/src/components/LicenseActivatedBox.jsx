127be50080ce808571480dee1abf36e4397755e4
Linkon Miyan <rudlinkon@gmail.com>
2024-07-16 18:26:53 +0600

    update activated license content

--- includes/templates/admin/eael-dashboard/src/components/LicenseActivatedBox.jsx
+++ includes/templates/admin/eael-dashboard/src/components/LicenseActivatedBox.jsx
@@ -6,9 +6,8 @@ function LicenseActivatedBox() {
                     <i className="ea-dash-icon ea-lock"></i>
                 </div>
                 <div className="max-w-454">
-                    <h4>Enjoy the Amazing Features & Support</h4>
-                    <p>Congratulations! You've unlocked Essential Addons PRO. Now build your dream website effortlessly
-                        with more advanced features. If you get stuck, take help from our amazing support team.</p>
+                    <h4>You have Unlocked Essential Addons Pro</h4>
+                    <p>Congratulations! Now build your dream website effortlessly with more advanced features & priority support.</p>
                     <span className="activated-btn"><i className="ea-dash-icon  ea-check"></i>Activated</span>
                 </div>
             </div>
