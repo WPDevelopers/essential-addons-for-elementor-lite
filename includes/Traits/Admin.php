bd7226abb642d2230980021edb66f9d4fb1c9b05
Linkon Miyan <rudlinkon@gmail.com>
2024-08-01 12:48:11 +0600

    update admin enqueue versions

--- includes/Traits/Admin.php
+++ includes/Traits/Admin.php
@@ -51,8 +51,8 @@ trait Admin {
 
 	    if ( isset( $hook ) && $hook == 'toplevel_page_eael-settings' ) {
 		    wp_enqueue_style( 'eael-admin-icon-css', EAEL_PLUGIN_URL . 'includes/templates/admin/icons/style.css', false, EAEL_PLUGIN_VERSION );
-		    wp_enqueue_style( 'eael-admin-css', EAEL_PLUGIN_URL . 'includes/templates/admin/eael-dashboard/dist/assets/ea-dashboard.css', false, time() );
-		    wp_enqueue_script( 'eael-admin-dashboard', EAEL_PLUGIN_URL . 'includes/templates/admin/eael-dashboard/dist/assets/ea-dashboard.js', array(), time(), true );
+		    wp_enqueue_style( 'eael-admin-css', EAEL_PLUGIN_URL . 'includes/templates/admin/eael-dashboard/dist/assets/ea-dashboard.css', false, EAEL_PLUGIN_VERSION );
+		    wp_enqueue_script( 'eael-admin-dashboard', EAEL_PLUGIN_URL . 'includes/templates/admin/eael-dashboard/dist/assets/ea-dashboard.js', array(), EAEL_PLUGIN_VERSION, true );
 		    add_filter( 'wp_script_attributes', [ $this, 'add_type_attribute' ] );
 
 		    //Internationalizing JS string translation
