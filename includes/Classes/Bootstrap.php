31f5b2e18921a5e6628a83c867ef16761e698b22
Himadree Shekhar Halder <himadree12@gmail.com>
2024-08-06 17:40:53 +0600

    Fix double add to cart button with astra theme

--- includes/Classes/Bootstrap.php
+++ includes/Classes/Bootstrap.php
@@ -262,9 +262,10 @@ class Bootstrap
 
 			    add_action( 'woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open' );
 			    add_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close' );
-			    add_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart' );
                 if( function_exists( 'astra_woo_woocommerce_shop_product_content' ) ){
                     add_action( 'woocommerce_after_shop_loop_item', 'astra_woo_woocommerce_shop_product_content' );
+                } else {
+                    add_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart' );
                 }
 		    } );
 	    }
