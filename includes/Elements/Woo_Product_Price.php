d28a1fd33379ae8b9cee7c32d51c459da061aac2
Himadree Shekhar Halder <himadree12@gmail.com>
2024-08-20 14:42:46 +0600

    Add style controllers

--- includes/Elements/Woo_Product_Price.php
+++ includes/Elements/Woo_Product_Price.php
@@ -19,7 +19,7 @@ class Woo_Product_Price extends Widget_Base {
 	}
 
 	public function get_icon() {
-		return 'eicon-meetup';
+		return 'eicon-product-price';
 	}
 
 	public function get_categories() {
@@ -41,16 +41,233 @@ class Woo_Product_Price extends Widget_Base {
 			]
 		);
 
+        //Start regular price
+        $this->add_control(
+			'regular_peice_heading',
+			[
+				'label' => esc_html__( 'Regular Price', 'essential-addons-for-elementor-lite' ),
+				'type' => \Elementor\Controls_Manager::HEADING,
+				'separator' => 'after',
+			]
+		);
+
 		$this->add_control(
-			'title_color',
+			'price_color',
 			[
 				'label'     => esc_html__( 'Color', 'essential-addons-for-elementor-lite' ),
 				'type'      => \Elementor\Controls_Manager::COLOR,
 				'selectors' => [
-					'{{WRAPPER}} .hello-world' => 'color: {{VALUE}};',
+					'.woocommerce {{WRAPPER}} .price' => 'color: {{VALUE}};',
 				],
 			]
 		);
+
+        $this->add_control(
+			'price_align',
+			[
+				'label'   => esc_html__( 'Alignment', 'essential-addons-for-elementor-lite' ),
+				'type'    => \Elementor\Controls_Manager::CHOOSE,
+				'options' => [
+					'left' => [
+						'title' => esc_html__( 'Left', 'essential-addons-for-elementor-lite' ),
+						'icon'  => 'eicon-text-align-left',
+					],
+					'center' => [
+						'title' => esc_html__( 'Center', 'essential-addons-for-elementor-lite' ),
+						'icon'  => 'eicon-text-align-center',
+					],
+					'right' => [
+						'title' => esc_html__( 'Right', 'essential-addons-for-elementor-lite' ),
+						'icon'  => 'eicon-text-align-right',
+					],
+				],
+				'toggle'       => true,
+				'selectors'    => [
+					'{{WRAPPER}}' =>  'text-align: {{VALUE}};',
+				],
+			]
+		);
+
+        $this->add_group_control(
+			\Elementor\Group_Control_Typography::get_type(),
+			[
+				'name'     => 'price_typography',
+				'selector' => '.woocommerce {{WRAPPER}} .price',
+			]
+		);
+
+        $this->add_control(
+			'price_text_decoration_color',
+			[
+				'label'     => esc_html__( 'Text Decoration Color', 'essential-addons-for-elementor-lite' ),
+				'type'      => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'.woocommerce {{WRAPPER}} .price del' => 'text-decoration-color: {{VALUE}};',
+				],
+			]
+		);
+        //End regular price
+
+        //Start Currency Symbol
+        $this->add_control(
+			'peice_currency_heading',
+			[
+				'label' => esc_html__( 'Price Currency Symbol', 'essential-addons-for-elementor-lite' ),
+				'type' => \Elementor\Controls_Manager::HEADING,
+				'separator' => 'before',
+			]
+		);
+
+        $this->add_control(
+			'price_currency_color',
+			[
+				'label'     => esc_html__( 'Color', 'essential-addons-for-elementor-lite' ),
+				'type'      => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'.woocommerce {{WRAPPER}} .price del .woocommerce-Price-currencySymbol' => 'color: {{VALUE}};',
+				],
+			]
+		);
+
+        $this->add_group_control(
+			\Elementor\Group_Control_Typography::get_type(),
+			[
+				'name'     => 'price_currency_typography',
+				'selector' => '.woocommerce {{WRAPPER}} .price del .woocommerce-Price-currencySymbol',
+			]
+		);
+
+        $this->add_control(
+			'price_currency_spacing',
+			[
+				'label' => esc_html__( 'Spacing', 'essential-addons-for-elementor-lite' ),
+				'type' => \Elementor\Controls_Manager::SLIDER,
+				'size_units' => [ 'px', 'rem', 'em', '%', 'custom' ],
+				'range' => [
+					'px' => [
+						'max' => 100,
+					],
+					'%' => [
+						'max' => 100,
+					],
+				],
+				'selectors' => [
+					'.woocommerce {{WRAPPER}} .price del .woocommerce-Price-currencySymbol' => 'margin-right: {{SIZE}}{{UNIT}};',
+				],
+			]
+		);
+        //End Currency Symbol
+
+        //Start sale price
+        $this->add_control(
+			'sale_heading',
+			[
+				'label' => esc_html__( 'Sale Price', 'essential-addons-for-elementor-lite' ),
+				'type' => \Elementor\Controls_Manager::HEADING,
+				'separator' => 'before',
+			]
+		);
+
+        $this->add_control(
+			'sale_color',
+			[
+				'label'     => esc_html__( 'Color', 'essential-addons-for-elementor-lite' ),
+				'type'      => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'.woocommerce {{WRAPPER}} .price ins' => 'color: {{VALUE}};',
+				],
+			]
+		);
+
+        $this->add_group_control(
+			\Elementor\Group_Control_Typography::get_type(),
+			[
+				'name'     => 'sale_typography',
+				'selector' => '.woocommerce {{WRAPPER}} .price ins',
+			]
+		);
+
+        $this->add_control(
+			'sale_spacing',
+			[
+				'label' => esc_html__( 'Spacing', 'essential-addons-for-elementor-lite' ),
+				'type' => \Elementor\Controls_Manager::SLIDER,
+				'size_units' => [ 'px', 'rem', 'em', '%', 'custom' ],
+				'range' => [
+					'px' => [
+						'max' => 100,
+					],
+					'%' => [
+						'max' => 100,
+					],
+				],
+				'selectors' => [
+					'.woocommerce {{WRAPPER}} .price del' => 'margin-right: {{SIZE}}{{UNIT}}',
+				],
+			]
+		);
+
+        $this->add_control(
+			'sale_text_decoration_color',
+			[
+				'label'     => esc_html__( 'Text Decoration Color', 'essential-addons-for-elementor-lite' ),
+				'type'      => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'.woocommerce {{WRAPPER}} .price ins' => 'text-decoration-color: {{VALUE}};',
+				],
+			]
+		);
+        //End sale price
+
+        //Start Currency Symbol
+        $this->add_control(
+			'sale_currency_heading',
+			[
+				'label' => esc_html__( 'Sale Currency Symbol', 'essential-addons-for-elementor-lite' ),
+				'type' => \Elementor\Controls_Manager::HEADING,
+				'separator' => 'before',
+			]
+		);
+
+        $this->add_control(
+			'sale_currency_color',
+			[
+				'label'     => esc_html__( 'Color', 'essential-addons-for-elementor-lite' ),
+				'type'      => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'.woocommerce {{WRAPPER}} .price ins .woocommerce-Price-currencySymbol' => 'color: {{VALUE}};',
+				],
+			]
+		);
+
+        $this->add_group_control(
+			\Elementor\Group_Control_Typography::get_type(),
+			[
+				'name'     => 'sale_currency_typography',
+				'selector' => '.woocommerce {{WRAPPER}} .price ins .woocommerce-Price-currencySymbol',
+			]
+		);
+
+        $this->add_control(
+			'sale_currency_spacing',
+			[
+				'label' => esc_html__( 'Spacing', 'essential-addons-for-elementor-lite' ),
+				'type' => \Elementor\Controls_Manager::SLIDER,
+				'size_units' => [ 'px', 'rem', 'em', '%', 'custom' ],
+				'range' => [
+					'px' => [
+						'max' => 100,
+					],
+					'%' => [
+						'max' => 100,
+					],
+				],
+				'selectors' => [
+					'.woocommerce {{WRAPPER}} .price ins .woocommerce-Price-currencySymbol' => 'margin-right: {{SIZE}}{{UNIT}}',
+				],
+			]
+		);
+        //End Currency Symbol
 
 		$this->end_controls_section();
 		// Style Tab End
