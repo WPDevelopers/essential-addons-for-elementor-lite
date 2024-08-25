53f6f8f67c6b18fc12cb645a52bf4589a4c4d376
Himadree Shekhar Halder <himadree12@gmail.com>
2024-08-25 11:14:45 +0600

    Add quantity controller

--- includes/Elements/Woo_Product_Add_To_Cart.php
+++ includes/Elements/Woo_Product_Add_To_Cart.php
@@ -238,6 +238,204 @@ class Woo_Product_Add_To_Cart extends Widget_Base {
 		$this->end_controls_section();
 		// End Button
 
+         // Start Quantity
+		$this->start_controls_section(
+			'eael_add_to_cart_quantity_section',
+			[
+				'label' => esc_html__( 'Quantity', 'essential-addons-for-elementor-lite' ),
+				'tab'   => \Elementor\Controls_Manager::TAB_STYLE,
+			]
+		);
+
+        $this->add_control(
+			'eael_show_quantity',
+			[
+				'label'        => esc_html__( 'Show Quantity', 'essential-addons-for-elementor-lite' ),
+				'type'         => \Elementor\Controls_Manager::SWITCHER,
+				'label_on'     => esc_html__( 'Show', 'essential-addons-for-elementor-lite' ),
+				'label_off'    => esc_html__( 'Hide', 'essential-addons-for-elementor-lite' ),
+				'return_value' => 'yes',
+				'default'      => 'yes',
+			]
+		);
+
+        $this->add_control(
+			'eael_add_to_cart_qt_spacing',
+			[
+				'label'      => esc_html__( 'Spacing', 'essential-addons-for-elementor-lite' ),
+				'type'       => \Elementor\Controls_Manager::SLIDER,
+				'size_units' => [ 'px', '%', 'rem', 'em', 'custom' ],
+				'range'      => [
+					'px' => [
+						'max' => 100,
+					],
+					'%' => [
+						'max' => 100,
+					],
+					'rem' => [
+						'max' => 100,
+					],
+					'em' => [
+						'max' => 100,
+					],
+				],
+				'selectors' => [
+					'.woocommerce {{WRAPPER}} .eael-single-product-add-to-cart .cart .button' => 'margin-left: {{SIZE}}{{UNIT}};',
+				],
+			]
+		);
+
+        $this->add_group_control(
+			\Elementor\Group_Control_Typography::get_type(),
+			[
+				'name'     => 'eael_add_to_cart_qt_typo',
+				'selector' => '.woocommerce {{WRAPPER}} .eael-single-product-add-to-cart .quantity .qty',
+			]
+		);
+
+        $this->add_group_control(
+			\Elementor\Group_Control_Border::get_type(),
+			[
+				'name'                 => 'eael_add_to_cart_qt_border',
+				'exclude'              => [ 'color' ],
+                'selector' => '.woocommerce {{WRAPPER}} .eael-single-product-add-to-cart .quantity .qty',
+			]
+		);
+
+        $this->add_control(
+			'eael_add_to_cart_qt_border_radius',
+			[
+				'label'      => esc_html__( 'Border Radius', 'essential-addons-for-elementor-lite' ),
+				'type'       => \Elementor\Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px', '%', 'rem', 'em', 'custom' ],
+				'selectors'  => [
+					'.woocommerce {{WRAPPER}} .eael-single-product-add-to-cart .quantity .qty' => 'border-radius: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+
+        $this->add_control(
+			'eael_add_to_cart_qt_padding',
+			[
+				'label'      => esc_html__( 'Padding', 'essential-addons-for-elementor-lite' ),
+				'type'       => \Elementor\Controls_Manager::DIMENSIONS,
+				'size_units' => [ 'px', '%', 'rem', 'em', 'custom' ],
+				'selectors'  => [
+					'.woocommerce {{WRAPPER}} .eael-single-product-add-to-cart .quantity .qty' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+				],
+			]
+		);
+
+        $this->start_controls_tabs( 'eael_add_to_cart_qt_style_tabs' );
+
+        $this->start_controls_tab( 'eael_add_to_cart_qt_style_normal',
+			[
+				'label' => esc_html__( 'Normal', 'essential-addons-for-elementor-lite' ),
+			]
+		);
+
+		$this->add_control(
+			'eael_add_to_cart_qt_text_color',
+			[
+				'label'     => esc_html__( 'Text Color', 'essential-addons-for-elementor-lite' ),
+				'type'      => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'.woocommerce {{WRAPPER}} .eael-single-product-add-to-cart .quantity .qty' => 'color: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->add_control(
+			'eael_add_to_cart_qt_bg_color',
+			[
+				'label'     => esc_html__( 'Background Color', 'essential-addons-for-elementor-lite' ),
+				'type'      => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'.woocommerce {{WRAPPER}} .eael-single-product-add-to-cart .quantity .qty' => 'background-color: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->add_control(
+			'eael_add_to_cart_qt_border_color',
+			[
+				'label'     => esc_html__( 'Border Color', 'essential-addons-for-elementor-lite' ),
+				'type'      => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'.woocommerce {{WRAPPER}} .eael-single-product-add-to-cart .quantity .qty' => 'border-color: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->end_controls_tab();
+
+        $this->start_controls_tab( 'eael_add_to_cart_qt_style_focus',
+			[
+				'label' => esc_html__( 'Focus', 'essential-addons-for-elementor-lite' ),
+			]
+		);
+
+		$this->add_control(
+			'eael_add_to_cart_qt_text_color_focus',
+			[
+				'label'     => esc_html__( 'Text Color', 'essential-addons-for-elementor-lite' ),
+				'type'      => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'.woocommerce {{WRAPPER}} .eael-single-product-add-to-cart .quantity .qty:focus' => 'color: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->add_control(
+			'eael_add_to_cart_qt_bg_color_focus',
+			[
+				'label'     => esc_html__( 'Background Color', 'essential-addons-for-elementor-lite' ),
+				'type'      => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'.woocommerce {{WRAPPER}} .eael-single-product-add-to-cart .quantity .qty:focus' => 'background-color: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->add_control(
+			'eael_add_to_cart_qt_border_color_focus',
+			[
+				'label'     => esc_html__( 'Border Color', 'essential-addons-for-elementor-lite' ),
+				'type'      => \Elementor\Controls_Manager::COLOR,
+				'selectors' => [
+					'.woocommerce {{WRAPPER}} .eael-single-product-add-to-cart .quantity .qty:focus' => 'border-color: {{VALUE}}',
+				],
+			]
+		);
+
+		$this->add_control(
+			'eael_add_to_cart_qt_transition',
+			[
+				'label'   => esc_html__( 'Transition Duration (s)', 'essential-addons-for-elementor-lite' ),
+				'type'    => \Elementor\Controls_Manager::SLIDER,
+				'default' => [
+					'size' => 0.2,
+				],
+				'range' => [
+					'px' => [
+						'min'  => 0,
+						'max'  => 3,
+						'step' => 0.1,
+					],
+				],
+				'selectors' => [
+					'.woocommerce {{WRAPPER}} .eael-single-product-add-to-cart .quantity .qty' => 'transition: all {{SIZE}}s',
+				],
+			]
+		);
+
+		$this->end_controls_tab();
+
+        $this->end_controls_tabs();
+
+		$this->end_controls_section();
+		// End
+
 	}
 
 	protected function render() {
