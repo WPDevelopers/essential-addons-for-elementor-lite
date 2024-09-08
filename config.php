4ad9cca7277a853019794bfded17e17f20547287
Linkon Miyan <rudlinkon@gmail.com>
2024-09-08 19:44:04 +0600

    sanitize the string contents by DOMPurify -- fancy text

--- config.php
+++ config.php
@@ -81,6 +81,11 @@ $config = [
                     ],
                 ],
                 'js' => [
+	                [
+		                'file' => EAEL_PLUGIN_PATH . '/assets/front-end/js/lib-view/dom-purify/purify.min.js',
+		                'type' => 'lib',
+		                'context' => 'view',
+	                ],
                     [
                         'file' => EAEL_PLUGIN_PATH . 'assets/front-end/js/lib-view/morphext/morphext.min.js',
                         'type' => 'lib',
