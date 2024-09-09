1697ef62dc9f9e33c68ec0aa3cf029f4aa16b569
Linkon Miyan <rudlinkon@gmail.com>
2024-09-09 12:04:47 +0600

    sanitize title attribute on the editor page also

--- includes/Elements/Fancy_Text.php
+++ includes/Elements/Fancy_Text.php
@@ -603,8 +603,6 @@ class Fancy_Text extends Widget_Base {
 	}
 
 	protected function render() {
-
-
 		$settings = $this->get_settings_for_display();
 		$fancy_text = $this->fancy_text($settings['eael_fancy_text_strings']);
 		if(!apply_filters('eael/pro_enabled', false)) { $settings['eael_fancy_text_style'] = 'style-1'; }
@@ -654,5 +652,4 @@ class Fancy_Text extends Widget_Base {
 
 	}
 
-	protected function content_template() {}
 }
