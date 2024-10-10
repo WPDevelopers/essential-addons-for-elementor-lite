de1bde42bb055989a657a9f284aac2578eb4045a
Jakaria Istauk <jakariamd35@gmail.com>
2024-10-10 16:07:32 +0600

    Variable name correction

--- includes/Elements/Advanced_Data_Table.php
+++ includes/Elements/Advanced_Data_Table.php
@@ -1647,8 +1647,8 @@ class Advanced_Data_Table extends Widget_Base
             foreach ($table_rows as $key => $tr) {
                 if( $is_edit_mode && 'yes' === $settings['ea_adv_data_table_pagination'] ){
                     $row_count++;
-                    $pasination_count = $settings['ea_adv_data_table_items_per_page'] > 0 ? $settings['ea_adv_data_table_items_per_page'] : 10;
-                    if( $row_count > $pasination_count ){
+                    $pagination_count = $settings['ea_adv_data_table_items_per_page'] > 0 ? $settings['ea_adv_data_table_items_per_page'] : 10;
+                    if( $row_count > $pagination_count ){
                         break;
                     }
                 }
