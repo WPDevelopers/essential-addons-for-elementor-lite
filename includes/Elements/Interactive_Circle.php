ef68f27bdd4bc5c113fb4db2cb7a3904d6371e7f
Jakaria Istauk <jakariamd35@gmail.com>
2024-10-15 15:03:59 +0600

    Interactive Circle: content rendering issue fixed

--- includes/Elements/Interactive_Circle.php
+++ includes/Elements/Interactive_Circle.php
@@ -1129,7 +1129,9 @@ class Interactive_Circle extends Widget_Base {
                                     </div>
                                     <div id="eael-interactive-<?php echo esc_attr( $item_count ); ?>" aria-labelledby="eael-circle-item-<?php echo esc_attr( $item_count ); ?>" class="eael-circle-btn-content eael-circle-item-<?php echo esc_attr( $item_count . ' ' . $is_active ); ?>">
                                         <div class="eael-circle-content">
-											<?php echo wp_kses( $item['eael_interactive_circle_item_content'], Helper::eael_allowed_tags() ); ?>
+											<?php 
+											// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+											echo $this->parse_text_editor( $item['eael_interactive_circle_item_content'] ); ?>
                                         </div>
                                     </div>
                                 </div>
