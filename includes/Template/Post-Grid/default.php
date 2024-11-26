080c6553794474c4167621cc50c4ddfe71293d5c
Nazmul Hasan Robin <robin.sust08@gmail.com>
2024-11-26 16:07:05 +0600

    Text domain issue fixing wip

--- includes/Template/Post-Grid/default.php
+++ includes/Template/Post-Grid/default.php
@@ -46,7 +46,7 @@ if ( is_object( $authordata ) ) {
 		'<a href="%1$s" title="%2$s" rel="author">%3$s</a>',
 		esc_url( $author_url ),
 		/* translators: %s: Author's display name. */
-		esc_attr( sprintf( __( 'Posts by %s' ), esc_html( $author_name ) ) ),
+		esc_attr( sprintf( __( 'Posts by %s', 'essential-addons-for-elementor-lite' ), esc_html( $author_name ) ) ),
 		esc_html( $author_name )
 	);
 }
