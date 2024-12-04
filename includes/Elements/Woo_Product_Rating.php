480e3fd75bb4480d06a6088631fa2055e955828f
himadree <himadree12@gmail.com>
2024-12-04 13:03:28 +0600

    Update rating color control

--- includes/Elements/Woo_Product_Rating.php
+++ includes/Elements/Woo_Product_Rating.php
@@ -84,10 +84,24 @@ class Woo_Product_Rating extends Widget_Base {
 		);
 
 		$this->add_control(
-			'eael_star_color',
+			'eael_rating_color',
 			[
-				'label'     => esc_html__( 'Star Color', 'essential-addons-for-elementor-lite' ),
+				'label'     => esc_html__( 'Rating Color', 'essential-addons-for-elementor-lite' ),
 				'type'      => \Elementor\Controls_Manager::COLOR,
+				'default'   => '#858483',
+				'selectors' => [
+					'.woocommerce {{WRAPPER}} .eael-single-product-rating .star-rating' => 'color: {{VALUE}};',
+					'{{WRAPPER}} .eael-single-product-rating .eael-product-rating-wrap .eael-product-rating.unfilled svg path' => 'fill: {{VALUE}};',
+				],
+			]
+		);
+
+		$this->add_control(
+			'eael_rating_count_color',
+			[
+				'label'     => esc_html__( 'Rating Given Color', 'essential-addons-for-elementor-lite' ),
+				'type'      => \Elementor\Controls_Manager::COLOR,
+				'default'   => '#fabb1c',
 				'selectors' => [
 					'.woocommerce {{WRAPPER}} .eael-single-product-rating .star-rating' => 'color: {{VALUE}};',
 					'{{WRAPPER}} .eael-single-product-rating .eael-product-rating-wrap .eael-product-rating.filled svg path' => 'fill: {{VALUE}};',
@@ -376,7 +390,7 @@ class Woo_Product_Rating extends Widget_Base {
 
 	public function eael_star_solid( $filled ) {
 		?>
-		<span class="eael-product-rating <?php echo $filled ? 'filled' : ''; ?>">
+		<span class="eael-product-rating <?php echo $filled ? 'filled' : 'unfilled'; ?>">
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
 				<path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/>
 			</svg>
@@ -386,7 +400,7 @@ class Woo_Product_Rating extends Widget_Base {
 
 	public function eael_star_classic( $filled ) {
 		?>
-		<span class="eael-product-rating <?php echo $filled ? 'filled' : ''; ?>">
+		<span class="eael-product-rating <?php echo $filled ? 'filled' : 'unfilled'; ?>">
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
 				<path d="M287.9 0c9.2 0 17.6 5.2 21.6 13.5l68.6 141.3 153.2 22.6c9 1.3 16.5 7.6 19.3 16.3s.5 18.1-5.9 24.5L433.6 328.4l26.2 155.6c1.5 9-2.2 18.1-9.7 23.5s-17.3 6-25.3 1.7l-137-73.2L151 509.1c-8.1 4.3-17.9 3.7-25.3-1.7s-11.2-14.5-9.7-23.5l26.2-155.6L31.1 218.2c-6.5-6.4-8.7-15.9-5.9-24.5s10.3-14.9 19.3-16.3l153.2-22.6L266.3 13.5C270.4 5.2 278.7 0 287.9 0zm0 79L235.4 187.2c-3.5 7.1-10.2 12.1-18.1 13.3L99 217.9 184.9 303c5.5 5.5 8.1 13.3 6.8 21L171.4 443.7l105.2-56.2c7.1-3.8 15.6-3.8 22.6 0l105.2 56.2L384.2 324.1c-1.3-7.7 1.2-15.5 6.8-21l85.9-85.1L358.6 200.5c-7.8-1.2-14.6-6.1-18.1-13.3L287.9 79z"/>
 			</svg>
@@ -396,7 +410,7 @@ class Woo_Product_Rating extends Widget_Base {
 
 	public function eael_star_half_stroke( $filled ) {
       ?>
-		<span class="eael-product-rating <?php echo $filled ? 'filled' : ''; ?>">
+		<span class="eael-product-rating <?php echo $filled ? 'filled' : 'unfilled'; ?>">
 			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
 				<path d="M309.5 13.5C305.5 5.2 297.1 0 287.9 0s-17.6 5.2-21.6 13.5L197.7 154.8 44.5 177.5c-9 1.3-16.5 7.6-19.3 16.3s-.5 18.1 5.9 24.5L142.2 328.4 116 483.9c-1.5 9 2.2 18.1 9.7 23.5s17.3 6 25.3 1.7l137-73.2 137 73.2c8.1 4.3 17.9 3.7 25.3-1.7s11.2-14.5 9.7-23.5L433.6 328.4 544.8 218.2c6.5-6.4 8.7-15.9 5.9-24.5s-10.3-14.9-19.3-16.3L378.1 154.8 309.5 13.5zM288 384.7l0-305.6 52.5 108.1c3.5 7.1 10.2 12.1 18.1 13.3l118.3 17.5L391 303c-5.5 5.5-8.1 13.3-6.8 21l20.2 119.6L299.2 387.5c-3.5-1.9-7.4-2.8-11.2-2.8z"/>
 			</svg>
