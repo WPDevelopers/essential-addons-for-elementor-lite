49181cf05e28646f8ec110a14fece39cdfc3751d
himadree <himadree12@gmail.com>
2024-12-08 11:54:47 +0600

    Fix thumb image size

--- includes/Elements/Woo_Product_Images.php
+++ includes/Elements/Woo_Product_Images.php
@@ -683,7 +683,7 @@ class Woo_Product_Images extends Widget_Base {
 				<div class="swiper swiper-slider swiper-container">
 					<div class="product_image_slider__trigger">
 						<a href="#">
-							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
+							<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg>
 						</a>
 					</div>
 					<div class="swiper-wrapper">
@@ -755,7 +755,8 @@ class Woo_Product_Images extends Widget_Base {
 		<div <?php $this->print_render_attribute_string( 'eael-pi-thumb' ); ?>>
 			<div class="swiper-container">
 				<?php if( 'yes' == $thumb_settings['thumbnail'] ) { ?>
-					<div class="swiper-wrapper">
+					<?php $single_thumb_img = ( count($img_links) > 1 ) ? '' : 'single-thumb-img'; ?>
+					<div class="swiper-wrapper <?php esc_attr_e( $single_thumb_img ); ?>">
 						<?php 
 							foreach ( $img_links as $img_link ) {
 								$this->render_slide( $img_link, 'product_image_slider__thumbs__image', 'thumbnail' );
