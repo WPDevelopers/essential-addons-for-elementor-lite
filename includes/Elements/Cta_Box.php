4b776b830db74e5151ffbb3d1306dc2ade603d55
Linkon Miyan <rudlinkon@gmail.com>
2025-01-05 13:35:03 +0600

    Simplify the CTA Box template rendering
    Replaced deprecated `Elementor\Frontend` usage with `Plugin::$instance->frontend` for fetching builder content. This ensures compatibility with the latest Elementor API and resolves potential issues with template rendering.

--- includes/Elements/Cta_Box.php
+++ includes/Elements/Cta_Box.php
@@ -1565,12 +1565,12 @@ class Cta_Box extends Widget_Base
         $contentMarkup = '';
         if ('content' == $settings['eael_cta_title_content_type']) {
             $contentMarkup .= wp_kses( $settings['eael_cta_content'], Helper::eael_allowed_tags() );
-        }else if ('template' == $settings['eael_cta_title_content_type']){
-            if (!empty($settings['eael_primary_templates'])) {
-                $eael_template_id = $settings['eael_primary_templates'];
-                $eael_frontend = new \Elementor\Frontend;
-                $contentMarkup .= $eael_frontend->get_builder_content($eael_template_id, true);
-            }
+        } else if ( 'template' == $settings['eael_cta_title_content_type'] ) {
+	        if ( ! empty( $settings['eael_primary_templates'] ) ) {
+		        $eael_template_id = $settings['eael_primary_templates'];
+
+		        $contentMarkup .= Plugin::$instance->frontend->get_builder_content( $eael_template_id, true );
+	        }
         }
 
         // button attributes
