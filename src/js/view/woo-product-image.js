131b27da4cea1a461ac4f76c11a14d76a9d1041a
himadree <himadree12@gmail.com>
2025-01-26 13:57:22 +0600

    Refactor variation image handling for improved readability

--- src/js/view/woo-product-image.js
+++ src/js/view/woo-product-image.js
@@ -1,95 +1,107 @@
 var WooProdectImage = function ($scope, $) {
-   var $productGallery = $(".eael-single-product-images");
+   //Select key elements
+   const $productGallery = $(".eael-single-product-images");
+   const $sliderThumbsOptions = $(".product_image_slider__thumbs", $scope);
+   const $sliderThumbs = $sliderThumbsOptions.data("pi_thumb");
 
-   let $sliderThumbsOptions = $(".product_image_slider__thumbs", $scope);
-   let $sliderThumbs = $sliderThumbsOptions.data("pi_thumb");
-
-   var $productGalleryImage = $(
+   //Find initial images
+   const $productGalleryImage = $(
       ".swiper-wrapper .swiper-slide:first-child .image_slider__image > img",
       $productGallery
    );
-
-   var $productThumbImage = $(
+   const $productThumbImage = $(
       ".swiper-wrapper .swiper-slide:first-child .product_image_slider__thumbs__image > img",
       $productGallery
    );
 
-   var originalImage = {
-      src: $productGalleryImage.attr("src"),
-      srcset: $productGalleryImage.attr("srcset"),
-      sizes: $productGalleryImage.attr("sizes"),
-   };
+   //Store original image attributes
+   const originalImage = getImageAttributes($productGalleryImage);
+   const originalThumbImage = getImageAttributes($productThumbImage);
 
-   var originalThumbImage = {
-      src: $productThumbImage.attr("src"),
-      srcset: $productThumbImage.attr("srcset"),
-      sizes: $productThumbImage.attr("sizes"),
-   };
+   //Helper function to get image attributes
+   function getImageAttributes($image) {
+      return {
+         src: $image.attr("src"),
+         srcset: $image.attr("srcset"),
+         sizes: $image.attr("sizes"),
+      };
+   }
 
-   // Listen for show_variation event
-   $(".variations_form").on("show_variation", function (event, variation) {
-      if (variation && variation.image && variation.image.src) {
-         eaelVariationProductImage(variation.image);
-         $(".swiper-container", $scope)[0].swiper.autoplay.stop();
-         $(".swiper-container", $scope)[0].swiper.slideTo(0);
-         $(".swiper-container", $scope)[1].swiper.autoplay.stop();
-         $(".swiper-container", $scope)[1].swiper.slideTo(0);
+   // Listen for show variation event
+   $(".variations_form").on("show_variation", handleShowVariation);
+
+   //Event handler for showing variation
+   function handleShowVariation(event, variation) {
+      if (variation?.image?.src) {
+         updateProductImage(variation.image);
+         stopSliders();
       }
-   });
+   }
 
-   $(".variations_form").on("hide_variation", function () {
-      eaelRemoveVariationProductImage();
+   //Stop sliders
+   function stopSliders() {
+      toggleSliderAutoplay("stop");
+   }
+
+   //Toggle slider autoplay
+   function toggleSliderAutoplay(action) {
+      const sliders = $(".swiper-container", $scope);
+      sliders.each((index, slider) => {
+         slider.swiper.autoplay[action]();
+         slider.swiper.slideTo(0);
+      });
+   }
+
+   //Update product iamges with variation images
+   function updateProductImage(variationImage) {
+      setImageAttributes($productGalleryImage, variationImage);
+      setImageAttributes($productThumbImage, variationImage);
+   }
+
+   //Set image attributes
+   function setImageAttributes($image, imageAttributes) {
+      $image
+         .attr("src", imageAttributes.src)
+         .attr("srcset", imageAttributes.srcset)
+         .attr("sizes", imageAttributes.sizes)
+         .attr("data-src", imageAttributes.src)
+         .attr("data-large_image", imageAttributes.full_src);
+   }
+
+   // Listen for hide variation or reset image event
+   $(".variations_form").on("hide_variation reset_image", handleResetVariation);
+
+   //Event handler for reseting variation
+   function handleResetVariation() {
+      resetProductImages();
+      resumeSliders();
+   }
+
+   //Resume sliders
+   function resumeSliders() {
       if ($sliderThumbs.autoplay !== undefined) {
-         $(".swiper-container", $scope)[0].swiper.autoplay.start();
-         $(".swiper-container", $scope)[1].swiper.autoplay.start();
+         toggleSliderAutoplay("start");
       }
-   });
+   }
 
-   $(".variations_form").on("reset_image", function () {
-      eaelRemoveVariationProductImage();
-      if ($sliderThumbs.autoplay !== undefined) {
-         $(".swiper-container", $scope)[0].swiper.autoplay.start();
-         $(".swiper-container", $scope)[1].swiper.autoplay.start();
-      }
-   });
+   //Reset product image to original state
+   function resetProductImages() {
+      resetProductImage($productGalleryImage, originalImage);
+      resetProductImage($productThumbImage, originalThumbImage);
+   }
 
-   const eaelVariationProductImage = (variationImage) => {
-      $productGalleryImage
-         .attr("src", variationImage.src)
-         .attr("srcset", variationImage.srcset)
-         .attr("sizes", variationImage.sizes)
-         .attr("data-src", variationImage.src)
-         .attr("data-large_image", variationImage.full_src);
-
-      $productThumbImage
-         .attr("src", variationImage.src)
-         .attr("srcset", variationImage.srcset)
-         .attr("sizes", variationImage.sizes)
-         .attr("data-src", variationImage.src)
-         .attr("data-large_image", variationImage.full_src);
-   };
-
-   const eaelRemoveVariationProductImage = () => {
-      $productGalleryImage.fadeOut(100, function () {
-         $productGalleryImage
-            .attr("src", originalImage.src)
-            .attr("srcset", originalImage.srcset)
-            .attr("sizes", originalImage.sizes)
+   //Reset a single image with fade effect
+   function resetProductImage($image, originalAttributes) {
+      $image.fadeOut(100, function () {
+         $image
+            .attr("src", originalAttributes.src)
+            .attr("srcset", originalAttributes.srcset)
+            .attr("sizes", originalAttributes.sizes)
             .removeAttr("data-src")
             .removeAttr("data-large_image");
-         $productGalleryImage.fadeIn(100);
+         $image.fadeIn(100);
       });
-
-      $productThumbImage.fadeOut(100, function () {
-         $productThumbImage
-            .attr("src", originalThumbImage.src)
-            .attr("srcset", originalThumbImage.srcset)
-            .attr("sizes", originalThumbImage.sizes)
-            .removeAttr("data-src")
-            .removeAttr("data-large_image");
-         $productThumbImage.fadeIn(100);
-      });
-   };
+   }
 
    const swiperLoader = (swiperElement, swiperConfig) => {
       if ("undefined" === typeof Swiper || "function" === typeof Swiper) {
