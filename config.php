a72d34bced1d93f3897f57d99dd7eb2d2860f07b
Jakaria Istauk <jakariamd35@gmail.com>
2025-06-04 16:11:31 +0600

    Custome cursor: Moved to pro

--- config.php
+++ config.php
@@ -1346,18 +1346,6 @@ $config = [
 		        ],
 	        ],
         ],
-        'custom-cursor' => [
-	        'class'      => '\Essential_Addons_Elementor\Extensions\Custom_Cursor',
-            'dependency' => [
-		        'js'  => [
-			        [
-				        'file'    => EAEL_PLUGIN_PATH . 'assets/front-end/js/edit/custom-cursor.min.js',
-				        'type'    => 'self',
-				        'context' => 'edit',
-			        ],
-		        ],
-	        ],
-        ],
         'special-hover-effect' => [
             'class'      => '\Essential_Addons_Elementor\Extensions\Hover_Effect',
             'dependency' => [
