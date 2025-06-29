cbcf4f8d3fad011dc739f32fcacb4c172c4e818c
Himadree Shekhar Halder <himadree12@gmail.com>
2025-06-29 17:11:29 +0600

    Refactor typography controls for Code Snippet widget. Removed redundant group control for line numbers.

--- includes/Elements/Code_Snippet.php
+++ includes/Elements/Code_Snippet.php
@@ -514,14 +514,6 @@ class Code_Snippet extends Widget_Base {
          ]
       );
 
-      $this->add_group_control(
-			\Elementor\Group_Control_Typography::get_type(),
-			[
-				'name'     => 'content_typography_line_numbers',
-				'selector' => '{{WRAPPER}} .eael-code-snippet-line-numbers',
-			]
-		);
-
       $this->end_controls_section();
 
       // Style Tab - Code Content Area
@@ -560,7 +552,7 @@ class Code_Snippet extends Widget_Base {
 			\Elementor\Group_Control_Typography::get_type(),
 			[
 				'name'     => 'content_typography',
-				'selector' => '{{WRAPPER}} .eael-code-snippet-code',
+				'selector' => '{{WRAPPER}} .eael-code-snippet-line-numbers, {{WRAPPER}} .eael-code-snippet-code, {{WRAPPER}} .eael-code-snippet-code code',
 			]
 		);
       $this->end_controls_section();
