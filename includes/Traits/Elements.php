f7b6b6005d7e3eea2f1a79bdd90bf69792a17cd3
Himadree Shekhar Halder <himadree12@gmail.com>
2025-07-03 13:25:01 +0600

    Add Liquid Glass Effect SVG and enhance styles for effect4

--- includes/Traits/Elements.php
+++ includes/Traits/Elements.php
@@ -42,6 +42,21 @@ trait Elements {
 	}
 
 	/**
+	 * Liquid Glass Effect SVG
+	 */
+	public function eael_liquid_glass_effect_svg() {
+		?>
+		<svg style="display: none">
+			<filter id="glass-distortion" x="0%" y="0%" width="100%" height="100%">
+            <feTurbulence type="fractalNoise" baseFrequency="0.008 0.008" numOctaves="2" seed="92" result="noise" />
+            <feGaussianBlur in="noise" stdDeviation="0.02" result="blur" />
+            <feDisplacementMap in="SourceGraphic" in2="blur" scale="77" xChannelSelector="R" yChannelSelector="G" />
+         </filter>
+      </svg>
+		<?php
+	}
+
+	/**
 	 * Register widgets
 	 *
 	 * @since v3.0.0
