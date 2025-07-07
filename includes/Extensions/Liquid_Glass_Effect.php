92f08a77390d43d28b8c4d1b8905f4cd3fe8122b
Himadree Shekhar Halder <himadree12@gmail.com>
2025-07-07 15:52:52 +0600

    Add custom Liquid Glass Effect settings controls for background color, opacity, and backdrop filter

--- includes/Extensions/Liquid_Glass_Effect.php
+++ includes/Extensions/Liquid_Glass_Effect.php
@@ -56,12 +56,102 @@ class Liquid_Glass_Effect {
 		);
 
 		$element->add_control(
+			'eael_liquid_glass_effect_settings',
+			[
+				'label'     => esc_html__( 'Liquid Glass Settings', 'essential-addons-for-elementor-lite' ),
+				'type'      => Controls_Manager::HEADING,
+				'separator' => 'before',
+				'condition' => [
+					'eael_liquid_glass_effect_switch' => 'yes',
+				]
+			]
+		);
+
+		$element->add_control(
+			'eael_liquid_glass_effect_bg_color',
+			[
+				'label'     => esc_html__( 'Bankground Color', 'essential-addons-for-elementor-lite' ),
+				'type'      => Controls_Manager::COLOR,
+				'selectors' => [
+					'{{WRAPPER}}.eael_liquid_glass-effect1, 
+					{{WRAPPER}}.eael_liquid_glass-effect2, 
+					{{WRAPPER}}.eael_liquid_glass-effect3, 
+					{{WRAPPER}}.eael_liquid_glass-effect4::before, 
+					{{WRAPPER}}.eael_liquid_glass-effect5::before, 
+					{{WRAPPER}}.eael_liquid_glass-effect6::before' => 'background-color: {{VALUE}}',
+				],
+				'condition' => [
+					'eael_liquid_glass_effect_switch' => 'yes',
+				],
+			]
+		);
+
+		$element->add_control(
+			'eael_liquid_glass_effect_opacity',
+			[
+				'label' => esc_html__( 'Opacity', 'essential-addons-for-elementor-lite' ),
+				'type'  => Controls_Manager::SLIDER,
+				'range' => [
+					'px' => [
+						'min'  => 0,
+						'max'  => 1,
+						'step' => .1,
+					],
+				],
+				'default' => [
+					'size' => 1,
+				],
+				'selectors' => [
+					'{{WRAPPER}}.eael_liquid_glass-effect1, 
+					{{WRAPPER}}.eael_liquid_glass-effect2, 
+					{{WRAPPER}}.eael_liquid_glass-effect3, 
+					{{WRAPPER}}.eael_liquid_glass-effect4::before, 
+					{{WRAPPER}}.eael_liquid_glass-effect5::before, 
+					{{WRAPPER}}.eael_liquid_glass-effect6::before' => 'opacity: {{SIZE}}',
+				],
+				'condition' => [
+					'eael_liquid_glass_effect_switch' => 'yes',
+				],
+			]
+		);
+
+		$element->add_control(
+			'eael_liquid_glass_effect_backdrop_filter',
+			[
+				'label' => esc_html__( 'Backdrop Filter', 'essential-addons-for-elementor-lite' ),
+				'type'  => Controls_Manager::SLIDER,
+				'range' => [
+					'px' => [
+						'min'  => 0,
+						'max'  => 30,
+						'step' => 1,
+					],
+				],
+				'default' => [
+					'size' => 5,
+				],
+				'selectors' => [
+					'{{WRAPPER}}.eael_liquid_glass-effect1, 
+					{{WRAPPER}}.eael_liquid_glass-effect2, 
+					{{WRAPPER}}.eael_liquid_glass-effect3, 
+					{{WRAPPER}}.eael_liquid_glass-effect4::before, 
+					{{WRAPPER}}.eael_liquid_glass-effect5::before, 
+					{{WRAPPER}}.eael_liquid_glass-effect6::before' => 'backdrop-filter: blur({{SIZE}}px)',
+				],
+				'condition' => [
+					'eael_liquid_glass_effect_switch' => 'yes',
+				],
+			]
+		);
+
+		$element->add_control(
 			'eael_liquid_glass_shadow_effect',
 			[
-				'label'   => esc_html__( 'Shadow Effects', 'essential-addons-for-elementor-lite' ),
-				'type'    => Controls_Manager::SELECT,
-				'default' => 'effect1',
-				'options' => [
+				'label'     => esc_html__( 'Shadow Effects', 'essential-addons-for-elementor-lite' ),
+				'type'      => Controls_Manager::SELECT,
+				'default'   => 'effect1',
+				'separator' => 'before',
+				'options'   => [
 					'' 		 => esc_html__( 'None', 'essential-addons-for-elementor-lite' ),
 					'effect1' => esc_html__( 'Effect 1', 'essential-addons-for-elementor-lite' ),
 					'effect2' => esc_html__( 'Effect 2', 'essential-addons-for-elementor-lite' ),
