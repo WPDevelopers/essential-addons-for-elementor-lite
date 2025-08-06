e504395ae9f8f8ed6c69a3d981565eb502c0bd1e
Himadree Shekhar Halder <himadree12@gmail.com>
2025-08-06 13:18:57 +0600

    Add brightness effect controll of flip box

--- includes/Elements/Flip_Box.php
+++ includes/Elements/Flip_Box.php
@@ -1691,12 +1691,38 @@ class Flip_Box extends Widget_Base
         $this->eael_wd_liquid_glass_effect_bg_color_effect( $this, 'effect2', '#FFFFFF1F', 'eael-elements-flip-box-front-container' );
 
         do_action( 'eael_wd_liquid_glass_effect_bg_color_effect4', $this, 'effect4', '#FFFFFF1F', 'eael-elements-flip-box-front-container' );
-        do_action( 'eael_wd_liquid_glass_effect_bg_color_effect5', $this, 'effect5', '', 'eael-elements-flip-box-front-container' );
-        do_action( 'eael_wd_liquid_glass_effect_bg_color_effect6', $this, 'effect6', '', 'eael-elements-flip-box-front-container' );
+        do_action( 'eael_wd_liquid_glass_effect_bg_color_effect5', $this, 'effect5', '#FFFFFF1F', 'eael-elements-flip-box-front-container' );
+        do_action( 'eael_wd_liquid_glass_effect_bg_color_effect6', $this, 'effect6', '#FFFFFF1F', 'eael-elements-flip-box-front-container' );
 
         // Backdrop Filter Controls
         $this->eael_wd_liquid_glass_effect_backdrop_filter_effect( $this, 'effect1', '24', 'eael-elements-flip-box-front-container' );
         $this->eael_wd_liquid_glass_effect_backdrop_filter_effect( $this, 'effect2', '20', 'eael-elements-flip-box-front-container' );
+
+        // Brightness Effect Controls
+		$this->add_control(
+			'eael_wd_liquid_glass_effect_brightness_effect2',
+			[
+				'label' => esc_html__( 'Brightness', 'essential-addons-for-elementor-lite' ),
+				'type'  => Controls_Manager::SLIDER,
+				'range' => [
+					'px' => [
+						'min'  => 0,
+						'max'  => 5,
+						'step' => .1,
+					],
+				],
+				'default' => [
+					'size' => 1,
+				],
+				'selectors' => [
+					'{{WRAPPER}}.eael_wd_liquid_glass-effect2 .eael-elements-flip-box-front-container' => 'backdrop-filter: blur({{eael_wd_liquid_glass_effect_backdrop_filter_effect2.SIZE}}px) brightness({{SIZE}});',
+				],
+				'condition'    => [
+					'eael_wd_liquid_glass_effect_switch' => 'yes',
+					'eael_wd_liquid_glass_effect'        => 'effect2',
+				]
+			]
+		);
 
         do_action( 'eael_wd_liquid_glass_effect_backdrop_filter_effect4', $this, 'effect4', '', 'eael-elements-flip-box-front-container' );
         do_action( 'eael_wd_liquid_glass_effect_backdrop_filter_effect5', $this, 'effect5', '', 'eael-elements-flip-box-front-container' );
@@ -1929,6 +1955,32 @@ class Flip_Box extends Widget_Base
         // Backdrop Filter Controls
         $this->eael_wd_liquid_glass_effect_backdrop_filter_effect_rear( $this, 'effect1', '24', 'eael-elements-flip-box-rear-container' );
         $this->eael_wd_liquid_glass_effect_backdrop_filter_effect_rear( $this, 'effect2', '20', 'eael-elements-flip-box-rear-container' );
+
+        // Brightness Effect Controls
+		$this->add_control(
+			'eael_wd_liquid_glass_effect_brightness_effect2_rear',
+			[
+				'label' => esc_html__( 'Brightness', 'essential-addons-for-elementor-lite' ),
+				'type'  => Controls_Manager::SLIDER,
+				'range' => [
+					'px' => [
+						'min'  => 0,
+						'max'  => 5,
+						'step' => .1,
+					],
+				],
+				'default' => [
+					'size' => 1,
+				],
+				'selectors' => [
+					'{{WRAPPER}}.eael_wd_liquid_glass_rear-effect2 .eael-elements-flip-box-rear-container' => 'backdrop-filter: blur({{eael_wd_liquid_glass_effect_backdrop_filter_rear_effect2.SIZE}}px) brightness({{SIZE}});',
+				],
+				'condition'    => [
+					'eael_wd_liquid_glass_effect_switch_rear' => 'yes',
+					'eael_wd_liquid_glass_effect_rear'        => 'effect2',
+				]
+			]
+		);
 
         do_action( 'eael_wd_liquid_glass_effect_backdrop_filter_rear_effect4', $this, 'effect4', '', 'eael-elements-flip-box-rear-container' );
         do_action( 'eael_wd_liquid_glass_effect_backdrop_filter_rear_effect5', $this, 'effect5', '', 'eael-elements-flip-box-rear-container' );
