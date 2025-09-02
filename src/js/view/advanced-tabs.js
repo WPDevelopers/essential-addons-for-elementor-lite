887a22896bf7adb66d026d4395879795ec97eb1c
Himadree Shekhar Halder <himadree12@gmail.com>
2025-09-02 14:59:19 +0600

    Enhance advanced tabs functionality with dynamic Glassey tab bar style Added functions to initialize and update Glassey tab positions based on active tabs.

--- src/js/view/advanced-tabs.js
+++ src/js/view/advanced-tabs.js
@@ -193,7 +193,97 @@ eael.hooks.addAction("init", "ea", () => {
 					$('html, body').animate({
 						scrollTop: $("#"+hashTag).offset().top - $customIdOffsetValTab,
 					}, 300);
-			}
+         }
+         
+         // Glassey tab bar style
+         const $glasseyTabs = $scope.find('.eael-tabs-glassey');
+         console.log( $glasseyTabs );
+         
+         if ($glasseyTabs.length) {
+            initGlasseyTabStyle($glasseyTabs);
+         }
+         
+         /**
+          * Initialize dynamic Glassey tab bar style
+          */
+         function initGlasseyTabStyle($glasseyTabs) {
+            $glasseyTabs.each(function() {
+               const $tabContainer = $(this);
+               const $tabsList = $tabContainer.find('ul');
+               const $tabs = $tabsList.find('li');
+               const widgetId = $tabContainer.closest('.eael-advance-tabs').attr('id');
+
+               if (!widgetId || $tabs.length === 0) {
+                  return;
+               }
+
+               // Generate unique style ID for this widget instance
+               const styleId = 'eael-glassey-style-' + widgetId;
+
+               // Remove existing style if it exists
+               $('#' + styleId).remove();
+
+               // Calculate tab widths and positions
+               updateGlasseyTabPositions($tabContainer, $tabs, styleId);
+
+               // Update positions on window resize
+               $(window).on('resize.glassey-' + widgetId, function() {
+                  updateGlasseyTabPositions($tabContainer, $tabs, styleId);
+               });
+
+               // Update positions when tabs are clicked
+               $tabs.on('click.glassey', function() {
+                  setTimeout(function() {
+                        updateGlasseyTabPositions($tabContainer, $tabs, styleId);
+                  }, 50);
+               });
+            });
+         }
+         
+         /**
+          * Update Glassey tab positions and generate dynamic CSS
+          */
+         function updateGlasseyTabPositions($tabContainer, $tabs, styleId) {
+            const widgetId = $tabContainer.closest('.eael-advance-tabs').attr('id');
+            let cssRules = '';
+
+            $tabs.each(function() {
+               const $tab = $(this);
+               const tabNumber = $tab.data('tab');
+               const tabPosition = $tab.position();
+               const tabWidth = $tab.outerWidth();
+
+               if (tabPosition && typeof tabPosition.left !== 'undefined' && tabWidth) {
+                     const translateX = Math.round(tabPosition.left);
+                     const dynamicWidth = Math.round(tabWidth - 8);
+                     cssRules += `
+                        #${widgetId} .eael-tabs-glassey > ul:has(li.active[data-tab="${tabNumber}"])::after {
+                           translate: ${translateX}px 0;
+                           width: ${dynamicWidth}px;
+                           transform-origin: right;
+                           transition: background-color 400ms cubic-bezier(1, 0, 0.4, 1),
+                                 box-shadow 400ms cubic-bezier(1, 0, 0.4, 1),
+                                 translate 400ms cubic-bezier(1, 0, 0.4, 1),
+                                 width 400ms cubic-bezier(1, 0, 0.4, 1);
+                           animation: scaleToggle 440ms ease;
+                        }
+                     `;
+               }
+            });
+
+            // Create or update the style element
+            if (cssRules) {
+               let $styleElement = $('#' + styleId);
+               if ($styleElement.length === 0) {
+                     $styleElement = $('<style>', {
+                        id: styleId,
+                        type: 'text/css'
+                     });
+                     $('head').append($styleElement);
+               }
+               $styleElement.html(cssRules);
+            }
+         }
 		}
 	);
 });
