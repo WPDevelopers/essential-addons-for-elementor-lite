aaadbccf23ae1eee255fe4a8aa64169b1a49a0be
Kamrul Hasan <mdkamrul2058@gmail.com>
2026-09-13 15:27:07 +0600

    chore(assets): remove GPL-incompatible GSAP and unused bundled files
    - lib-view/gsap/Draggable: the GreenSock Standard License is not
      GPL-compatible, and its enqueue was already commented out in config.php.
    - lib-view/typeform: unreferenced duplicate of lib-view/embed.
    - css/view/woo-product-slider.min.css: Woo Product Slider is a Pro widget
      and Pro ships its own stylesheet; Lite never loaded this copy.
    
    Refs #897

--- config.php
+++ config.php
@@ -1200,17 +1200,6 @@ $config = [
 				    ],
 			    ],
 			    'js'  => [
-                    //gsap
-				    // [
-					//     'file' => EAEL_PLUGIN_PATH . 'assets/front-end/js/lib-view/gsap/gsap.min.js',
-					//     'type' => 'lib',
-					//     'context' => 'view',
-				    // ],
-                    // [
-					//     'file' => EAEL_PLUGIN_PATH . 'assets/front-end/js/lib-view/gsap/Draggable.min.js',
-					//     'type' => 'lib',
-					//     'context' => 'view',
-				    // ],
                     // //marquee
                     // [
                     //     'file' => EAEL_PLUGIN_PATH . 'assets/front-end/js/lib-view/marquee/marquee.min.js',
